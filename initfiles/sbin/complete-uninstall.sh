42f3d81978d55047c3c69d24eec9d74688c34535
Richard Chapman <rchapman@hpccsystems.com>
2011-08-16 08:39:31 +0100

    FIX: #168 complete-uninstall.sh is redhat only
    The utility script sbin/complete-uninstall.sh had a few issues:
     - assumed a rpm-based system
     - incorrect copyright message
     - assumed (non-standard) location of hpcc home directory
     - was not shipped
    
    Change the script to handle uninstalls of .deb files too, and use
    option on userdel to handle home directory removal more cleanly.
    Add the file back to the install set.
    
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- initfiles/sbin/complete-uninstall.sh
+++ initfiles/sbin/complete-uninstall.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
-################################################################################
-## Copyright © 2011 HPCC Systems.  All rights reserved.
-################################################################################
+#############################################
+#    Copyright (C) 2011 HPCC Systems.
+#
+#    All rights reserved. This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with All rights reserved. This program is free software: you can redistribute program.  If not, see <http://www.gnu.org/licenses/>.
+#############################################
 
-echo "Removing RPM"
-rpm -e hpccsystems-clienttools
-rpm -e hpccsystems-graphcontrol
-rpm -e hpccsystems-documentation
-rpm -e hpccsystems-platform
-
-echo "Removing user"
-if [ -e /usr/sbin/userdel ]; then
-    /usr/sbin/userdel hpcc
-elif [ -e /usr/bin/userdel ]; then
-    /usr/bin/userdel hpcc
-elif [ -e /bin/userdel ]; then
-    /bin/userdel hpcc
+if [ -e /etc/debian_version ]; then
+    dpkg --purge hpccsystems-platform
+    dpkg --purge hpccsystems-clienttools
+    dpkg --purge hpccsystems-graphcontrol
+    dpkg --purge hpccsystems-documentation
+elif [ -e /etc/redhat-release -o -e /etc/SuSE-release ]; then
+    echo "Removing RPM"
+    rpm -e hpccsystems-clienttools
+    rpm -e hpccsystems-graphcontrol
+    rpm -e hpccsystems-documentation
+    rpm -e hpccsystems-platform
 fi
-
-rm -rf /Users/hpcc
 
 echo "Removing Directory - /opt/HPCCSystems/*"
 rm -rf /opt/HPCCSystems/*
@@ -30,5 +39,14 @@ for i in lock run log lib; do
     echo "Removing Directory - /var/$i/HPCCSystems/*"
     rm -rf /var/$i/HPCCSystems/*; 
 done
+
+echo "Removing user"
+if [ -e /usr/sbin/userdel ]; then
+    /usr/sbin/userdel -r hpcc
+elif [ -e /usr/bin/userdel ]; then
+    /usr/bin/userdel -r hpcc
+elif [ -e /bin/userdel ]; then
+    /bin/userdel -r hpcc
+fi
 
 exit 0
