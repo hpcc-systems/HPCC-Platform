fd2e8993adb6eb9d59357b37c688166c53916f98
Jake Smith <jake.smith@lexisnexis.com>
2011-11-15 12:15:40 +0000

    Minor change to comment and usage tracing
    Remove a redundant line
    Fix a comment related to recent transactional logging changes
    Fix dalidiag usage logging, related to transaction logging changes
    
    Signed-off-by: Jake Smith <jake.smith@lexisnexis.com>

--- dali/base/dadfs.cpp
+++ dali/base/dadfs.cpp
@@ -7967,7 +7967,6 @@ public:
             IPropertyTree *root = sroot->queryPropTree(querySdsRelationshipsRoot());
             Owned<IPropertyTreeIterator> iter = root?root->getElements(xpath.str()):NULL;
             mb.append(count);
-            unsigned start = msTick();
             // save as sequence of branches
             if (iter) {
                 ForEach(*iter.get()) {
