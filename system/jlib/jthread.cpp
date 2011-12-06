5d2e2f49a7d583a04601dd6da6cf61df78188eaa
Renato Golin <rengolin@systemcall.org>
2011-12-06 14:59:47 +0000

    Silent Warnings

--- system/jlib/jthread.cpp
+++ system/jlib/jthread.cpp
@@ -1784,8 +1784,8 @@ public:
 
             unsigned argc;
             char **argv=splitargs(prog,argc);
-            if (dir.get())
-                chdir(dir);
+            if (dir.get() && chdir(dir))
+                _exit(START_FAILURE);    // must be _exit!!
             execvp(argv[0],argv);
             _exit(START_FAILURE);    // must be _exit!!     
         }
