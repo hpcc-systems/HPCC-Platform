79e1815712825dd4b2f723efca8bf2df39be6570
Gavin Halliday <gavin.halliday@lexisnexis.com>
2012-03-21 15:58:49 +0000

    Fix problem with constant folding inline tables
    In unusual cicumstances (see associated example file klogermann21.ecl)
    selectors were being replaced inside nested operators which were
    applied to the same dataset.
    
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- ecl/hql/hqlfold.cpp
+++ ecl/hql/hqlfold.cpp
@@ -36,6 +36,8 @@
 #include "hqlfold.hpp"
 #include "hqlthql.hpp"
 
+//#define LOG_ALL_FOLDING
+
 //---------------------------------------------------------------------------
 // The following functions work purely on constant expressions - they do not attempt to process datasets.
 
@@ -5145,6 +5147,11 @@ IHqlExpression * CExprFolderTransformer::createTransformed(IHqlExpression * expr
 
     OwnedHqlExpr updated = (foldOptions & HFOpercolateconstants) ? percolateConstants(dft) : LINK(dft);
     OwnedHqlExpr transformed = doFoldTransformed(updated, expr);
+
+#ifdef LOG_ALL_FOLDING
+    if ((op != transformed->getOperator()) || (expr->numChildren() != transformed->numChildren()))
+        DBGLOG("Folding %s to %s", getOpString(updated->getOperator()), getOpString(transformed->getOperator()));
+#endif
 
 #ifdef _DEBUG
     if (expr->isConstant() && !transformed->queryValue())
