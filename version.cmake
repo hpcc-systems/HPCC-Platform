5b03f13ae821fde49e7bbd6b43e29ad1b4f5f071
Richard Chapman <rchapman@hpccsystems.com>
2012-04-23 15:31:11 +0100

    Split branch for 3.8.x closedown
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -3,8 +3,8 @@
 ###
 set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 3 )
-set ( HPCC_MINOR 7 )
+set ( HPCC_MINOR 8 )
 set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "trunk" )
+set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 1 )
 ###
