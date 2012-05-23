77b080c6c4a1b9438296e44d5cb1b933639852c7
Richard Chapman <rchapman@hpccsystems.com>
2012-05-23 15:47:18 +0100

    Community Edition 3.8.0 Release Candidate 0
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -5,6 +5,6 @@ set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 3 )
 set ( HPCC_MINOR 8 )
 set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "closedown" )
-set ( HPCC_SEQUENCE 1 )
+set ( HPCC_MATURITY "rc" )
+set ( HPCC_SEQUENCE 0 )
 ###
