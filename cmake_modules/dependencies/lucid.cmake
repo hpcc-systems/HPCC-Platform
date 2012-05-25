69663fb6b167041003beea5daeef6ce29675db90
Richard Chapman <rchapman@hpccsystems.com>
2012-05-25 10:46:11 +0100

    gh-2384 Incorrect libarchive dependency in lucid/maverick
    The (new) dependency on libarchive was not correct for Ubuntu 10.04
    and 11.04 (where the package is called libarchive1).
    
    Fixes gh-2384.
    
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- cmake_modules/dependencies/lucid.cmake
+++ cmake_modules/dependencies/lucid.cmake
@@ -1,1 +1,1 @@
-set ( CPACK_DEBIAN_PACKAGE_DEPENDS "libboost-regex1.40.0, libicu42, libxalan110, libxerces-c28, binutils, libldap-2.4-2, openssl, zlib1g, g++, openssh-client, openssh-server, expect, libarchive")
+set ( CPACK_DEBIAN_PACKAGE_DEPENDS "libboost-regex1.40.0, libicu42, libxalan110, libxerces-c28, binutils, libldap-2.4-2, openssl, zlib1g, g++, openssh-client, openssh-server, expect, libarchive1")
