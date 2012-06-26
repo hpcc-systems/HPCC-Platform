f669dbd9ae528f873cfaaeaf080413a529243eed
Gleb Aronsky <gleb.aronsky@lexisnexis.com>
2012-06-26 09:28:27 -0400

    Fix gh-2649 Configmgr - duplicate component   - Add 'Duplicate Componenet/Service' to context menu   - Duplicate components and assign unique name
    Signed-off-by: Gleb Aronsky <gleb.aronsky@lexisnexis.com>

--- esp/files/scripts/configmgr/navtree.js
+++ esp/files/scripts/configmgr/navtree.js
@@ -702,13 +702,6 @@ function createNavigationTree(navTreeData) {
                 top.document.lastSelectedRow = temp1[0];
                 getWaitDlg().hide();
                 navDS.flushCache();
-                //                  var tid= YAHOO.util.Get.script('/esp/files/scripts/navtreedata.js',{ 
-                //                    onSuccess: function(obj) {
-                //                      var parsedResults = navDS.parseArrayData(o, navTreeData);
-                //                      navDS.handleResponse("", parsedResults.results, {   success:navDT.onDataReturnInitializeTable,
-                //                                                 scope:navDT}, this, 999);
-                //                      clickCurrentSelOrName(navDT, temp1[0]);
-                //                    }});
                 refreshNavTree(navDS, navDT, temp1[0])
               }
             }
@@ -727,6 +720,40 @@ function createNavigationTree(navTreeData) {
         scope: this
       },
         getFileName(true) + 'Operation=Delete&XmlArgs=' + xmlStr);
+    }
+    else if (menuItemName ==="Duplicate Component/Service")
+    {
+      var targetRec;
+      xmlStr = navDT.selectionToXML(targetRec, navDT.getSelectedRows(), targetRec, "Components");
+
+      YAHOO.util.Connect.asyncRequest('POST', '/WsDeploy/HandleComponent', {
+        success: function(o) {
+          if (o.status === 200) {
+            if (o.responseText.indexOf("<?xml") === 0) {
+              var form = document.forms['treeForm'];
+              form.isChanged.value = "true";
+              var temp = o.responseText.split(/<CompName>/g);
+              var temp1 = temp[1].split(/<\/CompName>/g);
+              top.document.lastSelectedRow = temp1[0];
+              getWaitDlg().hide();
+              navDS.flushCache();
+              refreshNavTree(navDS, navDT)
+            }
+            else if (o.responseText.indexOf("<html") === 0) {
+              var temp = o.responseText.split(/td align=\"left\">/g);
+              var temp1 = temp[1].split(/<\/td>/g);
+              getWaitDlg().hide();
+              alert(temp1[0]);
+            }
+          }
+        },
+        failure: function(o) {
+          getWaitDlg().hide();
+          alert(o.statusText);
+        },
+        scope: this
+      },
+        getFileName(true) + 'Operation=Duplicate&XmlArgs=' + xmlStr);
     }
     else {
       var xmlStr = "<Components><Component buildSet='" + menuItemName + "'/></Components>";
@@ -1127,7 +1154,8 @@ function createNavigationTree(navTreeData) {
                                 },
                                 onclick: { fn: onMenuSWClick }
                               },
-                              { text: "Delete Component/Service", onclick: { fn: onMenuSWClick} }
+                              { text: "Delete Component/Service", onclick: { fn: onMenuSWClick} },
+                              { text: "Duplicate Component/Service", onclick: { fn: onMenuSWClick} }
                           ],
     "Columns": [
                               {
@@ -1197,6 +1225,7 @@ function createNavigationTree(navTreeData) {
 
       if (record.getData('Name') === 'Software' || record.getData('Name') === 'Directories'){
         this.getItem(2).cfg.setProperty("disabled", true);
+        this.getItem(3).cfg.setProperty("disabled", true);
       }
 
       if (record.getData('id') === 0) {
