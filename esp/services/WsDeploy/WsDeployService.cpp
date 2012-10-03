8627a28a983e59402b9f61334af57ab6daff7ee9
Gleb Aronsky <gleb.aronsky@lexisnexis.com>
2012-10-03 14:09:50 -0400

    HPCC-7963 Configmgr - Missing && breaking build
    Signed-off-by: Gleb Aronsky <gleb.aronsky@lexisnexis.com>

--- esp/services/WsDeploy/WsDeployService.cpp
+++ esp/services/WsDeploy/WsDeployService.cpp
@@ -1288,7 +1288,7 @@ bool CWsDeployFileInfo::saveSetting(IEspContext &context, IEspSaveSettingRequest
 
         pComp->setProp(XML_ATTR_FILESBASEDN,strFilesBasedn);
       }
-      else if (bUpdateFilesBasedn == true && pComp != NULL && strcmp(pszCompType, XML_TAG_ESPPROCESS) == 0 && strcmp(pszAttrName, TAG_SERVICE) == 0 pszNewValue && *pszNewValue)
+      else if (bUpdateFilesBasedn == true && pComp != NULL && strcmp(pszCompType, XML_TAG_ESPPROCESS) == 0 && strcmp(pszAttrName, TAG_SERVICE) == 0 && pszNewValue && *pszNewValue)
       {
         Owned<IPropertyTree> pActiveEnvRoot = getEnvTree(context, &req.getReqInfo());
 
