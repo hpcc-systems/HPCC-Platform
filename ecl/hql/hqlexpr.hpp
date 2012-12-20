45f47b4ae32cd841cfa7e9b2e6935a2056be8c9c
Richard Chapman <rchapman@hpccsystems.com>
2012-12-20 16:06:02 +0000

    HPCC-8335 Option on a field to control the default value
    The default value of a field will be used:
    
    1. When a dictionary lookup does not find a match
    2. When a dataset select indexes a non-existent row
    3. When assigning SELF := [] in a transform
    4. In the default row passed to transforms by a OUTER join
    5. When reading from XML and a column is not present
    
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- ecl/hql/hqlexpr.hpp
+++ ecl/hql/hqlexpr.hpp
@@ -1332,7 +1332,7 @@ extern HQL_API IHqlExpression * createPureVirtual(ITypeInfo * type);
 extern HQL_API IHqlExpression * cloneOrLink(IHqlExpression * expr, HqlExprArray & children);
 extern HQL_API IHqlExpression * createConstantOne();
 extern HQL_API IHqlExpression * createLocalAttribute();
-extern HQL_API bool isNullExpr(IHqlExpression * expr, ITypeInfo * type);
+extern HQL_API bool isNullExpr(IHqlExpression * expr, IHqlExpression * field);
 inline bool isNull(IHqlExpression * expr)       { return expr->getOperator() == no_null; }
 inline bool isNullAction(IHqlExpression * expr) { return isNull(expr) && expr->isAction(); }
 inline bool isFail(IHqlExpression * expr)       { return expr->getOperator() == no_fail; }
