c415f1d07845b14a814f12ea034f84e89d19eb2b
Richard Chapman <rchapman@hpccsystems.com>
2013-01-10 12:29:55 +0000

    HPCC-8541 - Add examples illustrating calling Python and Java from ECL
    Improve the Java example so that it does not need complex command line options
    to build, and can work 'out of the box' on a standard linux install.
    
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

---
+++ initfiles/examples/jni/JavaCat.java
@@ -0,0 +1,9 @@
+public class JavaCat
+{
+    public static String cat(String a, String b)
+    {
+        // System.out.println("In java");
+        // System.out.println(a+b);
+        return a + b;
+    }
+}
