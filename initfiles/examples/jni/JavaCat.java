71a0f837a869083039224fbcc0a4aae09b371799
Richard Chapman <rchapman@hpccsystems.com>
2013-02-04 15:08:50 +0000

    HPCC-8738 Delete the original embedding examples
    Now that embedding support is fully built-in, remove the original examples
    that did it the hard way.
    
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- initfiles/examples/jni/JavaCat.java
+++
@@ -1,9 +0,0 @@
-public class JavaCat
-{
-    public static String cat(String a, String b)
-    {
-        // System.out.println("In java");
-        // System.out.println(a+b);
-        return a + b;
-    }
-}
--- initfiles/examples/python/python_cat.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-def cat(a, b):
-  return a + b
-
-if __name__=="__main__":
-   print cat("Hello", " world")
