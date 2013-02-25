a2f7ad169abca328d04347a5acaaaada3397384a
Gavin Halliday <gavin.halliday@lexisnexis.com>
2013-02-25 14:14:11 +0000

    HPCC-8856 Fix problems with inactive selectors
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- ecl/hql/hqlutil.cpp
+++ ecl/hql/hqlutil.cpp
@@ -4564,6 +4564,10 @@ static bool splitDatasetAttribute(SharedHqlExpr & dataset, SharedHqlExpr & attri
     node_operator leftOp = left->getOperator();
     if ((leftOp !=no_select) || expr->hasProperty(newAtom))
     {
+        //If this is a selection from an inscope dataset then this must not be assumed to be an input dataset.
+        if (expr->isDataset() && !expr->hasProperty(newAtom))
+            return false;
+
         IHqlExpression * lhs = LINK(left);
         IHqlExpression * field = expr->queryChild(1);
         if (lhs->isDataset()) lhs = createRow(no_activerow, lhs);
