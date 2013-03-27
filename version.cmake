d7f9f4a50c81a9a38bb1bd6603d7a2df318235d1
Richard Chapman <rchapman@hpccsystems.com>
2013-03-27 18:08:47 +0000

    Split off closedown-4.0.x branch
    Changes on master will now by default end up in 4.2
    
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -2,8 +2,8 @@
 ## Version Information
 ###
 set ( HPCC_PROJECT "community" )
-set ( HPCC_MAJOR 3 )
-set ( HPCC_MINOR 11 )
+set ( HPCC_MAJOR 4 )
+set ( HPCC_MINOR 1 )
 set ( HPCC_POINT 0 )
 set ( HPCC_MATURITY "trunk" )
 set ( HPCC_SEQUENCE 1 )
