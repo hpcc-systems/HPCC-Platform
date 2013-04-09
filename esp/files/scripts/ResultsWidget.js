9329490a62f38d832dd50f8ac128b5cac3559edc
Gordon Smith <gordon.smith@lexisnexis.com>
2013-04-09 14:10:23 +0100

    HPCC-9113 Running workunit not refreshing tabs
    Initial "monitor" callback does not happen until next change.
    
    Fixes HPCC-9113
    
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- esp/files/scripts/ResultsWidget.js
+++ esp/files/scripts/ResultsWidget.js
@@ -131,7 +131,7 @@ define([
                             }
                         });
                         var currSel = context.getSelectedChild();
-                        if (currSel) {
+                        if (currSel && currSel.refresh) {
                             currSel.refresh();
                         }
                     }
