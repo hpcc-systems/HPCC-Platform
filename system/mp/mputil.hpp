8f57d36839650da4661ddb85bc228f037a3e3e7f
Jake Smith <jake.smith@lexisnexis.com>
2013-05-22 12:09:58 +0100

    HPCC-9303 - Use thread pool for DFS requests
    Use threadpool for DFS server (used to be single threaded)
    
    Signed-off-by: Jake Smith <jake.smith@lexisnexis.com>

--- system/mp/mputil.hpp
+++ system/mp/mputil.hpp
@@ -45,12 +45,12 @@ public:
         return CInterface::Release(); 
     }
 
-    CMessageHandler(const char *_name,PARENT *_parent,void (PARENT::*_handler)(CMessageBuffer &_mb), IExceptionHandler *exceptionHandler=NULL, unsigned maxthreads=40, unsigned timeoutOnRelease=INFINITE)
+    CMessageHandler(const char *_name,PARENT *_parent,void (PARENT::*_handler)(CMessageBuffer &_mb), IExceptionHandler *exceptionHandler=NULL, unsigned maxthreads=40, unsigned timeoutOnRelease=INFINITE, unsigned lowThreadsDelay=1000)
     {
         parent = _parent;
         handler = _handler;
         name = strdup(_name);
-        pool = createThreadPool(name,this,exceptionHandler,maxthreads,1000,0,timeoutOnRelease); // this will cause this to be linked
+        pool = createThreadPool(name,this,exceptionHandler,maxthreads,lowThreadsDelay,0,timeoutOnRelease); // this will cause this to be linked
         hasexceptionhandler = exceptionHandler!=NULL;
     }
     ~CMessageHandler()
@@ -122,7 +122,6 @@ public:
             runname.append(' ').appendhex(*(b++),true);
         pool->start(&mb,runname.str());
     }
-
 };
 
 
