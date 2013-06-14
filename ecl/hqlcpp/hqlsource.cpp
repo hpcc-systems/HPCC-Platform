34de854278e86e98ba319793e71ea4ca8b1544ae
Gavin Halliday <gavin.halliday@lexisnexis.com>
2013-06-14 14:59:01 +0100

    HPCC-4476 Fix KEYED substring matching against a field from the index
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- ecl/hqlcpp/hqlsource.cpp
+++ ecl/hqlcpp/hqlsource.cpp
@@ -5436,6 +5436,8 @@ bool MonitorExtractor::matchSubstringFilter(KeyConditionInfo & matches, node_ope
     IHqlExpression * selector = left->queryChild(0);
     if (!isKeySelect(selector) || !okToKey(selector, keyedKind))
         return false;
+    if (!isIndexInvariant(right))
+        return false;
     ITypeInfo * fieldType = selector->queryType();
     unsigned fieldLength = fieldType->getStringLen();
     if (fieldLength == UNKNOWN_LENGTH)
