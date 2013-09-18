da4dbc1c388fc9dca991ea2bc99884ab149c1b97
Jake Smith <jake.smith@lexisnexis.com>
2013-09-18 10:43:03 +0100

    HPCC-10013 - Correct usage syntax of listworkunits
    Signed-off-by: Jake Smith <jake.smith@lexisnexis.com>

--- dali/daliadmin/daliadmin.cpp
+++ dali/daliadmin/daliadmin.cpp
@@ -100,7 +100,7 @@ void usage(const char *exe)
   printf("  cleanscopes                     -- remove empty scopes\n");
   printf("\n");
   printf("Workunit commands:\n");
-  printf("  listworkunits <workunit-mask> [<prop>=<val> <lower> <upper>]\n");
+  printf("  listworkunits [<prop>=<val> [<lower> [<upper>]]] -- list workunits that match prop=val in workunit name range lower to upper\n");
   printf("  listmatches <connection xpath> [<match xpath>=<val> [<property xpath>]]\n");
   printf("  workunittimings <WUID>\n");
   printf("\n");
@@ -1563,15 +1563,17 @@ static void cleanscopes(IUserDescriptor *user)
 
 //=============================================================================
 
-static void listworkunits(const char *test,const char *min, const char *max)
+static void listworkunits(const char *test, const char *min, const char *max)
 {
     Owned<IRemoteConnection> conn = querySDS().connect("/", myProcessSession(), 0, daliConnectTimeoutMs);
     Owned<IPropertyTreeIterator> iter = conn->queryRoot()->getElements("WorkUnits/*");
-    ForEach(*iter) {
+    ForEach(*iter)
+    {
         IPropertyTree &e=iter->query();
         if (test&&*test) {
             const char *tval = strchr(test,'=');
-            if (!tval) {
+            if (!tval)
+            {
                 ERRLOG("missing '=' in %s",test);
                 return;
             }
