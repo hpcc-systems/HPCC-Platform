04e1990f8777e7607c35a122690f162067084416
Attila.Vamos <attila.vamos@gmail.com>
2013-09-19 15:20:15 +0100

    HPCC-10040 Increase blockSize value to avoid "End of record not found..." exception during XML spray
    Rework after rewiev.
    Increase maximum record size to 0x100000 (1MB).
    
    Signed-off-by: Attila.Vamos <attila.vamos@gmail.com>

--- dali/ft/daftformat.cpp
+++ dali/ft/daftformat.cpp
@@ -1183,7 +1183,7 @@ offset_t XmlSplitter::getFooterLength(BufferedDirectReader & reader, offset_t si
 }
 
 
-CXmlPartitioner::CXmlPartitioner(const FileFormat & _format) : CInputBasePartitioner(_format.maxRecordSize, _format.maxRecordSize), splitter(_format)
+CXmlPartitioner::CXmlPartitioner(const FileFormat & _format) : CInputBasePartitioner( _format.maxRecordSize, (_format.maxRecordSize > 0x100000 ?_format.maxRecordSize: 0x100000)), splitter(_format)
 {
     format.set(_format);
     unitSize = format.getUnitSize();
