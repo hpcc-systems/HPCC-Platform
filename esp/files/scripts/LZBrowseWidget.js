46650038e4b2a74ed8eb3dc3c9f3b7f3e89be135
Gordon Smith <gordon.smith@lexisnexis.com>
2013-10-11 12:45:33 +0100

    HPCC-9812 Ability to spray from any source in new ECL Watch
    Fixes HPCC-9812
    
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- esp/files/scripts/LZBrowseWidget.js
+++ esp/files/scripts/LZBrowseWidget.js
@@ -209,7 +209,6 @@ define([
 
         _onSprayFixed: function (event) {
             if (this.sprayFixedDialog.validate()) {
-                var context = this;
                 var selections = this.landingZonesGrid.getSelected();
                 var context = this;
                 arrayUtil.forEach(selections, function (item, idx) {
@@ -231,7 +230,6 @@ define([
 
         _onSprayVariable: function(event) {
             if (this.sprayVariableDialog.validate()) {
-                var context = this;
                 var selections = this.landingZonesGrid.getSelected();
                 var context = this;
                 arrayUtil.forEach(selections, function (item, idx) {
@@ -252,7 +250,6 @@ define([
 
         _onSprayXml: function(event) {
             if (this.sprayXmlDialog.validate()) {
-                var context = this;
                 var selections = this.landingZonesGrid.getSelected();
                 var context = this;
                 arrayUtil.forEach(selections, function (item, idx) {
@@ -288,7 +285,6 @@ define([
 
             this.initLandingZonesGrid();
             this.selectChild(this.landingZonesTab, true);
-            var context = this;
             this.sprayFixedDestinationSelect.init({
                 Groups: true
             });
