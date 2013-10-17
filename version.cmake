9033a49b9ed654e8e3ed92e58ba2961bd8d7f66f
Richard Chapman <rchapman@hpccsystems.com>
2013-10-17 14:16:23 +0100

    Community Edition Control 4.2.0-rc1 Release Candidate 1
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -4,7 +4,7 @@
 set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 4 )
 set ( HPCC_MINOR 2 )
-set ( HPCC_POINT 1 )
-set ( HPCC_MATURITY "closedown" )
+set ( HPCC_POINT 0 )
+set ( HPCC_MATURITY "rc" )
 set ( HPCC_SEQUENCE 1 )
 ###
