49b917f0c93f404f981e2a13881bfe88367adb57
Attila Vamos <attila.vamos@gmail.com>
2013-11-06 16:52:23 +0000

    HPCC-10365 Regression suite requires non-existent directory
    Fix directory checking
    
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- testing/regress/hpcc/util/expandcheck.py
+++ testing/regress/hpcc/util/expandcheck.py
@@ -1,12 +1,22 @@
 import os
+import logging
+
 
 class ExpandCheck:
 
     @staticmethod
     def dir_exists(path, require=False):
+        logging.debug("dir_exists(path: %s, require: %d", path, require)
         path = os.path.abspath(path)
-        if not os.path.exists(path) and require:
-            raise IOError("REQUIRED DIRECTORY NOT FOUND. " + path)
+        if not os.path.exists(path):
+            if require:
+                logging.debug("Path: %s not found and it is required!" ,path)
+                try:
+                    os.mkdir(path)
+                except:
+                    raise IOError("REQUIRED DIRECTORY NOT FOUND. " + path)
+            else:
+                logging.info( "DIRECTORY NOT FOUND. " + path)
 
         return(path)
 
