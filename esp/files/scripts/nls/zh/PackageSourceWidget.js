ddded8f54f8775b036dbd3ec332b868fb4648e01
Kevin Wang <kevin.wang@lexisnexis.com>
2014-02-11 13:38:10 -0500

    HPCC-10748 Add Chinese translation for Scheduler/package
    In this fix, Chinese translation is added for both Event
    Scheduled WU widget and Package Map Widget.
    
    Signed-off-by: Kevin Wang <kevin.wang@lexisnexis.com>

--- esp/files/scripts/nls/zh/PackageSourceWidget.js
+++ esp/files/scripts/nls/zh/PackageSourceWidget.js
@@ -1,12 +1,12 @@
 ﻿define(
 ({
-    Errors: "Error(s)",
-    FilesNoPackage: "Files without matching package definitions",
-    Loading: "Loading...",
-    NoErrorFound: "No errors found\n",
-    NoWarningFound: "No warnings found\n",
-    PackagesNoQuery: "Packages without matching queries",
-    QueriesNoPackage: "Queries without matching package",
-    Warnings: "Warning(s)"
+    Errors: "错误",
+    FilesNoPackage: "与文件包定义不匹配的文件",
+    Loading: "加载...",
+    NoErrorFound: "没有发现任何错误\n",
+    NoWarningFound: "没有发现任何警告\n",
+    PackagesNoQuery: "与查询程序不匹配的文件包",
+    QueriesNoPackage: "与文件包不匹配的查询程序",
+    Warnings: "警告"
 })
 );
