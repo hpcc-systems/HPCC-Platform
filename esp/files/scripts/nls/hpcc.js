2f85ce74694338e74969709c54a2d52f21d151b6
Gordon Smith <gordon.smith@lexisnexis.com>
2014-03-12 19:23:01 +0000

    HPCC-10722 LogicalFile Details Missing Information
    Missing Record Counts and Modified date.
    
    Fixes HPCC-10722
    Fixes HPCC-10721
    
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- esp/files/scripts/nls/hpcc.js
+++ esp/files/scripts/nls/hpcc.js
@@ -186,6 +186,7 @@ define({root:
     Members: "Members",
     Message: "Message",
     Min: "Min",
+    Modified: "Modified",
     Modification: "Modification",
     ModifiedUTCGMT: "Modified (UTC/GMT)",
     Modify: "Modify",
