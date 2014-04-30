fbfd51b4c98d146c42d64452e5c34bfcd72ea13f
Richard Chapman <rchapman@hpccsystems.com>
2014-04-30 17:07:34 +0100

    Community Edition 4.2.4-1 Gold
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -5,6 +5,6 @@ set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 4 )
 set ( HPCC_MINOR 2 )
 set ( HPCC_POINT 4 )
-set ( HPCC_MATURITY "rc" )
-set ( HPCC_SEQUENCE 4 )
+set ( HPCC_MATURITY "release" )
+set ( HPCC_SEQUENCE 1 )
 ###
