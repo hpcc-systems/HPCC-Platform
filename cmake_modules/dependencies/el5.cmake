8f96fd75d5eb7c3841d139c998ce47037764d836
Xiaoming Wang <xiaoming.wang@lexisnexis.com>
2014-06-03 09:02:22 -0400

    HPCC-11586 Add prerequisite packages apache portable runtime apr and apr-util
    For RPM based: apr and apr-util
    For DEB based: libapr1 and libaprutil1

--- cmake_modules/dependencies/el5.cmake
+++ cmake_modules/dependencies/el5.cmake
@@ -1,1 +1,1 @@
-set ( CPACK_RPM_PACKAGE_REQUIRES "boost141-regex, openldap, libicu, m4, libtool, xalan-c, xerces-c, gcc-c++, openssh-server, openssh-clients, expect, libarchive, rsync")
+set ( CPACK_RPM_PACKAGE_REQUIRES "boost141-regex, openldap, libicu, m4, libtool, xalan-c, xerces-c, gcc-c++, openssh-server, openssh-clients, expect, libarchive, rsync, apr, apr-util")
