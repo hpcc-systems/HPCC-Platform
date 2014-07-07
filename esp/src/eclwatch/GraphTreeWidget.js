b8bac12d5fc4ec31de023ff78e822ebb5fe7255d
Gordon Smith <gordon.smith@lexisnexis.com>
2014-07-07 15:31:57 +0100

    HPCC-11900 Roxie graphs fail to display
    Fixes HPCC-11900
    
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- esp/src/eclwatch/GraphTreeWidget.js
+++ esp/src/eclwatch/GraphTreeWidget.js
@@ -391,6 +391,7 @@ define([
                             }
                         },
                         onGetTimers: function (timers) {
+                            context.graphTimers = context.wu.getGraphTimers(context.GraphName);
                         }
                     });
                 });
@@ -427,7 +428,7 @@ define([
         },
 
         loadGraphFromXGMML: function (xgmml) {
-            if (this.global.loadXGMML(xgmml, false, this.wu.getGraphTimers(this.params.GraphName))) {
+            if (this.global.loadXGMML(xgmml, false, this.graphTimers)) {
                 this.global.setMessage("...");  //  Just in case it decides to render  ---
                 var initialSelection = [];
                 this.setMainRootItems(initialSelection);
@@ -439,7 +440,7 @@ define([
         },
 
         mergeGraphFromXGMML: function (xgmml) {
-            if (this.global.loadXGMML(xgmml, true, this.wu.getGraphTimers(this.params.GraphName))) {
+            if (this.global.loadXGMML(xgmml, true, this.graphTimers)) {
                 this.global.setMessage("...");  //  Just in case it decides to render  ---
                 this.refreshMainXGMML();
                 this.loadSubgraphs();
