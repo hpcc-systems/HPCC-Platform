2f3ff1cba9d3bfa45ef70e85f0cd6a7b8d6c73b6
Gavin Halliday <gavin.halliday@lexisnexis.com>
2015-01-20 13:48:41 +0000

    HPCC-12875 Fix problems with lines > 1024 characters long
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- system/jlib/jprop.cpp
+++ system/jlib/jprop.cpp
@@ -167,14 +167,18 @@ public:
             char buf[1024];
             while (fgets(buf,1024,inFile))
             {
-                const char *s=buf;
+                sbuff.clear();
                 do // handle lines longer than 1024 (bit kludgy)
                 {
                     size32_t l=(size32_t)strlen(buf);
                     if (!l || (buf[l-1]=='\n')) // should end in \n unless overflowed
                         break;
-                    s = sbuff.append(buf).str();
+                    sbuff.append(buf);
                 } while (fgets(buf,1024,inFile));
+
+                const char *s=buf;
+                if (sbuff.length())
+                    s = sbuff.append(buf).str();
 
                 if (*s == '#')
                     continue;
