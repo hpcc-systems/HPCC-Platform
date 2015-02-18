55fb8be1a7ad878e958b530709e6228e039f1ace
Gordon Smith <gordon.smith@lexisnexis.com>
2015-02-18 15:18:17 +0000

    HPCC-13064 Building 5.0.x after installing Node.JS will cause the packaging to fail.
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- esp/build.sh
+++ esp/build.sh
@@ -41,12 +41,10 @@ perl -pe "
 
 cd "$TOOLSDIR"
 
-if which node >/dev/null; then
-    node ../../dojo/dojo.js load=build --require "$PROFILE" --releaseDir "$DISTDIR" ${*:2}
-elif which java >/dev/null; then
+if which java > /dev/null; then
     java -Xms256m -Xmx256m  -cp ../shrinksafe/js.jar:../closureCompiler/compiler.jar:../shrinksafe/shrinksafe.jar org.mozilla.javascript.tools.shell.Main  ../../dojo/dojo.js baseUrl=../../dojo load=build --profile "$PROFILE" --releaseDir "$DISTDIR" ${*:2}
 else
-    echo "Need node.js or Java to build!"
+    echo "Need Java to build!"
     exit 1
 fi
 
