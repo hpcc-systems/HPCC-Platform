9991df9aef18f12ca4a110fb5d9d994626830673
Gavin Halliday <gavin.halliday@lexisnexis.com>
2015-02-25 11:45:44 +0000

    HPCC-13102 Regress HPCC-9577 so conditional subgraphs are not merged
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- ecl/hqlcpp/hqlcpp.cpp
+++ ecl/hqlcpp/hqlcpp.cpp
@@ -1731,6 +1731,7 @@ void HqlCppTranslator::cacheOptions()
         DebugOption(options.optimizeMax,"optimizeMax",false),
         DebugOption(options.useResultsForChildSpills,"useResultsForChildSpills",false),
         DebugOption(options.alwaysUseGraphResults,"alwaysUseGraphResults",false),
+        DebugOption(options.noConditionalLinks,"noConditionalLinks",false),
         DebugOption(options.reportAssertFilenameTail,"reportAssertFilenameTail",false),        
     };
 
