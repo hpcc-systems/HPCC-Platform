2c74bcd2758f805c82418bc2f4757a27760bf229
Shamser Ahmed <shamser.ahmed@lexisnexis.co.uk>
2015-06-29 18:28:11 +0100

    HPCC-13496 'this' pointer cannot be null in well-defined C++ code
    - Undo changes to archive.cpp

--- common/remote/hooks/libarchive/archive.cpp
+++ common/remote/hooks/libarchive/archive.cpp
@@ -209,7 +209,7 @@ public:
     ~ArchiveFileIO()
     {
 #if (ARCHIVE_VERSION_NUMBER >= 3000000)
-        //archive_read_free(archive);
+        archive_read_free(archive);
 #else
         archive_read_finish(archive);
 #endif
@@ -535,7 +535,7 @@ public:
             archive_entry_free(entry);
         }
 #if (ARCHIVE_VERSION_NUMBER >= 3000000)
-        //archive_read_free(archive);
+        archive_read_free(archive);
 #else
         archive_read_finish(archive);
 #endif
