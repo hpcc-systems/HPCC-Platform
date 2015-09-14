744f1e3ec36bb9c7e981209510d4127c6bfe8043
Michael Gardner <Michael.Gardner@lexisnexis.com>
2015-09-14 11:47:59 -0400

    hpcc-14160 Cache variable set differently in platform
    Signed-off-by: Michael Gardner <Michael.Gardner@lexisnexis.com>

--- cmake_modules/dependencies/el5.cmake
+++ cmake_modules/dependencies/el5.cmake
@@ -1,1 +1,1 @@
-SET_DEPENDENCIES ( CPACK_RPM_PACKAGE_REQUIRES "boost141-regex, openldap, libicu, m4, libtool, libxslt, libxml2, gcc-c++, openssh-server, openssh-clients, expect, libarchive, rsync, apr, apr-util, zip, tbb" )
+set ( CPACK_RPM_PACKAGE_REQUIRES "boost141-regex, openldap, libicu, m4, libtool, libxslt, libxml2, gcc-c++, openssh-server, openssh-clients, expect, libarchive, rsync, apr, apr-util, zip, tbb" CACHE STRING "" FORCE )
