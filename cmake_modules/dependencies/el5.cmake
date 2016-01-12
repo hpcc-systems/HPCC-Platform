6b4f1dcd3404047f446c63756d18ea2cb07dcc93
Michael Gardner <Michael.Gardner@lexisnexis.com>
2016-01-12 16:15:29 +0000

    HPCC-8401 Modifications to use dpkg-shlibdeps during packaging
    Signed-off-by: Michael Gardner <Michael.Gardner@lexisnexis.com>
    Signed-off-by: Michael Gardner <michael.gardner@lexisnexis.com>

--- cmake_modules/dependencies/el5.cmake
+++ cmake_modules/dependencies/el5.cmake
@@ -1,1 +1,1 @@
-SET_DEPENDENCIES ( CPACK_RPM_PACKAGE_REQUIRES boost141-regex openldap libicu m4 libtool libxslt libxml2 gcc-c++ openssh-server openssh-clients expect libarchive rsync apr apr-util zip tbb )
+SET_DEPENDENCIES ( CPACK_RPM_PACKAGE_REQUIRES m4 libtool gcc-c++ openssh-server openssh-clients expect rsync zip )
