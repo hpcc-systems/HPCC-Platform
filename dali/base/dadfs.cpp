9462aba98f2d3ec93104644dcf4151ac2aab17bd
Jake Smith <jake.smith@lexisnexis.com>
2016-01-18 11:36:23 +0000

    HPCC-14785 Throw an error on attempt to remove super that's owned
    Signed-off-by: Jake Smith <jake.smith@lexisnexis.com>

--- dali/base/dadfs.cpp
+++ dali/base/dadfs.cpp
@@ -3043,7 +3043,23 @@ public:
     {
         return !checkProtectAttr(logicalName.get(),root,reason);
     }
-
+    bool checkOwned(StringBuffer &error)
+    {
+        Owned<IPropertyTreeIterator> iter = root->getElements("SuperOwner");
+        if (iter->first())
+        {
+            error.append("Cannot remove file ").append(logicalName.get()).append(" as owned by SuperFile(s): ");
+            loop
+            {
+                error.append(iter->query().queryProp("@name"));
+                if (!iter->next())
+                    break;
+                error.append(", ");
+            }
+            return true;
+        }
+        return false;
+    }
     bool canRemove(StringBuffer &reason,bool ignoresub=false)
     {
         CriticalBlock block(sect);
@@ -3054,31 +3070,25 @@ public:
             reason.appendf("empty filename");
             return false;
         }
-        if (logicalName.isQuery()) {
+        if (logicalName.isQuery())
+        {
             reason.appendf("%s is query",logicalname);
             return false;
         }
-        if (logicalName.isForeign()) {
+        if (logicalName.isForeign())
+        {
             reason.appendf("%s is foreign",logicalname);
             return false;
         }
-        if (logicalName.isMulti()) {
+        if (logicalName.isMulti())
+        {
             reason.appendf("%s is multi",logicalname);
             return false;
         }
-        if (!ignoresub) {
-            // And has super owners
-            Owned<IPropertyTreeIterator> iter = root->getElements("SuperOwner");
-            if (iter->first()) {
-                reason.append("Cannot remove file ").append(logicalname).append(" as owned by SuperFile(s): ");
-                loop {
-                    reason.append(iter->query().queryProp("@name"));
-                    if (!iter->next())
-                        break;
-                    reason.append(", ");
-                }
+        if (!ignoresub)
+        {
+            if (checkOwned(reason))
                 return false;
-            }
         }
         return true;
     }
@@ -5468,7 +5478,10 @@ public:
         assertex(conn.get()); // must be attached
         CriticalBlock block(sect);
         checkModify("CDistributedSuperFile::detach");
-        subfiles.kill();    
+        StringBuffer reason;
+        if (checkOwned(reason))
+            throw MakeStringException(-1, "detach: %s", reason.str());
+        subfiles.kill();
 
         // Remove from SDS
 
