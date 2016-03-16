6b36cd0f08e665163f96bac7cb132c06cabfd2c2
Gordon Smith <gordonjsmith@gmail.com>
2016-03-16 22:25:06 +0000

    HPCC-15091 Graph optionally hide spill write/reads
    Added option to hide activities.
    Set better defaults for legacy page.
    Options persist no longer global.
    
    Fixes HPCC-15091
    
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- esp/src/eclwatch/JSGraphWidget.js
+++ esp/src/eclwatch/JSGraphWidget.js
@@ -27,15 +27,18 @@ define([
 ], function (declare, lang, i18n, nlsHPCC, arrayUtil, Evented,
             WsWorkunits, GraphWidget, ESPGraph) {
 
-    var persist = {
+    var Persist = declare([], {
+        constructor: function(id) {
+            this.id = "JSGraphWidget" + id + "_";
+        },
         remove: function (key) {
             if (typeof (Storage) !== "undefined") {
-                localStorage.removeItem("JSGraphWidget_" + key);
+                localStorage.removeItem(this.id + key);
             }
         },
         set: function (key, val) {
             if (typeof (Storage) !== "undefined") {
-                localStorage.setItem("JSGraphWidget_" + key, val);
+                localStorage.setItem(this.id + key, val);
             }
         },
         setObj: function (key, val) {
@@ -43,7 +46,7 @@ define([
         },
         get: function (key, defValue) {
             if (typeof (Storage) !== "undefined") {
-                var retVal = localStorage.getItem("JSGraphWidget_" + key);
+                var retVal = localStorage.getItem(this.id + key);
                 return retVal === null ? defValue : retVal;
             }
             return "";
@@ -57,12 +60,12 @@ define([
         },
         exists: function (key) {
             if (typeof (Storage) !== "undefined") {
-                var retVal = localStorage.getItem("JSGraphWidget_" + key);
+                var retVal = localStorage.getItem(this.id + key);
                 return retVal === null;
             }
             return false;
         }
-    };
+    });
 
     var faCharFactory = function (kind) {
         switch (kind) {
@@ -310,8 +313,8 @@ define([
                         return this.cleanObjects(this.graphData.edges);
                     },
 
-                    getLocalisedXGMML: function (selectedItems, depth, distance) {
-                        return this.graphData.getLocalisedXGMML(selectedItems, depth, distance);
+                    getLocalisedXGMML: function (selectedItems, depth, distance, noSpills) {
+                        return this.graphData.getLocalisedXGMML(selectedItems, depth, distance, noSpills);
                     },
 
                     startLayout: function (layout) {
@@ -340,9 +343,9 @@ define([
 
                     _loadXGMML: function (xgmml, merge) {
                         if (merge) {
-                            this.graphData.merge(xgmml);
+                            this.graphData.merge(xgmml, {});
                         } else {
-                            this.graphData.load(xgmml);
+                            this.graphData.load(xgmml, {});
                         }
                         if (!this._skipRender) {
                             this.rebuild(merge);
@@ -391,48 +394,59 @@ define([
                         var labelTpl = this.option("vlabel");
                         var tooltipTpl = this.option("vtooltip");
                         arrayUtil.forEach(this.graphData.vertices, function (item, idx) {
-                            if (!merge || !item.__widget) {
-                                switch (item._kind) {
-                                    case "point":
-                                        item.__widget = new Shape()
-                                            .radius(7)
-                                        ;
-                                        break;
-                                    default:
-                                        if (this.option("vicon") && this.option("vlabel")) {
-                                            item.__widget = new Vertex()
-                                                .faChar(faCharFactory(item._kind))
-                                            ;
-                                        } else if (this.option("vicon")) {
-                                            item.__widget = new Icon()
-                                                .faChar(faCharFactory(item._kind))
-                                            ;
-                                        } else if (this.option("vlabel")) {
-                                            item.__widget = new TextBox()
-                                            ;
-                                        } else {
+                            if (!this.option("vhidespills") || !item.isSpill()) {
+                                if (!merge || !item.__widget) {
+                                    switch (item._kind) {
+                                        case "point":
                                             item.__widget = new Shape()
                                                 .radius(7)
                                             ;
-                                        }
-                                        break;
+                                            break;
+                                        default:
+                                            if (this.option("vicon") && this.option("vlabel")) {
+                                                item.__widget = new Vertex()
+                                                    .faChar(faCharFactory(item._kind))
+                                                ;
+                                            } else if (this.option("vicon")) {
+                                                item.__widget = new Icon()
+                                                    .faChar(faCharFactory(item._kind))
+                                                ;
+                                            } else if (this.option("vlabel")) {
+                                                item.__widget = new TextBox()
+                                                ;
+                                            } else {
+                                                item.__widget = new Shape()
+                                                    .radius(7)
+                                                ;
+                                            }
+                                            break;
+                                    }
+                                    item.__widget.__hpcc_globalID = item.__hpcc_id;
                                 }
-                                item.__widget.__hpcc_globalID = item.__hpcc_id;
+                                if (item.__widget.text) {
+                                    var label = this.format(labelTpl, item);
+                                    item.__widget.text(label);
+                                }
+                                if (item.__widget.tooltip) {
+                                    var tooltip = this.format(tooltipTpl, item);
+                                    item.__widget.tooltip(tooltip);
+                                }
+                                vertices.push(item.__widget);
                             }
-                            if (item.__widget.text) {
-                                var label = this.format(labelTpl, item);
-                                item.__widget.text(label);
-                            }
-                            if (item.__widget.tooltip) {
-                                var tooltip = this.format(tooltipTpl, item);
-                                item.__widget.tooltip(tooltip);
-                            }
-                            vertices.push(item.__widget);
                         }, this);
                         labelTpl = this.option("elabel");
                         tooltipTpl = this.option("etooltip");
                         arrayUtil.forEach(this.graphData.edges, function (item, idx) {
-                            if (!merge || !item.__widget) {
+                            var source = item.getSource();
+                            var target = item.getTarget();
+                            if (!this.option("vhidespills") || !target.isSpill()) {
+                                var label = this.format(labelTpl, item);
+                                var tooltip = this.format(tooltipTpl, item);
+                                var slavesTotal = parseInt(item.NumSlaves);
+                                var started = parseInt(item.NumStarted) > 0;
+                                var finished = parseInt(item.NumStopped) === parseInt(item.NumSlaves);
+                                var active = started && !finished;
+
                                 var strokeDasharray = null;
                                 var weight = 100;
                                 if (item._dependsOn) {
@@ -440,48 +454,48 @@ define([
                                     strokeDasharray = "1,5";
                                 } else if (item._childGraph) {
                                     strokeDasharray = "5,5";
-                                } else if (item._sourceActivity || item._targetActivity) {
+                                } else if (item._isSpill) {
                                     weight = 25;
                                     strokeDasharray = "5,5,10,5";
                                 }
-
+                                if (this.option("vhidespills") && source.isSpill()) {
+                                    label += "\n(" + nlsHPCC.Spill + ")";
+                                    weight = 25;
+                                    strokeDasharray = "5,5,10,5";
+                                    while (source.isSpill()) {
+                                        var inputs = source.getInVertices();
+                                        source = inputs[0];
+                                    }
+                                }
                                 item.__widget = new Edge()
-                                    .sourceVertex(item.getSource().__widget)
-                                    .targetVertex(item.getTarget().__widget)
+                                    .sourceVertex(source.__widget)
+                                    .targetVertex(target.__widget)
                                     .targetMarker("arrowHead")
                                     .weight(weight)
                                     .strokeDasharray(strokeDasharray)
                                 ;
                                 item.__widget.__hpcc_globalID = item.__hpcc_id;
+                                item.__widget.text(label);
+                                item.__widget.tooltip(tooltip);
+                                item.__widget.classed({
+                                    started: started && !finished && !active,
+                                    finished: finished && !active,
+                                    active: active
+                                });
+                                edges.push(item.__widget);
                             }
-                            var label = this.format(labelTpl, item);
-                            var tooltip = this.format(tooltipTpl, item);
-                            var slavesTotal = parseInt(item.NumSlaves);
-                            var started = parseInt(item.NumStarted) > 0;
-                            var finished = parseInt(item.NumStopped) === parseInt(item.NumSlaves);
-                            var active = started && !finished;
-                            item.__widget.text(label);
-                            item.__widget.tooltip(tooltip);
-                            item.__widget.classed({
-                                started: started && !finished && !active,
-                                finished: finished && !active,
-                                active: active
-                            });
-                            edges.push(item.__widget);
                         }, this);
                         if (this.option("subgraph")) {
                             arrayUtil.forEach(this.graphData.subgraphs, function (subgraph, idx) {
                                 arrayUtil.forEach(subgraph.__hpcc_subgraphs, function (item, idx) {
-                                    if (!subgraph.__widget || !item.__widget) {
-                                        var d = 0;
+                                    if (subgraph.__widget && item.__widget) {
+                                        hierarchy.push({ parent: subgraph.__widget, child: item.__widget });
                                     }
-                                    hierarchy.push({ parent: subgraph.__widget, child: item.__widget });
                                 }, this);
                                 arrayUtil.forEach(subgraph.__hpcc_vertices, function (item, idx) {
-                                    if (!subgraph.__widget || !item.__widget) {
-                                        var d = 0;
+                                    if (subgraph.__widget && item.__widget) {
+                                        hierarchy.push({ parent: subgraph.__widget, child: item.__widget });
                                     }
-                                    hierarchy.push({ parent: subgraph.__widget, child: item.__widget });
                                 }, this);
                             }, this);
                         }
@@ -519,11 +533,11 @@ define([
 
         _onOptionsApply: function () {
             var optionsValues = this.optionsForm.getValues();
-            persist.setObj("options", optionsValues);
+            this.persist.setObj("options", optionsValues);
             this.optionsDropDown.closeDropDown();
             this._plugin.optionsReset(optionsValues);
-            this._plugin.rebuild();
-            this._onClickRefresh();
+            delete this.xgmml;
+            this._onRefreshScope();
         },
 
         _onOptionsReset: function () {
@@ -547,11 +561,25 @@ define([
 
         createPlugin: function () {
             if (!this.hasPlugin()) {
+                this.persist = new Persist(this._persistID || "");
                 var context = this;
                 loadJSPlugin(function (JSPlugin) {
                     context._plugin = new JSPlugin(context.graphContentPane.domNode);
                     context._plugin._optionsDefault = context.optionsForm.getValues();
-                    var optionsValues = lang.mixin({}, context._plugin._optionsDefault, persist.getObj("options"));
+                    switch (context._persistID) {
+                        case "overview":
+                            context._plugin._optionsDefault.subgraph = ["on"];
+                            context._plugin._optionsDefault.vlabel = "";
+                            break;
+                        case "local":
+                            context._plugin._optionsDefault.subgraph = ["on"];
+                            context._plugin._optionsDefault.vhidespills = ["off"];
+                            break;
+                        default:
+                            context._plugin._optionsDefault.vhidespills = ["on"];
+                            break;
+                    }
+                    var optionsValues = lang.mixin({}, context._plugin._optionsDefault, context.persist.getObj("options"));
                     context._plugin.optionsReset(optionsValues);
                     context.optionsForm.setValues(optionsValues);
                     context.version = {
