487c9331d8cbebfa82d173e2c0aacea9173396c1
Richard Chapman <rchapman@hpccsystems.com>
2016-06-03 08:31:41 +0100

    Create 6.0.x branch for continuous integration of documentation
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -1,10 +1,10 @@
 ###
 ## Version Information
 ###
-set ( HPCC_PROJECT "community" )
+set ( HPCC_PROJECT "documentation" )
 set ( HPCC_MAJOR 6 )
 set ( HPCC_MINOR 0 )
-set ( HPCC_POINT 1 )
-set ( HPCC_MATURITY "closedown" )
+set ( HPCC_POINT 0 )
+set ( HPCC_MATURITY "continuous" )
 set ( HPCC_SEQUENCE 0  )
 ###
