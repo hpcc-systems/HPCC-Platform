5b9fe36a94e039aeadc44c76064474d857a3b501
Jake Smith <jake.smith@lexisnexis.com>
2016-06-16 15:51:03 +0100

    HPCC-15674 Report key node stats. at end of graph, not per channel
    Signed-off-by: Jake Smith <jake.smith@lexisnexis.com>

--- thorlcr/graph/thgraphslave.cpp
+++ thorlcr/graph/thgraphslave.cpp
@@ -1080,22 +1080,6 @@ void CSlaveGraph::done()
         throw LINK(exception.get());
 }
 
-void CSlaveGraph::end()
-{
-    CGraphBase::end();
-    if (!queryOwner())
-    {
-        if (atomic_read(&nodesLoaded)) // wouldn't mean much if parallel jobs running
-            GraphPrintLog("JHTree node stats:\ncacheAdds=%d\ncacheHits=%d\nnodesLoaded=%d\nblobCacheHits=%d\nblobCacheAdds=%d\nleafCacheHits=%d\nleafCacheAdds=%d\nnodeCacheHits=%d\nnodeCacheAdds=%d\n", atomic_read(&cacheAdds), atomic_read(&cacheHits), atomic_read(&nodesLoaded), atomic_read(&blobCacheHits), atomic_read(&blobCacheAdds), atomic_read(&leafCacheHits), atomic_read(&leafCacheAdds), atomic_read(&nodeCacheHits), atomic_read(&nodeCacheAdds));
-        JSocketStatistics stats;
-        getSocketStatistics(stats);
-        StringBuffer s;
-        getSocketStatisticsString(stats,s);
-        GraphPrintLog("Socket statistics : %s\n",s.str());
-        resetSocketStatistics();
-    }
-}
-
 bool CSlaveGraph::serializeStats(MemoryBuffer &mb)
 {
     unsigned beginPos = mb.length();
@@ -1546,6 +1530,18 @@ void CJobSlave::startJob()
             throw MakeThorException(TE_NotEnoughFreeSpace, "Node %s has %u MB(s) of available disk space, specified minimum for this job: %u MB(s)", ep.getUrlStr(s).str(), (unsigned) freeSpace / 0x100000, minFreeSpace);
         }
     }
+}
+
+void CJobSlave::reportGraphEnd(graph_id gid)
+{
+    if (atomic_read(&nodesLoaded)) // wouldn't mean much if parallel jobs running
+        PROGLOG("Graph[%" GIDPF "u] - JHTree node stats:\ncacheAdds=%d\ncacheHits=%d\nnodesLoaded=%d\nblobCacheHits=%d\nblobCacheAdds=%d\nleafCacheHits=%d\nleafCacheAdds=%d\nnodeCacheHits=%d\nnodeCacheAdds=%d\n", gid, atomic_read(&cacheAdds), atomic_read(&cacheHits), atomic_read(&nodesLoaded), atomic_read(&blobCacheHits), atomic_read(&blobCacheAdds), atomic_read(&leafCacheHits), atomic_read(&leafCacheAdds), atomic_read(&nodeCacheHits), atomic_read(&nodeCacheAdds));
+    JSocketStatistics stats;
+    getSocketStatistics(stats);
+    StringBuffer s;
+    getSocketStatisticsString(stats,s);
+    PROGLOG("Graph[%" GIDPF "u] - Socket statistics : %s\n", gid, s.str());
+    resetSocketStatistics();
 }
 
 __int64 CJobSlave::getWorkUnitValueInt(const char *prop, __int64 defVal) const
