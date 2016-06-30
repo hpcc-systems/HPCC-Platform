5286765ff5f2558743f4a3ae7519ca8106608b9c
Michael Gardner <michael.gardner@lexisnexis.com>
2016-06-30 13:55:04 +0000

    HPCC-15807 Added psmisc package to dependencies for thorslave restarts
    Signed-off-by: Michael Gardner <michael.gardner@lexisnexis.com>

--- cmake_modules/dependencies/wily.cmake
+++ cmake_modules/dependencies/wily.cmake
@@ -1,1 +1,1 @@
-SET_DEPENDENCIES ( CPACK_DEBIAN_PACKAGE_DEPENDS g++ openssh-client openssh-server expect rsync libapr1 python )
+SET_DEPENDENCIES ( CPACK_DEBIAN_PACKAGE_DEPENDS g++ openssh-client openssh-server expect rsync libapr1 python psmisc )
