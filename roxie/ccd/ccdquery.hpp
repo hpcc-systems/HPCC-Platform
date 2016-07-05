ad2150237a22a1fe5cdbf9f1949ec3db105ce32a
Richard Chapman <rchapman@hpccsystems.com>
2016-07-05 13:04:52 +0100

    HPCC-15871 Add a mode for standalone Roxie queries to use a local workunit
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- roxie/ccd/ccdquery.hpp
+++ roxie/ccd/ccdquery.hpp
@@ -304,7 +304,7 @@ extern IRecordLayoutTranslator *createRecordLayoutTranslator(const char *logical
 extern IQueryFactory *createServerQueryFactory(const char *id, const IQueryDll *dll, const IRoxiePackage &package, const IPropertyTree *stateInfo, bool isDynamic, bool forceRetry);
 extern IQueryFactory *createSlaveQueryFactory(const char *id, const IQueryDll *dll, const IRoxiePackage &package, unsigned _channelNo, const IPropertyTree *stateInfo, bool isDynamic, bool forceRetry);
 extern IQueryFactory *getQueryFactory(hash64_t hashvalue, unsigned channel);
-extern IQueryFactory *createServerQueryFactoryFromWu(IConstWorkUnit *wu);
+extern IQueryFactory *createServerQueryFactoryFromWu(IConstWorkUnit *wu, const IQueryDll *_dll);
 extern IQueryFactory *createSlaveQueryFactoryFromWu(IConstWorkUnit *wu, unsigned channelNo);
 extern unsigned checkWorkunitVersionConsistency(const IConstWorkUnit *wu );
 
