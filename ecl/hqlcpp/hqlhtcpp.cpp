16689ba447ed9e67a009e58c716b4d69ef53b28c
Shamser Ahmed <shamser.ahmed@lexisnexis.co.uk>
2016-08-24 17:45:28 +0100

    HPCC-15367 no_extractresults may prevent other optimizations
    Signed-off-by: Shamser Ahmed <shamser.ahmed@lexisnexis.co.uk>

--- ecl/hqlcpp/hqlhtcpp.cpp
+++ ecl/hqlcpp/hqlhtcpp.cpp
@@ -9376,6 +9376,10 @@ IHqlExpression * HqlCppTranslator::getResourcedGraph(IHqlExpression * expr, IHql
     traceExpression("AfterOptimize", resourced);
     checkNormalized(resourced);
 
+    resourced.setown(convertSetResultToExtract(resourced));
+    traceExpression("After ConvertSetResultToExtract", resourced);
+    checkNormalized(resourced);
+
     if (true)
         resourced.setown(optimizeCompoundSource(resourced, CSFpreload|csfFlags));
 
@@ -18836,6 +18840,19 @@ void HqlCppTranslator::optimizePersists(HqlExprArray & exprs)
     }
     createCompoundEnsure(exprs, 0, max-1);
 }
+
+IHqlExpression * HqlCppTranslator::convertSetResultToExtract(IHqlExpression * expr)
+{
+    cycle_t startCycles = get_cycles_now();
+
+    SetResultToExtractTransformer transformer;
+
+    IHqlExpression * ret = transformer.transformRoot(expr);
+    noteFinishedTiming("compile:tree transform: convert SetResult to Extract", startCycles);
+
+    return ret;
+}
+
 
 IHqlExpression * HqlCppTranslator::extractGlobalCSE(IHqlExpression * expr)
 {
