6d2c0a5f853519ef4edb6a263b8bdfc571321b50
rpastrana <rodrigo.pastrana@lexisnexis.com>
2016-09-16 13:31:06 -0400

    HPCC-15375 DESDL Auto ESP feature auth
    - change attribute name.
    
    Signed-off-by: rpastrana <rodrigo.pastrana@lexisnexis.com>

--- tools/hidl/hidlcomp.h
+++ tools/hidl/hidlcomp.h
@@ -28,7 +28,7 @@
 #define HIDLVER "1.3"
 
 #define RETURNNAME "_return"
-#define FEATEACCESSATTRIBUTE "autofeatureauth"
+#define FEATEACCESSATTRIBUTE "auth_feature"
 
 #ifndef _WIN32
 #define stricmp strcasecmp
