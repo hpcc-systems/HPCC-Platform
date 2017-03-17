d509dc191a27311648ac0e1ef85eff1feec47f79
hezouat <zahir.hezouat@fircosoft>
2017-03-17 13:50:30 +0000

    HPCC-17227  removed Doxygen

--- plugins/sqs/doc/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sqshpcc_2ecpp',['sqshpcc.cpp',['../sqshpcc_8cpp.html',1,'']]]
-];
--- plugins/sqs/doc/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['notfound',['NOTFOUND',['../sqshpcc_8cpp.html#a6e8fc25e46611d236041d15956d9bb5f',1,'sqshpcc.cpp']]]
-];
