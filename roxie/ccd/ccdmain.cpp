0f3507efd6be3504a8d7fd58beccd540f267720d
Richard Chapman <rchapman@hpccsystems.com>
2017-06-05 11:44:11 +0100

    HPCC-17745 JPtree memory usage greater than intended
    Structure did not pack as intended.
    
    Restore "Waiting for queries" logging line to its previous form, in case
    external tools look for it.
    
    Use correct check for little-endian - prior one did not work on Windows, and
    would not have worked on big-endian machines.
    
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- roxie/ccd/ccdmain.cpp
+++ roxie/ccd/ccdmain.cpp
@@ -1201,7 +1201,8 @@ int STARTQUERY_API start_query(int argc, const char *argv[])
                 roxieServer->start();
             }
             writeSentinelFile(sentinelFile);
-            DBGLOG("Waiting for queries LPT=%u APT=%u", queryNumLocalTrees(), queryNumAtomTrees());
+            DBGLOG("Startup completed - LPT=%u APT=%u", queryNumLocalTrees(), queryNumAtomTrees());
+            DBGLOG("Waiting for queries");
             if (pingInterval)
                 startPingTimer();
             LocalIAbortHandler abortHandler(waiter);
