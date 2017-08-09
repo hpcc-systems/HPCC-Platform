456e85df70efb011e46d4399880213245da96f1e
David Skaff <david.skaff@lexisnexisrisk.com>
2017-08-09 11:14:05 -0400

    HPCC-18113 Unicode Implementation of EndsWith
    - Adds test cases for EndsWith in ecllibrary/teststd/uni/TestEndsWith.ecl
    - Adds documentation for EndsWith in ecllibrary/std/uni/Uni.ecl
    - Adds code for EndsWith in unicodelib.cpp
    
    Signed-off-by: David Skaff <david.skaff@lexisnexisrisk.com>

--- plugins/unicodelib/unicodelib.hpp
+++ plugins/unicodelib/unicodelib.hpp
@@ -105,6 +105,7 @@ UNICODELIB_API void UNICODELIB_CALL ulUnicodeLocaleExcludeNthWord(unsigned & tgt
 UNICODELIB_API void UNICODELIB_CALL ulUnicodeLocaleExcludeLastWord(unsigned & tgtLen, UChar * & tgt, unsigned textLen, UChar const * text, char const * localename);
 UNICODELIB_API void UNICODELIB_CALL ulUnicodeLocaleTranslate(unsigned & tgtLen, UChar * & tgt, unsigned textLen, UChar const * text, unsigned searLen, UChar const * sear, unsigned replLen, UChar * repl);
 UNICODELIB_API bool UNICODELIB_CALL ulUnicodeLocaleStartsWith(unsigned srcLen, UChar const * src, unsigned prefLen, UChar const * pref, unsigned formLen, char const * form);
+UNICODELIB_API bool UNICODELIB_CALL ulUnicodeLocaleEndsWith(unsigned srcLen, UChar const * src, unsigned suffLen, UChar const * suff, unsigned formLen, char const * form);
 }
 
 #endif
