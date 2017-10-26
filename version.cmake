4d0fe48d85f69b1a32a5fd100557f634cb4f991b
Richard Chapman <rchapman@hpccsystems.com>
2017-10-26 09:11:14 +0100

    Split off candidate-6.4.6
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -5,7 +5,7 @@ set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 6 )
 
 set ( HPCC_MINOR 4 )
-set ( HPCC_POINT 4 )
-set ( HPCC_MATURITY "rc" )
-set ( HPCC_SEQUENCE 2)
+set ( HPCC_POINT 5 )
+set ( HPCC_MATURITY "closedown" )
+set ( HPCC_SEQUENCE 0)
 ###
