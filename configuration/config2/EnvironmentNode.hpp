0550ad53ae18fa280878b98dae2f4ed5a3a801dd
Ken Rowland <kenneth.rowland@lexisnexisrisk.com>
2018-01-09 13:58:39 -0500

    HPCC-17851 New config manager core library
    Adds the core library for the new configuration manager.
    
    Signed-off-by: Ken Rowland <kenneth.rowland@lexisnexisrisk.com>

---
+++ configuration/config2/EnvironmentNode.hpp
@@ -0,0 +1,80 @@
+/*##############################################################################
+
+    HPCC SYSTEMS software Copyright (C) 2017 HPCC Systems®.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+############################################################################## */
+
+#ifndef _CONFIG2_ENVIRONMENTNODE_HPP_
+#define _CONFIG2_ENVIRONMENTNODE_HPP_
+
+#include <memory>
+#include <map>
+#include "SchemaItem.hpp"
+#include "EnvironmentValue.hpp"
+#include "SchemaValue.hpp"
+#include "Status.hpp"
+#include "NameValue.hpp"
+#include "platform.h"
+
+
+class DECL_EXPORT EnvironmentNode : public std::enable_shared_from_this<EnvironmentNode>
+{
+    public:
+
+        EnvironmentNode(const std::shared_ptr<SchemaItem> &pCfgItem, const std::string &elemName, const std::shared_ptr<EnvironmentNode> &pParent = nullptr) :
+            m_pSchemaItem(pCfgItem), m_name(elemName), m_pParent(pParent) { }
+        ~EnvironmentNode() { }
+        const std::string &getName() const { return m_name;  }
+        void addChild(std::shared_ptr<EnvironmentNode> pNode);
+        bool removeChild(std::shared_ptr<EnvironmentNode> pNode);
+        void getChildren(std::vector<std::shared_ptr<EnvironmentNode>> &children, const std::string &name="") const;
+        bool hasChildren() const { return m_children.size() != 0; }
+        int getNumChildren() const { return m_children.size(); }
+        std::shared_ptr<EnvironmentNode> getParent() const;
+        bool addAttribute(const std::string &name, std::shared_ptr<EnvironmentValue> pValue);
+        void setAttributeValues(const std::vector<NameValue> &values, Status &status, bool allowInvalid, bool forceCreate);
+        void setAttributeValue(const std::string &name, const std::string &value, Status &status, bool allowInvalid=false, bool forceCreate=false);   // candidate for a variant?
+        std::string getAttributeValue(const std::string &name) const;                                  // candidate for a variant?
+        void addMissingAttributesFromConfig();
+        bool setLocalValue(const std::string &newValue, Status &status, bool force = false);
+        std::string getLocalValue() const;
+        void setLocalEnvValue(const std::shared_ptr<EnvironmentValue> &pEnvValue) { m_pLocalValue = pEnvValue;  }
+        const std::shared_ptr<EnvironmentValue> &getLocalEnvValue() const { return m_pLocalValue;  }
+        bool isLocalValueSet() const { return m_pLocalValue != nullptr; }
+        void getAttributes(std::vector<std::shared_ptr<EnvironmentValue>> &attrs) const;
+        const std::shared_ptr<EnvironmentValue> getAttribute(const std::string &name) const;
+        bool hasAttribute(const std::string &name) const { return m_attributes.find(name) != m_attributes.end();  }
+        bool hasAttributes() const { return m_attributes.size() != 0; }
+        void setId(const std::string &id) { m_id = id; }
+        const std::string &getId() const { return m_id;  }
+        void validate(Status &status, bool includeChildren=false) const;
+        void getAttributeValueForAllSiblings(const std::string &attrName, std::vector<std::string> &result) const;
+        const std::shared_ptr<SchemaItem> &getSchemaItem() const { return m_pSchemaItem; }
+        void getInsertableItems(std::vector<std::shared_ptr<SchemaItem>> &items) const;
+        void initialize();
+
+
+    protected:
+
+        std::string m_name;
+        std::shared_ptr<SchemaItem> m_pSchemaItem;
+        std::weak_ptr<EnvironmentNode> m_pParent;
+        std::multimap<std::string, std::shared_ptr<EnvironmentNode>> m_children;
+        std::shared_ptr<EnvironmentValue> m_pLocalValue;   // not normal because values usually in attributes
+        std::map<std::string, std::shared_ptr<EnvironmentValue>> m_attributes;
+        std::string m_id;
+};
+
+
+#endif
---
+++ configuration/config2/SchemaTypeLimits.hpp
@@ -0,0 +1,66 @@
+/*##############################################################################
+
+    HPCC SYSTEMS software Copyright (C) 2017 HPCC Systems®.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+############################################################################## */
+
+#ifndef _CONFIG2_CFGLIMITS_HPP_
+#define _CONFIG2_CFGLIMITS_HPP_
+
+#include <memory>
+#include <vector>
+#include <string>
+#include "platform.h"
+
+
+class EnvironmentValue;
+
+struct DECL_EXPORT AllowedValue
+{
+    AllowedValue(const std::string &value, const std::string &desc="") : m_value(value), m_description(desc) { }
+    std::string m_value;
+    std::string m_description;
+};
+
+class DECL_EXPORT SchemaTypeLimits
+{
+    public:
+
+        SchemaTypeLimits() { }
+        virtual ~SchemaTypeLimits() { }
+        void addAllowedValue(const std::string &value, const std::string &desc="") { m_enumeratedValues.push_back(AllowedValue(value, desc)); }
+        std::vector<AllowedValue> getEnumeratedValues() const;
+        bool isEnumerated() const { return !m_enumeratedValues.empty(); }
+        bool isValueValid(const std::string &testValue) const;
+        virtual std::string getLimitString() const { return "No value limits"; }
+        virtual bool isMaxSet() const { return false; }
+        virtual bool isMinSet() const { return false; }
+        virtual int getMax() const { return 0; }
+        virtual int getMin() const { return 0; }
+
+
+    protected:
+
+        virtual bool isValidEnumeratedValue(const std::string &testValue) const;
+        virtual bool doValueTest(const std::string &testValue) const { return true;  }
+
+
+    protected:
+
+        std::vector<AllowedValue> m_enumeratedValues;
+};
+
+
+
+#endif // _CONFIG2_CFGLIMITS_HPP_
