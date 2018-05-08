b5d2b960547565c087487a0129ed8630abb40529
Shamser Ahmed <shamser.ahmed@lexisnexis.co.uk>
2018-05-08 11:56:50 +0100

    HPCC-19639 Update WUDetails regression with schema changes
    Signed-off-by: Shamser Ahmed <shamser.ahmed@lexisnexis.co.uk>

--- testing/esp/wudetails/wutest.py
+++ testing/esp/wudetails/wutest.py
@@ -218,14 +218,14 @@ testCase = namedtuple('testCase', ['ScopeFilter',
 
 #scopeFilter(MaxDepth='999', Scopes=set(), Ids=set(), ScopeTypes=set()),
 #nestedFilter(Depth='999', ScopeTypes=set()),
-#propertiesToReturn(AllStatistics='1', AllAttributes='1', AllHints='1', MinVersion='0', Measure='', Properties=set(), ExtraProperties=set()),
+#propertiesToReturn(AllStatistics='1', AllProperties='1', AllHints='1', MinVersion='0', Measure='', Properties=set(), ExtraProperties=set()),
 #scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
 #propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeFormatted='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
 testCases = [ 
              testCase(
                  scopeFilter(MaxDepth='999'),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1', AllHints='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1', AllHints='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeFormatted='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
@@ -239,7 +239,7 @@ testCases = [
              testCase(
                  scopeFilter(MaxDepth='999'),
                  nestedFilter(),
-                 propertiesToReturn(AllAttributes='1'),
+                 propertiesToReturn(AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
@@ -253,14 +253,14 @@ testCases = [
              testCase(
                  scopeFilter(MaxDepth='999', Scopes={'Scope':'graph1'}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1', AllHints='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1', AllHints='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
              testCase(
                  scopeFilter(MaxDepth='1', ScopeTypes={'ScopeType':'graph'}),
                  nestedFilter(Depth='1'),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1', AllHints='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1', AllHints='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
@@ -295,14 +295,14 @@ testCases = [
              testCase(
                  scopeFilter(MaxDepth='2'),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions()
              ),
              testCase(
                  scopeFilter(MaxDepth='1'),
                  nestedFilter(Depth='1'),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(),
                  propertyOptions()
              ),
@@ -330,70 +330,70 @@ testCases = [
              testCase(
                  scopeFilter(MaxDepth='999', ScopeTypes={'ScopeType':'subgraph'}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1'),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
              testCase(
                  scopeFilter(MaxDepth='999', ScopeTypes={'ScopeType':'subgraph'}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeScope='1'),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
              testCase(
                  scopeFilter(MaxDepth='999', ScopeTypes={'ScopeType':'subgraph'}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeId='1'),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
              testCase(
                  scopeFilter(MaxDepth='999', ScopeTypes={'ScopeType':'subgraph'}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
              testCase(
                  scopeFilter(MaxDepth='999', ScopeTypes={'ScopeType':'subgraph'}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeName='0')
              ),
              testCase(
                  scopeFilter(MaxDepth='999', ScopeTypes={'ScopeType':'subgraph'}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeRawValue='1')
              ),
              testCase(
                  scopeFilter(MaxDepth='999', ScopeTypes={'ScopeType':'subgraph'}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeMeasure='0')
              ),
              testCase(
                  scopeFilter(MaxDepth='999', ScopeTypes={'ScopeType':'subgraph'}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeCreator='0')
              ),
              testCase(
                  scopeFilter(MaxDepth='999', ScopeTypes={'ScopeType':'subgraph'}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeCreatorType='0')
              ),
              testCase(
                  scopeFilter(MaxDepth='2', Scopes={'Scope':'graph1:sg1'}),
                  nestedFilter(Depth=0),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
@@ -407,28 +407,28 @@ testCases = [
              testCase(
                  scopeFilter(MaxDepth='999', PropertyFilters={'PropertyFilter':{'Name':'NumRowsProcessed','MinValue':'10000','MaxValue':'20000'}}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
              testCase(
                  scopeFilter(MaxDepth='999', PropertyFilters={'PropertyFilter':{'Name':'NumIndexSeeks','MaxValue':'3'}}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
              testCase(
                  scopeFilter(MaxDepth='999', PropertyFilters={'PropertyFilter':{'Name':'NumIndexSeeks','ExactValue':'4'}}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
              testCase(
                  scopeFilter(MaxDepth='999', PropertyFilters={'PropertyFilter':[{'Name':'NumIndexSeeks','ExactValue':'4'},{'Name':'NumAllocations','MinValue':'5','MaxValue':'10'}]}),
                  nestedFilter(),
-                 propertiesToReturn(AllStatistics='1', AllAttributes='1'),
+                 propertiesToReturn(AllStatistics='1', AllProperties='1'),
                  scopeOptions(IncludeMatchedScopesInResults='1', IncludeScope='1', IncludeId='1', IncludeScopeType='1'),
                  propertyOptions(IncludeName='1', IncludeRawValue='1', IncludeMeasure='1', IncludeCreator='1', IncludeCreatorType='1')
              ),
