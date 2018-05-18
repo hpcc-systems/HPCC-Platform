fdb4e1bc63ea5d93a370033a83ca89aa415f1881
Richard Chapman <rchapman@hpccsystems.com>
2018-05-18 17:51:49 +0100

    Community Edition 6.2.28-rc1 Release Candidate 1
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -5,6 +5,6 @@ set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 6 )
 set ( HPCC_MINOR 2 )
 set ( HPCC_POINT 28 )
-set ( HPCC_MATURITY "closedown" )
-set ( HPCC_SEQUENCE 0 )
+set ( HPCC_MATURITY "rc" )
+set ( HPCC_SEQUENCE 1 )
 ###
