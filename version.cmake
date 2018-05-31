4c9becc48dbcb34d32a79e8cbc82b5a7de24ee51
Richard Chapman <rchapman@hpccsystems.com>
2018-05-31 13:57:29 +0100

    Split off 6.2.30
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -4,7 +4,7 @@
 set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 6 )
 set ( HPCC_MINOR 2 )
-set ( HPCC_POINT 28 )
-set ( HPCC_MATURITY "release" )
+set ( HPCC_POINT 30 )
+set ( HPCC_MATURITY "rc" )
 set ( HPCC_SEQUENCE 1 )
 ###
