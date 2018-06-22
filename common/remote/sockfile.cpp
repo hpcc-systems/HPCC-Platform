694a0e4b64ed047e2d0e5e65c61282fe0f3a334c
Jake Smith <jake.smith@lexisnexisrisk.com>
2018-06-22 17:06:52 +0100

    HPCC-19977 Create correct output type for index count
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

--- common/remote/sockfile.cpp
+++ common/remote/sockfile.cpp
@@ -4345,6 +4345,12 @@ IRemoteActivity *createRemoteIndexRead(IPropertyTree &actNode)
 }
 
 
+// create a { unsigned8 } output meta for the count
+static const RtlIntTypeInfo indexCountFieldType(type_unsigned|type_int, 8);
+static const RtlFieldStrInfo indexCountField("count", nullptr, &indexCountFieldType);
+static const RtlFieldInfo * const indexCountFields[2] = { &indexCountField, nullptr };
+static const RtlRecordTypeInfo indexCountRecord(type_record, 2, indexCountFields);
+
 class CRemoteIndexCountActivity : public CRemoteIndexBaseActivity
 {
     typedef CRemoteIndexBaseActivity PARENT;
@@ -4356,9 +4362,9 @@ public:
     {
         rowLimit = config.getPropInt64("chooseN");
 
-        // create helper
         inMeta.setown(getTypeInfoOutputMetaData(config, "input", false));
-        outMeta.set(inMeta);
+        outMeta.setown(new CDynamicOutputMetaData(indexCountRecord));
+
         initCommon(config);
     }
 // IRemoteActivity impl.
