3fb7550536b8e73b46561cc45efcb790c616e365
samindaw <samindaw@gmail.com>
2018-06-27 12:51:21 -0400

    braces in new line + update in mpbase.cpp to support MPI imeplementation + use original mpbase.cpp instead of mpbase_mpi.cpp

--- system/mp/mpcomm_mpi.cpp
+++ system/mp/mpcomm_mpi.cpp
@@ -78,53 +78,64 @@
 
 #define _TRACING
 
-class NodeCommunicator: public ICommunicator, public CInterface{
+class NodeCommunicator: public ICommunicator, public CInterface
+{
 private:
-    class SelfMessage{
+    class SelfMessage
+    {
         mptag_t mptag;
         CMessageBuffer* message = NULL;
     public:
         SelfMessage(mptag_t _tag, CMessageBuffer* _message): mptag(_tag), message(_message){};
-        bool isTag(mptag_t _tag){
+        bool isTag(mptag_t _tag)
+        {
             return mptag == _tag;
         }
-        CMessageBuffer *getMessage(){
+        CMessageBuffer *getMessage()
+        {
             return message;
         }
-        mptag_t tag(){
+        mptag_t tag()
+        {
             return mptag;
         }
 
-        ~SelfMessage(){
+        ~SelfMessage()
+        {
             if (message){
                 delete message;
             }
         }
     };
-    NodeGroup *group;
-    bool outer;
+    IGroup *group;
     rank_t myrank;
+    MPI_Comm comm;
     std::vector<SelfMessage*> selfMessages;
     CriticalSection selfMessagesLock;
 private:
-    void addSelfMessage(SelfMessage *selfMessage){
+    void addSelfMessage(SelfMessage *selfMessage)
+    {
         _TF("addSelfMessage", selfMessage->tag());
         CriticalBlock block(selfMessagesLock);
         selfMessages.push_back(selfMessage);
     }
 
-    SelfMessage *popSelfMessage(mptag_t tag){
+    SelfMessage *popSelfMessage(mptag_t tag)
+    {
         _TF("popSelfMessage", tag);
         CriticalBlock block(selfMessagesLock);
         int index = -1;
-        for(int i=0; i< selfMessages.size(); i++){
-            if (selfMessages[i]->isTag(tag)){
+        for(int i=0; i< selfMessages.size(); i++)
+        {
+            if (selfMessages[i]->isTag(tag))
+            {
                 index = i;
                 break;
             }
         }
         SelfMessage *ret = NULL;
-        if (index != -1){
+        if (index != -1)
+        {
             ret = selfMessages[index];
             selfMessages.erase(selfMessages.begin()+index);
         }
@@ -134,22 +145,28 @@ private:
 public:
     IMPLEMENT_IINTERFACE;
 
-    bool send(CMessageBuffer &mbuf, rank_t dstrank, mptag_t tag, unsigned timeout){ 
+    bool send(CMessageBuffer &mbuf, rank_t dstrank, mptag_t tag, unsigned timeout)
+    {
         _TF("send", dstrank, tag, timeout);
         assertex(dstrank!=RANK_NULL);
         CTimeMon tm(timeout);
         rank_t myrank = group->rank();
         rank_t startrank = dstrank;
         rank_t endrank;
-        if (dstrank==RANK_ALL || dstrank==RANK_ALL_OTHER) {
+        if (dstrank==RANK_ALL || dstrank==RANK_ALL_OTHER)
+        {
             startrank = 0;
             endrank = group->ordinality()-1;
-        } else if (dstrank==RANK_RANDOM) {
-            if (group->ordinality()>1) {
-                do {
+        } else if (dstrank==RANK_RANDOM)
+        {
+            if (group->ordinality()>1)
+            {
+                do
+                {
                     startrank = getRandom()%group->ordinality();
                 } while (startrank==myrank);
-            } else {
+            } else
+            {
                 assertex(myrank!=0);
                 startrank = 0;
             }
@@ -157,21 +174,21 @@ public:
         } else {
             endrank = startrank;
         }
-        for (;startrank<=endrank;startrank++) {
-            if (startrank==myrank) {
-                if (dstrank !=RANK_ALL_OTHER){
+        for (;startrank<=endrank;startrank++)
+        {
+            if (startrank==myrank)
+            {
+                if (dstrank !=RANK_ALL_OTHER)
+                {
                     CMessageBuffer *ret = mbuf.clone();
                     addSelfMessage(new SelfMessage(tag, ret));
-//                    SelfMessage *msg = popSelfMessage(tag);
-//                    mptag_t reply = msg->getMessage()->getReplyTag();
-//                    _T("Reply tag of the message adding = "<<reply);
-//                    addSelfMessage(msg);
                 }
-            }else{
+            }else
+            {
                 unsigned remaining;
                 if (tm.timedout(&remaining))
                     return false;
-                hpcc_mpi::CommStatus status = hpcc_mpi::sendData(startrank, tag, mbuf, (*group)(), remaining);
+                hpcc_mpi::CommStatus status = hpcc_mpi::sendData(startrank, tag, mbuf, comm, remaining);
                 if (status != hpcc_mpi::CommStatus::SUCCESS)
                     return false;
             }
@@ -180,22 +197,25 @@ public:
         return true;
     }
 
-    bool recv(CMessageBuffer &mbuf, rank_t srcrank, mptag_t tag, rank_t *sender, unsigned timeout=MP_WAIT_FOREVER){
+    bool recv(CMessageBuffer &mbuf, rank_t srcrank, mptag_t tag, rank_t *sender, unsigned timeout=MP_WAIT_FOREVER)
+    {
         _TF("recv", srcrank, tag, timeout);
         CTimeMon tm(timeout);
         unsigned remaining;
         bool messageFromSelf = (srcrank == group->rank());
         bool completed = false;
 
-        if (messageFromSelf || srcrank == RANK_ALL){
+        if (messageFromSelf || srcrank == RANK_ALL)
+        {
             SelfMessage *msg = NULL;
-            while (msg == NULL && !tm.timedout(&remaining)){
+            while (msg == NULL && !tm.timedout(&remaining))
+            {
                 msg = popSelfMessage(tag);
-                if (!messageFromSelf){
+                if (!messageFromSelf)
                     break; //if its not specifically from self, we should continue checking from other nodes
-                }
             }
-            if (msg){
+            if (msg)
+            {
                 _T("Message found in self message list");
                 mbuf.clear();
                 _T("Buffer cleared");
@@ -211,37 +231,44 @@ public:
                 completed = true;
             }
         }
-        if (!messageFromSelf){
+        if (!messageFromSelf)
+        {
             tm.timedout(&remaining);
-            hpcc_mpi::CommStatus status = hpcc_mpi::readData(srcrank, tag, mbuf, (*group)(), remaining);
+            hpcc_mpi::CommStatus status = hpcc_mpi::readData(srcrank, tag, mbuf, comm, remaining);
             _T("recv status="<<status);
             completed = (status == hpcc_mpi::CommStatus::SUCCESS);
             //TODO what if no message received and selfMsg bcomes available now?
         }
-        if (completed){
+        if (completed)
+        {
             mbuf.reset();
         }
         return completed;
     }
     
-    void barrier(void){
-        hpcc_mpi::barrier((*group)());
+    void barrier(void)
+    {
+        hpcc_mpi::barrier(comm);
     }
 
-    unsigned probe(rank_t srcrank, mptag_t tag, rank_t *sender, unsigned timeout=0){
+    unsigned probe(rank_t srcrank, mptag_t tag, rank_t *sender, unsigned timeout=0)
+    {
         _TF("probe", srcrank, tag, timeout);
-        if (hpcc_mpi::hasIncomingMessage(srcrank, tag, (*group)())){
+        if (hpcc_mpi::hasIncomingMessage(srcrank, tag, comm))
+        {
             if (sender)
                 *sender = srcrank;
         }
         return false;
     }
     
-    void flush(mptag_t tag){
+    void flush(mptag_t tag)
+    {
         // Handled by MPI
     }
 
-    bool sendRecv(CMessageBuffer &mbuff, rank_t sendrank, mptag_t sendtag, unsigned timeout=MP_WAIT_FOREVER){
+    bool sendRecv(CMessageBuffer &mbuff, rank_t sendrank, mptag_t sendtag, unsigned timeout=MP_WAIT_FOREVER)
+    {
         _TF("sendRecv", sendrank, sendtag, timeout);
         //TODO share timeout between send/recv?
         mptag_t replytag = createReplyTag();
@@ -256,12 +283,15 @@ public:
         return recv(mbuff,sendrank,replytag,NULL,remaining);
     }
 
-    bool reply(CMessageBuffer &mbuf, unsigned timeout=MP_WAIT_FOREVER){
+    bool reply(CMessageBuffer &mbuf, unsigned timeout=MP_WAIT_FOREVER)
+    {
         _TF("reply", mbuf.getReplyTag(), timeout);
         mptag_t replytag = mbuf.getReplyTag();
         rank_t dstrank = group->rank(mbuf.getSender());
-        if (dstrank!=RANK_NULL) {
-            if (send (mbuf, dstrank, replytag,timeout)) {
+        if (dstrank!=RANK_NULL)
+        {
+            if (send (mbuf, dstrank, replytag,timeout))
+            {
                 mbuf.setReplyTag(TAG_NULL);
                 return true;
             }
@@ -270,41 +300,52 @@ public:
         return false;
     }
 
-    void cancel(rank_t srcrank, mptag_t tag){
+    void cancel(rank_t srcrank, mptag_t tag)
+    {
         _TF("recv", srcrank, tag);
-        assertex(srcrank!=RANK_NULL);       
-        std::vector<hpcc_mpi::CommRequest> req = group->getCommRequest(srcrank, tag);
-        if (req.size() > 0){
-            for(int i=0;i<req.size();i++) hpcc_mpi::cancelComm(req[i]);
-        }
+        //TODO
+        UNIMPLEMENTED;
+//        assertex(srcrank!=RANK_NULL);
+//        std::vector<hpcc_mpi::CommRequest> req = group->getCommRequest(srcrank, tag);
+//        if (req.size() > 0){
+//            for(int i=0;i<req.size();i++) hpcc_mpi::cancelComm(req[i]);
+//        }
     }
 
-    bool verifyConnection(rank_t rank,  unsigned timeout){
+    bool verifyConnection(rank_t rank,  unsigned timeout)
+    {
         UNIMPLEMENTED;
     }
 
-    bool verifyAll(bool duplex, unsigned timeout){
+    bool verifyAll(bool duplex, unsigned timeout)
+    {
         UNIMPLEMENTED;
     }
     
-    void disconnect(INode *node){
+    void disconnect(INode *node)
+    {
         UNIMPLEMENTED;
     }
 
-    IGroup &queryGroup() { 
+    IGroup &queryGroup()
+    {
         return *group;
     }
     
-    IGroup *getGroup()  { 
+    IGroup *getGroup()
+    {
         return group;
     }
 
-    NodeCommunicator(IGroup *_group){
-        //TODO throw meaningful exception if _group is not NodeGroup type
-        this->group = (NodeGroup*) _group;
+    NodeCommunicator(IGroup *_group, MPI_Comm _comm)
+    {
+        this->group = _group;
+        this->comm = _comm;
+        myrank = this->group->rank();
     }
     
-    ~NodeCommunicator(){
+    ~NodeCommunicator()
+    {
     }
 
 };
@@ -312,43 +353,52 @@ public:
 
 ///////////////////////////////////
 
-IMPServer *startNewMPServer(unsigned port){
+IMPServer *startNewMPServer(unsigned port)
+{
     UNIMPLEMENTED;
 }
 
 
-MODULE_INIT(INIT_PRIORITY_STANDARD){
+MODULE_INIT(INIT_PRIORITY_STANDARD)
+{
     return true;
 }
 
-MODULE_EXIT(){
+MODULE_EXIT()
+{
   
 }
 
-void startMPServer(unsigned port, bool paused){
+void startMPServer(unsigned port, bool paused)
+{
     hpcc_mpi::initialize(true);
 }
 
-void stopMPServer(){
+void stopMPServer()
+{
     hpcc_mpi::finalize();
 }
 
-bool hasMPServerStarted(){
+bool hasMPServerStarted()
+{
     UNIMPLEMENTED;
 }
 CriticalSection replyTagSect;
 byte RTsalt = 0xff;
 int rettag;
-mptag_t createReplyTag(){
+mptag_t createReplyTag()
+{
     UNIMPLEMENTED;
     mptag_t ret;
     {
         CriticalBlock block(replyTagSect);
-        if (RTsalt==0xff) {
+        if (RTsalt==0xff)
+        {
             RTsalt = (byte)(getRandom()%16);
             rettag = (int)TAG_REPLY_BASE-RTsalt;
         }
-        if (rettag>(int)TAG_REPLY_BASE) {           // wrapped
+        if (rettag>(int)TAG_REPLY_BASE)
+        {           // wrapped
             rettag = (int)TAG_REPLY_BASE-RTsalt;
         }
         ret = (mptag_t)rettag;
@@ -359,34 +409,52 @@ mptag_t createReplyTag(){
     return ret;
 }
 
-ICommunicator *createCommunicator(IGroup *group, bool outer){
-    return new NodeCommunicator(group);
+ICommunicator *createCommunicator(IGroup *group, bool outer)
+{
+    int size = hpcc_mpi::size(MPI_COMM_WORLD);
+    int rank = hpcc_mpi::rank(MPI_COMM_WORLD);
+    INode* nodes[size];
+    for(int i=0; i<size; i++)
+    {
+        SocketEndpoint ep(i);
+        nodes[i] = createINode(ep);
+    }
+    initMyNode(rank);
+    IGroup* _group = createIGroup(size, nodes);
+    return new NodeCommunicator(_group, MPI_COMM_WORLD);
 }
 
-IInterCommunicator &queryWorldCommunicator(){
+IInterCommunicator &queryWorldCommunicator()
+{
     UNIMPLEMENTED;
 }
 
-StringBuffer &getReceiveQueueDetails(StringBuffer &buf){
+StringBuffer &getReceiveQueueDetails(StringBuffer &buf)
+{
     UNIMPLEMENTED;
 }
 
-void addMPConnectionMonitor(IConnectionMonitor *monitor){
+void addMPConnectionMonitor(IConnectionMonitor *monitor)
+{
     UNIMPLEMENTED;
 }
 
-void removeMPConnectionMonitor(IConnectionMonitor *monitor){
+void removeMPConnectionMonitor(IConnectionMonitor *monitor)
+{
     UNIMPLEMENTED;
 }
 
-IMPServer *getMPServer(){
+IMPServer *getMPServer()
+{
     UNIMPLEMENTED;
 }
 
-void registerSelfDestructChildProcess(HANDLE handle){
+void registerSelfDestructChildProcess(HANDLE handle)
+{
     UNIMPLEMENTED;
 }
 
-void unregisterSelfDestructChildProcess(HANDLE handle){
+void unregisterSelfDestructChildProcess(HANDLE handle)
+{
     UNIMPLEMENTED;
 }
--- system/mp/test/mptest_mpi.cpp
+++ system/mp/test/mptest_mpi.cpp
@@ -24,8 +24,10 @@ using namespace std;
 
 rank_t myrank;
 
-void printHelp(int argc, char** argv){
-    if (myrank == 0){
+void printHelp(int argc, char** argv)
+{
+    if (myrank == 0)
+    {
         printf("USAGE \n\t$ mpirun -np <# of procs> %s -t <test-type> <para_1...para_n>\n", argv[0]);
         printf("\n");
         printf("-t <test-type>\t Select the type of test to run. Avaialable tests are the following:\n");
@@ -48,7 +50,8 @@ void printHelp(int argc, char** argv){
 }
 
 //--- Rank Test --//
-void TEST_rank(ICommunicator* comm){
+void TEST_rank(ICommunicator* comm)
+{
     IGroup *group = comm->getGroup();
     assertex(group->rank() >= 0);
     assertex(group->ordinality() > 1); //at least 2 processors
@@ -56,19 +59,22 @@ void TEST_rank(ICommunicator* comm){
     PrintLog("Hello from %d. Total of %d nodes.",group->rank(), group->ordinality());
 }
 
-void TEST_single_send(ICommunicator* comm){
+void TEST_single_send(ICommunicator* comm)
+{
     IGroup* group = comm->getGroup();
     int expected_msg = 42;
     int received_msg;
     CMessageBuffer testMsg;
     assertex(group->ordinality() > 1);
     
-    if (group->rank() == 0){
+    if (group->rank() == 0)
+    {
         rank_t target = 1;
         testMsg.append(expected_msg);
         comm->send(testMsg, target, MPTAG_TEST);
     }
-    if (group->rank() == 1){
+    if (group->rank() == 1)
+    {
         rank_t source = 0;
         bool success = comm->recv(testMsg, source, MPTAG_TEST, NULL);
         assertex(success == true);
@@ -78,7 +84,8 @@ void TEST_single_send(ICommunicator* comm){
     }
 }
 
-void TEST_right_shift(ICommunicator* comm){
+void TEST_right_shift(ICommunicator* comm)
+{
     IGroup* group = comm->getGroup();
     rank_t p = group->ordinality();
     rank_t rank = group->rank();
@@ -97,19 +104,22 @@ void TEST_right_shift(ICommunicator* comm){
     PrintLog("Message received from node %d to node %d.", source_rank, rank);
 }
 
-void TEST_receive_from_any(ICommunicator* comm, rank_t nodeRank){
+void TEST_receive_from_any(ICommunicator* comm, rank_t nodeRank)
+{
     IGroup* group = comm->getGroup();
     rank_t p = group->ordinality();
     rank_t rank = group->rank();
     rank_t destinationRank = (p-1);
     double expectedValue = 1234.0;
-    if (rank == nodeRank){
+    if (rank == nodeRank)
+    {
         CMessageBuffer sendMsg;
         sendMsg.append(expectedValue); 
         comm->send(sendMsg, destinationRank, MPTAG_TEST);
         PrintLog("Message sent by node %d to node %d.", rank, destinationRank);
     }
-    if (rank == destinationRank){
+    if (rank == destinationRank)
+    {
         CMessageBuffer recvMsg;
         comm->recv(recvMsg, RANK_ALL, MPTAG_TEST);
         double receivedValue;
@@ -120,13 +130,15 @@ void TEST_receive_from_any(ICommunicator* comm, rank_t nodeRank){
     }
 }
 
-void TEST_one_to_all(ICommunicator* comm, rank_t nodeRank){
+void TEST_one_to_all(ICommunicator* comm, rank_t nodeRank)
+{
     IGroup* group = comm->getGroup();
     rank_t p = group->ordinality();
     rank_t rank = group->rank();
     double expectedValue = 1234.0;
     double receivedValue;
-    if (rank == nodeRank){
+    if (rank == nodeRank)
+    {
         CMessageBuffer sendMsg;
         sendMsg.append(expectedValue);        
         comm->send(sendMsg, RANK_ALL, MPTAG_TEST);
@@ -138,7 +150,8 @@ void TEST_one_to_all(ICommunicator* comm, rank_t nodeRank){
     PrintLog("Message received from node %d to node %d.", nodeRank, rank);
 }
 
-void TEST_one_from_all(ICommunicator* comm, rank_t nodeRank){
+void TEST_one_from_all(ICommunicator* comm, rank_t nodeRank)
+{
     IGroup* group = comm->getGroup();
     rank_t p = group->ordinality();
     rank_t rank = group->rank();
@@ -149,8 +162,10 @@ void TEST_one_from_all(ICommunicator* comm, rank_t nodeRank){
     sendMsg.append(expectedValue); 
     comm->send(sendMsg, nodeRank, MPTAG_TEST);
     
-    if (rank == nodeRank){
-        for(int i=0; i<p; i++){
+    if (rank == nodeRank)
+    {
+        for(int i=0; i<p; i++)
+        {
             CMessageBuffer recvMsg;
             comm->recv(recvMsg, i, MPTAG_TEST, NULL);
             double receivedValue;
@@ -166,7 +181,8 @@ void TEST_one_from_all(ICommunicator* comm, rank_t nodeRank){
 CriticalSection sendCriticalSec;
 CriticalSection recvCriticalSec;
 CriticalSection validateCriticalSec;
-int getNextCount(CriticalSection &sect, int &count){
+int getNextCount(CriticalSection &sect, int &count)
+{
     CriticalBlock block(sect);
     if (count)
         return count--;
@@ -174,17 +190,21 @@ int getNextCount(CriticalSection &sect, int &count){
         return 0;
 }
 
-void TEST_MT_simple_send_recv(ICommunicator* comm){
+void TEST_MT_simple_send_recv(ICommunicator* comm)
+{
     assertex(comm->getGroup()->ordinality()>1);
     rank_t rank = comm->getGroup()->rank();
     // nodes ranked 0 and 1 will be conducting this test
-    if (rank<2){
-        class SWorker: public Thread{
+    if (rank<2)
+    {
+        class SWorker: public Thread
+        {
         private:
             ICommunicator* comm;
         public:
             SWorker(ICommunicator* _comm):comm(_comm){}
-            int run(){
+            int run()
+            {
                 IGroup *group = comm->getGroup();
                 rank_t p = group->ordinality();
                 rank_t rank = group->rank();
@@ -198,19 +218,22 @@ void TEST_MT_simple_send_recv(ICommunicator* comm){
                 return 1;
             }
         } s(comm);
-        class RWorker: public Thread{
+        class RWorker: public Thread
+        {
         private:
             ICommunicator* comm;
         public:
             RWorker(ICommunicator* _comm):comm(_comm){}
-            int run(){
+            int run()
+            {
                 IGroup *group = comm->getGroup();
                 rank_t p = group->ordinality();
                 rank_t rank = group->rank();
                 rank_t source_rank = 1 - rank;
 
                 CMessageBuffer recvMsg;
-                if (comm->recv(recvMsg, source_rank, MPTAG_TEST, NULL, 100)){
+                if (comm->recv(recvMsg, source_rank, MPTAG_TEST, NULL, 100))
+                {
                     int received_msg;
                     recvMsg.read(received_msg);
                     //TODO validate received messge
@@ -227,7 +250,8 @@ void TEST_MT_simple_send_recv(ICommunicator* comm){
 bool* validate;
 
 //validate that numbers from 1 to maxCounter are received only once
-void setValidate(int i, int maxCounter){
+void setValidate(int i, int maxCounter)
+{
     CriticalBlock block(validateCriticalSec);
     assertex(i>0);
     assertex(i<=maxCounter);
@@ -235,23 +259,27 @@ void setValidate(int i, int maxCounter){
     validate[i-1] = true;
 }
 
-void TEST_MT_send_recv(ICommunicator* comm, int counter){
+void TEST_MT_send_recv(ICommunicator* comm, int counter)
+{
     assertex(comm->getGroup()->ordinality()>1);
     int SEND_THREADS, RECV_THREADS;
     SEND_THREADS = RECV_THREADS = 8;
     rank_t rank = comm->getGroup()->rank();
 
     // nodes ranked 0 and 1 will be conducting this test
-    if (rank<2){
+    if (rank<2)
+    {
         validate = new bool[counter];
         for(int i=0; i<counter; i++) validate[i] = false;
-        class SWorker: public Thread{
+        class SWorker: public Thread
+        {
         private:
             ICommunicator* comm;
             int* counter;
         public:
             SWorker(ICommunicator* _comm, int* _counter):comm(_comm), counter(_counter){}
-            int run(){
+            int run()
+            {
                 IGroup *group = comm->getGroup();
                 rank_t p = group->ordinality();
                 rank_t rank = group->rank();
@@ -259,14 +287,17 @@ void TEST_MT_send_recv(ICommunicator* comm, int counter){
 
                 CMessageBuffer sendMsg;
                 int served = 0;
-                while(true){
+                while(true)
+                {
                     sendMsg.clear();
                     int v = getNextCount(sendCriticalSec, *counter);
-                    if (v > 0){
+                    if (v > 0)
+                    {
                         sendMsg.append(v);
                         comm->send(sendMsg, destination_rank, MPTAG_TEST);
                         served++;
-                    } else {
+                    } else
+                    {
                         break;
                     }
                 }
@@ -274,7 +305,8 @@ void TEST_MT_send_recv(ICommunicator* comm, int counter){
                 return 0;
             }
         };
-        class RWorker: public Thread{
+        class RWorker: public Thread
+        {
         private:
             ICommunicator* comm;
             int* counter;
@@ -282,7 +314,8 @@ void TEST_MT_send_recv(ICommunicator* comm, int counter){
 
         public:
             RWorker(ICommunicator* _comm, int* _counter):comm(_comm), counter(_counter), maxCounter(*_counter){}
-            int run(){
+            int run()
+            {
                 IGroup *group = comm->getGroup();
                 rank_t p = group->ordinality();
                 rank_t rank = group->rank();
@@ -290,9 +323,11 @@ void TEST_MT_send_recv(ICommunicator* comm, int counter){
                 int served = 0;
                 CMessageBuffer recvMsg;
                 int received_msg;
-                while (*counter){
+                while (*counter)
+                {
                     recvMsg.clear();
-                    if (comm->recv(recvMsg, source_rank, MPTAG_TEST, NULL, 100)){
+                    if (comm->recv(recvMsg, source_rank, MPTAG_TEST, NULL, 100))
+                    {
                         recvMsg.read(received_msg);
                         setValidate(received_msg, maxCounter);
                         getNextCount(recvCriticalSec, *counter);
@@ -307,19 +342,24 @@ void TEST_MT_send_recv(ICommunicator* comm, int counter){
         int s_counter, r_counter;
         s_counter = r_counter = counter;
         _T("counter="<<counter);
-        for(int i=0;i<SEND_THREADS; i++){
+        for(int i=0;i<SEND_THREADS; i++)
+        {
             workers.push_back(new SWorker(comm, &s_counter));
         }
-        for(int i=0;i<RECV_THREADS; i++){
+        for(int i=0;i<RECV_THREADS; i++)
+        {
             workers.push_back(new RWorker(comm, &r_counter));
         }
-        for(int i=0;i<workers.size(); i++){
+        for(int i=0;i<workers.size(); i++)
+        {
             workers[i]->start();
         }
-        for(int i=0;i<workers.size(); i++){
+        for(int i=0;i<workers.size(); i++)
+        {
             workers[i]->join();
         }
-        for(int i=0;i<workers.size(); i++){
+        for(int i=0;i<workers.size(); i++)
+        {
             delete workers[i];
         }
         assertex(s_counter == 0);
@@ -331,70 +371,87 @@ void TEST_MT_send_recv(ICommunicator* comm, int counter){
     comm->barrier();
 }
 
-void run_tests(ICommunicator* comm, int type, int paramCount, char* parameter[]) {
-    switch (type) {
-        case RANK_TEST: {
+void run_tests(ICommunicator* comm, int type, int paramCount, char* parameter[])
+{
+    switch (type)
+    {
+        case RANK_TEST:
+        {
             TEST_rank(comm);
             break;
         }
-        case SINGLE_SEND_TEST: {
+        case SINGLE_SEND_TEST:
+        {
             TEST_single_send(comm);
             break;
         }
-        case RIGHT_SHIFT_TEST: {
+        case RIGHT_SHIFT_TEST:
+        {
             TEST_right_shift(comm);
             break;
         }
-        case CUSTOM_SEND_TEST: {
+        case CUSTOM_SEND_TEST:
+        {
             break;
         }
-        case RECEIVE_FROM_ANY_TEST: {
+        case RECEIVE_FROM_ANY_TEST:
+        {
             int rank = (paramCount == 1) ? atoi(parameter[0]) : 0;
             TEST_receive_from_any(comm, rank);
             break;
         }
-        case SEND_ONE_TO_ALL_TEST: {
+        case SEND_ONE_TO_ALL_TEST:
+        {
             int rank = (paramCount == 1) ? atoi(parameter[0]) : 0;
             TEST_one_to_all(comm, rank);
             break;
         }
-        case RECEIVE_ONE_FROM_ALL_TEST: {
+        case RECEIVE_ONE_FROM_ALL_TEST:
+        {
             int rank = (paramCount == 1) ? atoi(parameter[0]) : 0;
             TEST_one_from_all(comm, rank);
             break;
         }
-        case MT_SEND_RECV: {
+        case MT_SEND_RECV:
+        {
             int counter = (paramCount == 1) ? atoi(parameter[0]) : 10;
             TEST_MT_send_recv(comm, counter);
             break;
         }
-        case MT_SIMPLE_SEND_RECV: {
+        case MT_SIMPLE_SEND_RECV:
+        {
             TEST_MT_simple_send_recv(comm);
             break;
         }
-        default:{
+        default:
+        {
             //TODO run all tests
         }
     }
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
     InitModuleObjects();
-    try {
+    try
+    {
         EnableSEHtoExceptionMapping();
         startMPServer(0);
-        IGroup* group = createIGroup(0, (INode **) NULL);
-        ICommunicator* comm = createCommunicator(group);
-        myrank = group->rank();
+//        IGroup* group = createIGroup(0, (INode **) NULL);
+        ICommunicator* comm = createCommunicator(NULL);
+        myrank = comm->getGroup()->rank();
         int type;
-        if ((argc > 2) && strcmp(argv[1], "-t")==0){
+        if ((argc > 2) && strcmp(argv[1], "-t")==0)
+        {
             type = strcmp(argv[2], "all")==0? -1:atoi(argv[2]);
-        } else {
+        } else
+        {
             throw makeStringException(0, "Invalid commandline parameters.");
         }
         run_tests(comm, type, argc-3, &argv[3]);
         stopMPServer();
-    } catch (IException *e){
+    } catch (IException *e)
+    {
         stopMPServer();
         if ((argc != 2) || strcmp(argv[1], "--help"))
             pexception("Exception", e);
