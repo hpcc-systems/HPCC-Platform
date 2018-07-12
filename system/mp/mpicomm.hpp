d859cb60abbc9419d6caf2286c19bcfd10658c80
samindaw <samindaw@gmail.com>
2018-07-12 12:21:20 -0400

    adding comments and refactoring

--- system/mp/mpicomm.hpp
+++ system/mp/mpicomm.hpp
@@ -12,11 +12,27 @@
 #include <mpi.h>
 #include "mpcomm.hpp"
 
+/**
+ * Create an instance of an ICommunicator that uses MPI framework for communication
+ *
+ * @param group     IGroup containing the nodes which the communicator would initiate
+ *                  communications on
+ */
 extern mp_decl ICommunicator *createMPICommunicator(IGroup *group);
 
+/**
+ * Initialize the MPI framework with the provided MPI communicator
+ */
 extern mp_decl void initializeMPI(MPI::Comm& comm);
+
+/**
+ * Finalize MPI framework
+ */
 extern mp_decl void terminateMPI();
 
+/**
+ * Get the global rank among all the MPI hosts
+ */
 extern mp_decl int getMPIGlobalRank();
 
 #endif /* SYSTEM_MP_MPICOMM_HPP_ */
