972e1736e7ff1b12e9698f66e902d887254849ed
Michael Gardner <michael.gardner@lexisnexisrisk.com>
2018-08-30 11:32:22 -0400

    HPCC-20431 Added curl to dependency list
    Signed-off-by: Michael Gardner <michael.gardner@lexisnexisrisk.com>

--- cmake_modules/dependencies/artful.cmake
+++ cmake_modules/dependencies/artful.cmake
@@ -1,2 +1,2 @@
 # Ubuntu 17.04
-SET_DEPENDENCIES ( CPACK_DEBIAN_PACKAGE_DEPENDS g++ openssh-client openssh-server expect rsync libapr1 python psmisc )
+SET_DEPENDENCIES ( CPACK_DEBIAN_PACKAGE_DEPENDS g++ openssh-client openssh-server expect rsync libapr1 python psmisc curl )
--- cmake_modules/dependencies/el7.cmake
+++ cmake_modules/dependencies/el7.cmake
@@ -1,2 +1,2 @@
 # Centos 7
-SET_DEPENDENCIES ( CPACK_RPM_PACKAGE_REQUIRES m4 libtool gcc-c++ openssh-server openssh-clients expect rsync zip psmisc )
+SET_DEPENDENCIES ( CPACK_RPM_PACKAGE_REQUIRES m4 libtool gcc-c++ openssh-server openssh-clients expect rsync zip psmisc curl )
