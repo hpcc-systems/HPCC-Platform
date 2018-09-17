fa1033c4a5c55652c9cf344f62841f52c63e9d9f
Miguel Vazquez <miguel.vazquez@lexisnexis.com>
2018-09-16 22:54:40 -0400

    HPCC-18339 Browser tab synchronization
    Add preliminary aspects of localstorage to ECLWatch framework to allow
    communication between multiple tabs within the same origin.
    
    Signed-off-by: Miguel Vazquez <miguel.vazquez@lexisnexis.com>

--- esp/src/eclwatch/LockDialogWidget.js
+++ esp/src/eclwatch/LockDialogWidget.js
@@ -13,6 +13,7 @@ define([
     "dojo/keys",
     "dojo/cookie",
     "dojo/topic",
+    "dojo/query",
 
     "dijit/registry",
     "dijit/form/Select",
@@ -21,6 +22,7 @@ define([
     "hpcc/_Widget",
     "src/Utility",
     "src/ws_account",
+    "src/ESPUtil",
 
     "dojo/text!../templates/LockDialogWidget.html",
 
@@ -32,9 +34,9 @@ define([
 
     "hpcc/TableContainer"
 
-], function (declare, lang, i18n, nlsHPCC, arrayUtil, dom, domForm, domClass, on, domStyle, xhr, keys, cookie, topic,
+], function (declare, lang, i18n, nlsHPCC, arrayUtil, dom, domForm, domClass, on, domStyle, xhr, keys, cookie, topic, query,
     registry, Select, CheckBox,
-    _Widget, Utility, WsAccount,
+    _Widget, Utility, WsAccount, ESPUtil,
     template) {
         return declare("LockDialogWidget", [_Widget], {
             templateString: template,
@@ -43,6 +45,7 @@ define([
 
             _width: "480px",
             lockDialogWidget: null,
+            storage: null,
 
             postCreate: function (args) {
                 this.inherited(arguments);
@@ -51,6 +54,8 @@ define([
                 this.unlockUserName = registry.byId(this.id + "UnlockUserName");
                 this.unlockPassword = registry.byId(this.id + "UnlockPassword");
                 this.unlockForm = registry.byId(this.id + "UnlockForm");
+                this.unlockStatus = dom.byId(this.id + "UnlockStatus");
+                this.storage = new ESPUtil.LocalStorage();
             },
 
             startup: function (args) {
@@ -58,15 +63,32 @@ define([
             },
 
             show: function (event) {
+                var context = this;
+                on(this.unlockPassword, "keypress", function (event) {
+                    if (event.key === "Enter") {
+                        context._onUnlock();
+                    }
+                });
+
                 this.unlockDialog.show();
                 domClass.add("SessionLock", "overlay");
-                dojo.removeClass(this.id + "UnlockDialog_underlay", "dijitDialogUnderlay _underlay");
                 this.unlockUserName.set("value", dojoConfig.username);
-                this._onLock();
+
+                topic.publish("hpcc/session_management_status", {
+                    status: "Locked"
+                });
             },
 
             hide: function (event) {
+                domClass.remove("SessionLock", "overlay");
                 this.unlockDialog.hide();
+                this.unlockDialog.destroyRecursive()
+                dojo.query(".dijitDialogUnderlayWrapper").forEach(function(node){
+                    dojo.destroy(node.id);
+                });
+                dojo.query(".unlockDialogToHide").forEach(function(node){
+                    dojo.destroy(node.id);
+                });
             },
 
             _onUnlock: function (event) {
@@ -80,21 +102,22 @@ define([
                             password: this.unlockPassword.get("value")
                         }
                     }).then(function (response) {
-                        var status = dom.byId("UnlockStatus");
 
                         if (response.UnlockResponse.Error === 0) {
-                            if (status.innerHTML !== "") {
-                                status.innerHTML = "";
+                            if (context.unlockStatus.innerHTML !== "") {
+                                context.unlockStatus.innerHTML = "";
                             }
-                            context.hide();
-                            context.unlockDialog.destroyRecursive();
                             domClass.remove("SessionLock", "overlay");
+                            context.unlockDialog.hide();
+                            context.unlockDialog.destroyRecursive();
                             topic.publish("hpcc/session_management_status", {
                                 status: "Unlocked"
                             });
                             cookie("Status", "Unlocked");
+                            context.storage.removeItem("Status");
+                            context.storage.setItem("Status", "Unlocked");
                         } else {
-                            status.innerHTML = response.UnlockResponse.Message;
+                            context.unlockStatus.innerHTML = response.UnlockResponse.Message;
                             cookie("Status", "Locked");
                         }
                     });
@@ -110,16 +133,23 @@ define([
                     }
                 });
 
-                xhr("esp/lock", {
-                    method: "post",
-                }).then(function(response){
-                    if (response) {
-                        topic.publish("hpcc/session_management_status", {
-                            status: "Locked"
-                        });
-                        cookie("Status", "Locked");
-                    }
-                });
+                if (cookie("Status") === "Unlocked") {
+                    xhr("esp/lock", {
+                        method: "post",
+                    }).then(function(response){
+                        if (response) {
+                            context.unlockDialog.show();
+                            domClass.add("SessionLock", "overlay");
+                            context.unlockUserName.set("value", dojoConfig.username);
+                            topic.publish("hpcc/session_management_status", {
+                                status: "Locked"
+                            });
+                            cookie("Status", "Locked");
+                            context.storage.removeItem("Status");
+                            context.storage.setItem("Status", "Locked");
+                        }
+                    });
+                }
             },
 
             init: function (params) {
