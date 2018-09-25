a1b27fe354b2f773c4a74969fb35d00b13a3e92b
Gordon Smith <gordonjsmith@gmail.com>
2018-09-25 10:26:27 +0100

    HPCC-20565 ECL Watch grid title tooltip
    Only display grid title tooltips when text is clipped.
    
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- esp/src/src/ESPUtil.ts
+++ esp/src/src/ESPUtil.ts
@@ -172,17 +172,31 @@ var GridHelper = declare(null, {
     postCreate: override(function (inherited) {
         inherited(arguments);
 
-        this.__hpcc_tooltip = new Tooltip({
+        this.__hpcc_tooltip_header = new Tooltip({
             connectId: [this.id],
-            selector: "td,.dgrid-resize-header-container",
-            showDelay: 800,
-            getContent: function (node) {
-                if (node.offsetWidth < node.scrollWidth) {
+            selector: ".dgrid-resize-header-container",
+            showDelay: 400,
+            getContent(node) {
+                if (node.offsetWidth < node.scrollWidth - 4) {
                     return node.innerHTML;
                 }
                 return "";
             }
         });
+        this.__hpcc_tooltip_header.position = ["above-centered", "below-centered", "before-centered", "after-centered"];
+
+        this.__hpcc_tooltip = new Tooltip({
+            connectId: [this.id],
+            selector: "td",
+            showDelay: 400,
+            getContent(node) {
+                if (node.offsetWidth <= node.scrollWidth) {
+                    return node.innerHTML;
+                }
+                return "";
+            }
+        });
+        this.__hpcc_tooltip.position = ["above-centered", "below-centered", "before-centered", "after-centered"];
     }),
 
     _onNotify: override(function (inherited, object, existingId) {
