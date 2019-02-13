56a718f4b2357e5b6aa94ee0d3e85ba8fff9bb3a
Richard Chapman <rchapman@hpccsystems.com>
2019-02-13 15:48:49 +0000

    Split off 7.0.14
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -4,7 +4,7 @@
 set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 7 )
 set ( HPCC_MINOR 0 )
-set ( HPCC_POINT 13 )
+set ( HPCC_POINT 15 )
 set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 0 )
 ###
