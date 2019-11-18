4656397cd6682aee7b3eea498f1f157d6d5e6991
Jake Smith <jake.smith@lexisnexisrisk.com>
2019-11-18 13:43:41 +0000

    HPCC-23137 Balanced splitter can cause OOM fix
    The buffered rows in a balanced splitter were not fully taking
    into account the memory usage, particularly when dealing with
    rows with child datasets. As a consequence splitter could hoard
    too much memory leading to roxie out of memory conditions.
    
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

--- thorlcr/thorutil/thbuf.cpp
+++ thorlcr/thorutil/thbuf.cpp
@@ -909,6 +909,7 @@ protected:
     Owned<CRowSet> inMemRows;
     CriticalSection crit;
     Linked<IOutputMetaData> meta;
+    Linked<IOutputRowSerializer> serializer;
     QueueOf<CRowSet, false> chunkPool;
     unsigned maxPoolChunks;
     bool reuseRowSets;
@@ -1042,7 +1043,8 @@ public:
 
     IMPLEMENT_IINTERFACE_USING(CSimpleInterface);
 
-    CSharedWriteAheadBase(CActivityBase *_activity, unsigned _outputCount, IThorRowInterfaces *rowIf) : activity(_activity), outputCount(_outputCount), meta(rowIf->queryRowMetaData())
+    CSharedWriteAheadBase(CActivityBase *_activity, unsigned _outputCount, IThorRowInterfaces *rowIf)
+        : activity(_activity), outputCount(_outputCount), meta(rowIf->queryRowMetaData()), serializer(rowIf->queryRowSerializer())
     {
         init();
         minChunkSize = 0x2000;
@@ -1193,7 +1195,6 @@ class CSharedWriteAheadDisk : public CSharedWriteAheadBase
     QueueOf<Chunk, false> savedChunks;
     offset_t highOffset;
     Linked<IEngineRowAllocator> allocator;
-    Linked<IOutputRowSerializer> serializer;
     Linked<IOutputRowDeserializer> deserializer;
     IOutputMetaData *serializeMeta;
 
@@ -1501,7 +1502,7 @@ class CSharedWriteAheadDisk : public CSharedWriteAheadBase
     }
 public:
     CSharedWriteAheadDisk(CActivityBase *activity, const char *spillName, unsigned outputCount, IThorRowInterfaces *rowIf, IDiskUsage *_iDiskUsage) : CSharedWriteAheadBase(activity, outputCount, rowIf),
-        allocator(rowIf->queryRowAllocator()), serializer(rowIf->queryRowSerializer()), deserializer(rowIf->queryRowDeserializer()), serializeMeta(meta->querySerializedDiskMeta()), iDiskUsage(_iDiskUsage)
+        allocator(rowIf->queryRowAllocator()), deserializer(rowIf->queryRowDeserializer()), serializeMeta(meta->querySerializedDiskMeta()), iDiskUsage(_iDiskUsage)
     {
         assertex(spillName);
         spillFile.setown(createIFile(spillName));
@@ -1608,7 +1609,11 @@ class CSharedWriteAheadMem : public CSharedWriteAheadBase
     }
     virtual size32_t rowSize(const void *row)
     {
-        return meta->getRecordSize(row); // space in mem.
+        if (!row)
+            return 1; // eog;
+        CSizingSerializer ssz;
+        serializer->serialize(ssz, (const byte *)row);
+        return ssz.size();
     }
 public:
     CSharedWriteAheadMem(CActivityBase *activity, unsigned outputCount, IThorRowInterfaces *rowif, unsigned buffSize) : CSharedWriteAheadBase(activity, outputCount, rowif)
