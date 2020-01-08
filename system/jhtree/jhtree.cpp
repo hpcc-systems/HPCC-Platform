42493d52e50cd9b936805665ea091c7b4c8b797e
Richard Chapman <rchapman@hpccsystems.com>
2020-01-08 11:21:46 +0000

    HPCC-23259 Improve error reported when reading a corrupt index
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- system/jhtree/jhtree.cpp
+++ system/jhtree/jhtree.cpp
@@ -977,26 +977,26 @@ void CKeyIndex::init(KeyHdr &hdr, bool isTLK, bool allowPreload)
     try
     {
         keyHdr->load(hdr);
+        offset_t rootPos = keyHdr->getRootFPos();
+        Linked<CNodeCache> nodeCache = queryNodeCache();
+        if (allowPreload)
+        {
+            if (nodeCache->getNodeCachePreload() && !nodeCache->isPreloaded(iD, rootPos))
+            {
+                cacheNodes(nodeCache, rootPos, isTLK);
+            }
+        }
+        rootNode = nodeCache->getNode(this, iD, rootPos, NULL, isTLK);
+        loadBloomFilters();
     }
     catch (IKeyException *ke)
     {
         if (!name.get()) throw;
         StringBuffer msg;
-        IKeyException *ke2 = MakeKeyException(ke->errorCode(), "%s. In key '%s'.", ke->errorMessage(msg).str(), name.get());
+        IKeyException *ke2 = MakeKeyException(ke->errorCode(), "%s. In key '%s' (corrupt index?)", ke->errorMessage(msg).str(), name.get());
         ke->Release();
         throw ke2;
     }
-    offset_t rootPos = keyHdr->getRootFPos();
-    Linked<CNodeCache> nodeCache = queryNodeCache();
-    if (allowPreload)
-    {
-        if (nodeCache->getNodeCachePreload() && !nodeCache->isPreloaded(iD, rootPos))
-        {
-            cacheNodes(nodeCache, rootPos, isTLK);
-        }
-    }
-    rootNode = nodeCache->getNode(this, iD, rootPos, NULL, isTLK);
-    loadBloomFilters();
 }
 
 CKeyIndex::~CKeyIndex()
