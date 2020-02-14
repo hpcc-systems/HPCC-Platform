7d1378bb5c5cf1bd1477a781750e277f0c5f3085
Jake Smith <jake.smith@lexisnexisrisk.com>
2020-02-14 13:25:01 +0000

    incr

--- dockerfiles/incr.sh
+++ dockerfiles/incr.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 HEAD=$(git rev-parse --short HEAD)
-PREV=$(git rev-parse --short HEAD^)
+
+PREV=$1
+BUILD_USER=$2
+[[ -z ${BUILD_USER} ]] && BUILD_USER=${INPUT_BUILD_USER}
+[[ -z ${BUILD_USER} ]] && BUILD_USER=${GITHUB_REPOSITORY%/*}
+[[ -z ${BUILD_USER} ]] && BUILD_USER=hpcc-systems
+
+[[ -z ${PREV} ]] && BUILD_VER=$(git rev-parse --short HEAD^)
 
 set -e
 
+build_image() {
+  local name=$1
+  local ver=$2
+  [[ -z $ver ]] || local usever="--build-arg BUILD_VER=$ver"
+  local useuser="--build-arg BUILD_USER=$BUILD_USER"
+
+  if ! docker pull hpccsystems/${name}:${ver} ; then
+    docker image build -t hpccsystems/${name}:${ver} ${usever} ${useuser} ${name}/ 
+    if [ "$PUSH" = "1" ] ; then
+      docker push hpccsystems/${name}:${ver}
+    fi
+  fi
+}
+
+echo HERE1
+
 #more - only do this if head does not match BUILD_VER
-docker image build -t hpccsystems/platform-build:${HEAD} --build-arg BUILD_VER=${PREV} --build-arg COMMIT=${HEAD} platform-build-incremental/
-docker image build -t hpccsystems/platform-core:${HEAD} --build-arg BUILD_VER=${HEAD} platform-core/  
+docker image build -t hpccsystems/platform-build:${HEAD} --build-arg=BUILD_USER=${BUILD_USER} --build-arg BUILD_VER=${PREV} --build-arg COMMIT=${HEAD} platform-build-incremental/
 
-docker image build -t hpccsystems/roxie:${HEAD} --build-arg BUILD_VER=${HEAD} roxie/
-docker image build -t hpccsystems/dali:${HEAD} --build-arg BUILD_VER=${HEAD} dali/
-docker image build -t hpccsystems/esp:${HEAD} --build-arg BUILD_VER=${HEAD} esp/
-docker image build -t hpccsystems/eclccserver:${HEAD} --build-arg BUILD_VER=${HEAD} eclccserver/
-docker image build -t hpccsystems/eclagent:${HEAD} --build-arg BUILD_VER=${HEAD} eclagent/
+echo HERE2
 
+build_image platform-core ${HEAD}
+
+build_image roxie ${HEAD}
+build_image dali ${HEAD}
+build_image esp ${HEAD}
+build_image eclccserver ${HEAD}
+build_image eclagent ${HEAD}
+build_image thormaster ${HEAD}
+build_image thorslave ${HEAD}
--- dockerfiles/startall.sh
+++ dockerfiles/startall.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-HEAD=$(git rev-parse --short HEAD)
+BUILD_VER=$1
+[[ -z ${BUILD_VER} ]] && BUILD_VER=${INPUT_BUILD_VER}
+[[ -z ${BUILD_VER} ]] && BUILD_VER=$(git describe --exact-match --tags)
+[[ -z ${BUILD_VER} ]] && BUILD_VER=$(git rev-parse --short ${BUILD_VER})
 
-kubectl run dali --image=hpccsystems/dali:${HEAD} --image-pull-policy=Never
+kubectl run dali --image=hpccsystems/dali:${BUILD_VER} --image-pull-policy=Never
 kubectl expose deployment dali --port=7070
-kubectl run esp --image=hpccsystems/esp:${HEAD} --image-pull-policy=Never
+kubectl run esp --image=hpccsystems/esp:${BUILD_VER} --image-pull-policy=Never
 kubectl expose deployment esp --port=8010 --type=LoadBalancer
-kubectl run roxie --image=hpccsystems/roxie:${HEAD} --image-pull-policy=Never
-kubectl run eclcc --image=hpccsystems/eclccserver:${HEAD} --image-pull-policy=Never
-kubectl run eclagent --image=hpccsystems/eclagent:${HEAD} --image-pull-policy=Never
-kubectl run thormaster --image=hpccsystems/thormaster:${HEAD} --image-pull-policy=Never
+kubectl run roxie --image=hpccsystems/roxie:${BUILD_VER} --image-pull-policy=Never
+kubectl run eclcc --image=hpccsystems/eclccserver:${BUILD_VER} --image-pull-policy=Never
+kubectl run eclagent --image=hpccsystems/eclagent:${BUILD_VER} --image-pull-policy=Never
+kubectl run thormaster --image=hpccsystems/thormaster:${BUILD_VER} --image-pull-policy=Never
 kubectl expose deployment thormaster --port=20000
-kubectl run thorslave --image=hpccsystems/thorslave:${HEAD} --image-pull-policy=Never master=thormaster
+kubectl run thorslave --image=hpccsystems/thorslave:${BUILD_VER} --image-pull-policy=Never master=thormaster
 
 #kubectl logs mydali-759f975769-v6tmm
 
