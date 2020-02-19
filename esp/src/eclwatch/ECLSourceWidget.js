f6172ed195956a45a305bd4725068f3428b7f619
Jaman Brundage <jbrundage372@gmail.com>
2020-02-19 13:25:15 -0500

    HPCC-19274  Add ECLArchiveViewer
    Add ECL Archive tree view next to ECL textbox.
    
    Clicking on a section name will display the contents on the right.
    
    The ECL query is displayed on the right by default. If no archive contents are found for a WU the left side "DirectoryTree" is hidden.
    
    Signed-off-by: Jaman Brundage <jbrundage372@gmail.com>

--- esp/src/eclwatch/ECLSourceWidget.js
+++ esp/src/eclwatch/ECLSourceWidget.js
@@ -9,10 +9,14 @@ define([
 
     "dijit/registry",
 
-    "src/CodeMirror",
-
     "hpcc/_Widget",
     "src/ESPWorkunit",
+
+    "@hpcc-js/comms",
+    "@hpcc-js/tree",
+    "@hpcc-js/codemirror",
+    "@hpcc-js/phosphor",
+    "@hpcc-js/util",
 
     "dojo/text!../templates/ECLSourceWidget.html",
 
@@ -24,8 +28,12 @@ define([
 
 ], function (declare, lang, i18n, nlsHPCC, dom, xhr, topic,
     registry,
-    CodeMirror,
     _Widget, ESPWorkunit,
+    hpccjsComms,
+    hpccjsTree,
+    hpccjsCodemirror,
+    hpccjsPhosphor,
+    hpccjsUtil,
     template) {
     return declare("ECLSourceWidget", [_Widget], {
         templateString: template,
@@ -56,13 +64,12 @@ define([
         resize: function (args) {
             this.inherited(arguments);
             this.borderContainer.resize();
-            if (this.editor) {
-                this.editor.setSize("100%", "100%");
+            if (this.archiveViewer) {
+                this.archiveViewer
+                    .resize()
+                    .render()
+                ;
             }
-        },
-
-        layout: function (args) {
-            this.inherited(arguments);
         },
 
         //  Plugin wrapper  ---
@@ -70,120 +77,197 @@ define([
             if (this.inherited(arguments))
                 return;
 
-            var mode = "ecl";
-            if (params.sourceMode !== undefined) {
-                mode = params.sourceMode;
-            } else if (this.WUXml) {
-                mode = "xml";
-            }
-
-            if (params.readOnly !== undefined)
-                this.readOnly = params.readOnly;
-
-            this.editor = CodeMirror.fromTextArea(document.getElementById(this.id + "EclCode"), {
-                tabMode: "indent",
-                matchBrackets: true,
-                lineNumbers: true,
-                mode: mode,
-                readOnly: this.readOnly,
-                foldGutter: mode === "xml" ? true : false,
-                gutters: mode === "xml" ? ["CodeMirror-linenumbers", "CodeMirror-foldgutter"] : ["CodeMirror-linenumbers"]
-            });
-            dom.byId(this.id + "EclContent").style.backgroundColor = this.readOnly ? 0xd0d0d0 : 0xffffff;
-            this.editor.setSize("100%", "100%");
-
-            var context = this;
-            if (params.Wuid) {
-                this.wu = ESPWorkunit.Get(params.Wuid);
-                if (this.WUXml) {
-                    this.wu.fetchXML(function (xml) {
-                        context.setText(xml);
-                    });
+            var flatMap = {};
+            var flatNodes = [];
+            function transformNode(node, pathStr, pathToQuery) {
+                if(typeof pathStr === "undefined") {
+                    pathStr = "";
+                }
+                var ret = {};
+                var hasName = node.name;
+                var hasMoneyName = node["$"] && node["$"].name;
+                var childrenUndefined = typeof node._children === "undefined";
+                ret.label = hasMoneyName ? node["$"].name : "";
+    
+                var pathToNode = (pathStr.length > 0 ? pathStr + "." : "") + ret.label;
+                
+                if (!hasMoneyName && hasName) {
+                    ret.label = node.name;
+                }
+                if (!childrenUndefined && (node._children.length > 0 || !node.content)) {
+                    if (node._children.length === 1 && node._children[0].name === "Text") {
+                        ret.content = node._children[0].content.trim();
+                        if (pathToNode === pathToQuery) {
+                            ret.selected = true;
+                        }
+                    } else {
+                        ret.children = node._children.map(function(n) {
+                                return transformNode(n, pathToNode, pathToQuery);
+                            })
+                            .filter(function(n){
+                                return n;
+                            })
+                            ;
+                    }
+                } else if (typeof node.content === "string" && node.content.trim()) {
+                    ret.content = node.content.trim();
+                    ret.selected = pathToNode === pathToQuery;
                 } else {
-                    this.wu.fetchText(function (text) {
-                        context.setText(text);
+                    if(node.name && node.name !== "Query"){
+                        ret = {
+                            label: node.name,
+                            content: JSON.stringify(node, undefined, 4)
+                        };
+                    }
+                    return false;
+                }
+                var path = (pathStr.length > 0 ? pathStr + "." : "") + ret.label;
+                if(node["$"] && node["$"].name === "" && node["$"].key === "") {
+                    // new root folder
+                    path = "";
+                }
+                if(ret.label === "Query" && (!node.children || node._children.length === 0)){
+                    // skipping 'Query' metadata and empty folders
+                } else {
+                    flatMap[path] = ret;
+                    flatNodes.push({
+                        data: ret,
+                        path: path,
+                        node: node
                     });
                 }
-            } else if (lang.exists("ECL", params)) {
-                this.setText(params.ECL ? params.ECL : "");
-            } else if (lang.exists("Usergenerated", params)) {
-                this.setText(params.Usergenerated);
-            } else if (lang.exists("sourceURL", params)) {
-                xhr(params.sourceURL, {
-                    handleAs: "text"
-                }).then(function (data) {
-                    context.setText(data);
-                });
             }
-        },
 
-        clearErrors: function (errWarnings) {
-            for (var i = 0; i < this.markers.length; ++i) {
-                this.markers[i].clear();
-            }
-            this.markers = [];
-        },
+            var context = this;
 
-        setErrors: function (errWarnings) {
-            for (var i = 0; i < errWarnings.length; ++i) {
-                var line = parseInt(errWarnings[i].LineNo, 10);
-                this.markers.push(this.editor.doc.markText({
-                    line: line - 1,
-                    ch: 0
-                }, {
-                    line: line,
-                    ch: 0
-                }, {
-                    className: errWarnings[i].Severity + "Line"
-                }));
-            }
-        },
+            var directoryTree = new hpccjsTree.DirectoryTree()
+                .textFileIcon("fa fa-file-code-o")
+                .omitRoot(true)
+                ;
+            var editor = new hpccjsCodemirror.ECLEditor();
+            var panels = new hpccjsPhosphor.SplitPanel("horizontal");
+        
+            var wu = hpccjsComms.Workunit.attach({ baseUrl: "" }, params.Wuid);
+            wu.refresh(true).then(function(wuInfo){
 
-        setCursor: function (line, col) {
-            this.editor.setCursor(line - 1, col - 1);
-            this.editor.focus();
-        },
+                editor.text(wuInfo.Query.Text);
+                
+                if(!wuInfo.HasArchiveQuery){
+                    editor
+                        .target(context.id + "EclContent")
+                        .render()
+                        ;
+                } else {
+                    
+                    directoryTree
+                        .data({
+                            label: "root",
+                            children: [
+                                {
+                                    label:"loading...",
+                                }
+                            ]
+                        })
+                        .textFileIcon("fa fa-spinner fa-spin")
+                        .iconSize(20)
+                        .rowItemPadding(2)
+                        ;
+                    panels
+                        .target(context.id + "EclContent")
+                        .addWidget(directoryTree)
+                        .addWidget(editor)
+                        .relativeSizes([0.38,0.62])
+                        .render()
+                        ;
 
-        clearHighlightLines: function () {
-            for (var i = 0; i < this.highlightLines.length; ++i) {
-                this.highlightLines[i].clear();
-            }
-        },
+                    wu.fetchArchive().then(function(archiveResp) {
+                        var json = hpccjsUtil.xml2json(archiveResp);
+                        var pathToQuery = "";
+                        try{
+                            pathToQuery = json._children[0]._children.find(function(n){
+                                return n.name && n.name.toLowerCase()==="query";
+                            })["$"].attributePath;
+                        }catch(e){}
+                        var data = {
+                            label: "",
+                            children: []
+                        };
+                        if (json._children && json._children[0]) {
+                            json._children[0]._children.forEach(function(n){
+                                transformNode(n, "", pathToQuery);
+                            });
+                            flatNodes.sort(function(a,b){
+                                var aIsFolder = !!a.data.children;
+                                var bIsFolder = !!b.data.children;
+                                if((aIsFolder && bIsFolder) || (!aIsFolder && !bIsFolder)) {
+                                    return a.path.toLowerCase() > b.path.toLowerCase() ? 1 : -1;
+                                } else {
+                                    return aIsFolder && !bIsFolder ? -1 : 1;
+                                }
+                            })
+                            flatNodes.forEach(function(flatNode,i){
+                                var pathArr = flatNode.path.split(".").filter(function(n){
+                                    return n !== "";
+                                });
+                                if (pathArr.length === 0) {
+                                    data.children = data.children.concat(flatNode.data.children);
+                                } else {
+                                    var parentPath = pathArr.slice(0,-1).join('.');
+                                    if(!parentPath) {
+                                        data.children.push(flatNode.data);
+                                    } else {
+                                        if(!flatNode.data.content || !flatNode.data.content.trim()){
+                                            flatNode.data.label = pathArr.slice(-1);
+                                        }
+                                        if(!flatMap[parentPath].children){
+                                            flatMap[parentPath].children = [];
+                                        }
+                                        flatMap[parentPath].children.push(flatNode.data);
+                                    }
+                                }
+                                
+                            })
+                        }
+                        directoryTree
+                            .data({})
+                            .render(function(){
+                                directoryTree
+                                    .data(data)
+                                    .iconSize(16)
+                                    .rowItemPadding(2)
+                                    .textFileIcon("fa fa-file-code-o")
+                                    .render()
+                                    ;
+                                
+                                directoryTree.rowClick = function(contentStr) {
+                                    editor.text(contentStr);
+                                };
+                                
+                                panels.render(function(){
+                                    var rw = directoryTree.calcRequiredWidth() + 20;
+                                    var pw = panels.width();
+                                    var ratio = rw / pw;
+                                    panels.relativeSizes([ratio, 1 - ratio]);
+                                });
+                            });
+            
+                        directoryTree.rowClick = function(contentStr) {
+                            editor.text(contentStr);
+                        }
+                        
+                        panels.render(function(){
+                            var rw = directoryTree.calcRequiredWidth() + 20;
+                            var pw = panels.width();
+                            var ratio = rw / pw;
+                            panels.relativeSizes([ratio, 1 - ratio]);
+                        });
+                    });
+                }
+                
+            });
 
-        highlightLine: function (line) {
-            this.highlightLines.push(this.editor.doc.markText({
-                line: line - 1,
-                ch: 0
-            }, {
-                line: line,
-                ch: 0
-            }, {
-                className: "highlightline"
-            }));
-        },
-
-        setText: function (text) {
-            try {
-                this.editor.setValue(text);
-            } catch (e) {
-                topic.publish("hpcc/brToaster", {
-                    Severity: "Error",
-                    Source: "ECLSourceWidget.setText",
-                    Exceptions: [
-                        { Message: this.i18n.SetTextError },
-                        { Message: e.toString ? (this.i18n.Details + ":\n" + e.toString()) : e }
-                    ]
-                });
-            }
-        },
-
-        setReadOnly: function (readonly) {
-            this.editor.readOnly(readonly);
-        },
-
-        getText: function () {
-            return this.editor.getValue();
+            
         }
-
     });
 });
+
