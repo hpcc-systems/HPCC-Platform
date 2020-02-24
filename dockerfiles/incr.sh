d17a242d627b3fe8ee14238f014aeadc7ea606ec
Richard Chapman <rchapman@hpccsystems.com>
2020-02-24 12:23:08 +0000

    HPCC-23532 Dockerfiles should have some copyright headers
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- dockerfiles/incr.sh
+++ dockerfiles/incr.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+##############################################################################
+#
+#    HPCC SYSTEMS software Copyright (C) 2020 HPCC Systems®.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+##############################################################################
+
 # Use this script to build local images for testing, while leveraging the work already done in the most recent tag
 # on the branch that your local branch was based on
 #
