eb3ac4cc67e5eb3a2105b22fc63dbcfa2582b228
Jake Smith <jake.smith@lexisnexisrisk.com>
2020-06-03 12:06:42 +0100

    HPCC-24149 startall.sh improvements
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

--- dockerfiles/startall.sh
+++ dockerfiles/startall.sh
@@ -20,11 +20,38 @@
 # Utility script for starting a local cluster corresponding to current git branch
 # Usage startup.sh [<image.version>] [<helm-install-options>]
 
-LABEL=$1
-[[ -z ${LABEL} ]] && LABEL=$(docker image ls | fgrep 'hpccsystems/platform-core' | head -n 1 | awk '{print $2}')
-shift
+DOCKER_REPO=hpccsystems
+restArgs=()
 
-helm install mycluster hpcc/ --set global.image.version=$LABEL --set global.privileged=true $*
+while [ "$#" -gt 0 ]; do
+  arg=$1
+  if [[ ${arg:0:1} == '-' ]]; then
+    case "${arg:1:1}" in
+      l) shift
+         LABEL=$1
+         ;;
+      d) shift;
+         INPUT_DOCKER_REPO=$1
+         ;;
+      h) echo "Usage: startall.sh [options]"
+         echo "    -d <docker-repo>   Docker repository to fetch images from"
+         echo "    -l                 Build image label to use"
+         exit
+         ;;
+      *) restArgs+=(${arg})
+         ;;
+    esac
+  else
+    restArgs+=(${arg})
+  fi
+  shift
+done
+
+[[ -n ${INPUT_DOCKER_REPO} ]] && DOCKER_REPO=${INPUT_DOCKER_REPO}
+[[ -z ${LABEL} ]] && LABEL=$(docker image ls | fgrep "${DOCKER_REPO}/platform-core" | head -n 1 | awk '{print $2}')
+
+helm install mycluster hpcc/ --set global.image.root="${DOCKER_REPO}" --set global.image.version=$LABEL --set global.privileged=true ${restArgs[@]}
+
 sleep 1
 kubectl get pods
 
