73c8feadd5e99f81d47818d9008b6d930de2da10
Richard Chapman <rchapman@hpccsystems.com>
2020-09-21 10:32:02 +0100

    HPCC-24757 wuget may core if archive resource is empty
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- common/dllserver/thorplugin.cpp
+++ common/dllserver/thorplugin.cpp
@@ -666,7 +666,8 @@ extern DLLSERVER_API bool decompressResource(size32_t len, const void *data, Mem
 extern DLLSERVER_API bool decompressResource(size32_t len, const void *data, StringBuffer &result)
 {
     MemoryBuffer tgt;
-    decompressResource(len, data, tgt);
+    if (len)
+        decompressResource(len, data, tgt);
     tgt.append((char)0);
     unsigned expandedLen = tgt.length();
     result.setBuffer(expandedLen, reinterpret_cast<char *>(tgt.detach()), expandedLen-1);
