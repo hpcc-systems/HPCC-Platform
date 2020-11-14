9b3dcabdbfc58156f39f65602d4aeddb68ec8ba5
Gordon Smith <GordonJSmith@gmail.com>
2020-11-14 08:31:03 +0000

    HPCC-25350 React WU SourceFiles
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- esp/src/src-react/hooks/Workunit.ts
+++ esp/src/src-react/hooks/Workunit.ts
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { Workunit, Result, WUStateID } from "@hpcc-js/comms";
+import { Workunit, Result, WUStateID, WUInfo } from "@hpcc-js/comms";
+import nlsHPCC from "src/nlsHPCC";
 
-export function useWorkunit(wuid: string): [Workunit, WUStateID] {
+export function useWorkunit(wuid: string, full: boolean = false): [Workunit, WUStateID] {
 
     const [workunit, setWorkunit] = React.useState<Workunit>();
     const [state, setState] = React.useState<WUStateID>();
@@ -9,13 +10,19 @@ export function useWorkunit(wuid: string): [Workunit, WUStateID] {
     React.useEffect(() => {
         const wu = Workunit.attach({ baseUrl: "" }, wuid);
         const handle = wu.watch(() => {
-            setState(wu.StateID);
+            if (full) {
+                wu.refresh(true).then(() => {
+                setState(wu.StateID);
+                });
+            } else {
+                setState(wu.StateID);
+            }
         });
         setWorkunit(wu);
         return () => {
             handle.release();
         };
-    }, [wuid]);
+    }, [wuid, full]);
 
     return [workunit, state];
 }
@@ -32,4 +39,82 @@ export function useWorkunitResults(wuid: string): [Result[], Workunit, WUStateID
     }, [workunit, state]);
 
     return [results, workunit, state];
+}
+
+export interface Variable {
+    Type: string;
+    Name: string;
+    Value: string;
+}
+
+export function useWorkunitVariables(wuid: string): [Variable[], Workunit, WUStateID] {
+
+    const [workunit, state] = useWorkunit(wuid);
+    const [variables, setVariables] = React.useState<Variable[]>([]);
+
+    React.useEffect(() => {
+        workunit?.fetchInfo({
+            IncludeVariables: true,
+            IncludeApplicationValues: true,
+            IncludeDebugValues: true
+        }).then(response => {
+            const vars: Variable[] = response?.Workunit?.Variables?.ECLResult?.map(row => {
+                return {
+                    Type: nlsHPCC.ECL,
+                    Name: row.Name,
+                    Value: row.Value
+                };
+            }) || [];
+            const appData: Variable[] = response?.Workunit?.ApplicationValues?.ApplicationValue.map(row => {
+                return {
+                    Type: row.Application,
+                    Name: row.Name,
+                    Value: row.Value
+                };
+            }) || [];
+            const debugData: Variable[] = response?.Workunit?.DebugValues?.DebugValue.map(row => {
+                return {
+                    Type: nlsHPCC.Debug,
+                    Name: row.Name,
+                    Value: row.Value
+                };
+            }) || [];
+            setVariables([...vars, ...appData, ...debugData]);
+        });
+    }, [workunit, state]);
+
+    return [variables, workunit, state];
+}
+
+export interface SourceFile extends WUInfo.ECLSourceFile {
+    __hpcc_parentName: string;
+}
+
+export function useWorkunitSourceFiles(wuid: string): [SourceFile[], Workunit, WUStateID] {
+
+    const [workunit, state] = useWorkunit(wuid);
+    const [sourceFiles, setSourceFiles] = React.useState<SourceFile[]>([]);
+
+    React.useEffect(() => {
+        workunit?.fetchInfo({
+            IncludeSourceFiles: true
+        }).then(response => {
+            const sourceFiles: SourceFile[] = [];
+            response?.Workunit?.SourceFiles?.ECLSourceFile.forEach(sourceFile => {
+                sourceFiles.push({
+                    __hpcc_parentName: "",
+                    ...sourceFile
+                });
+                sourceFile?.ECLSourceFiles?.ECLSourceFile.forEach(childSourceFile => {
+                    sourceFiles.push({
+                        __hpcc_parentName: sourceFile.Name,
+                        ...childSourceFile
+                    });
+                });
+            });
+            setSourceFiles(sourceFiles);
+        });
+    }, [workunit, state]);
+
+    return [sourceFiles, workunit, state];
 }
