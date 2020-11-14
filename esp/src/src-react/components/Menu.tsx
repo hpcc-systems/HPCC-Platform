961e0ca46a5503f50a2be409832a87f3d7a6583e
Gordon Smith <GordonJSmith@gmail.com>
2020-11-14 08:31:03 +0000

    HPCC-25199 Add React Queries Page
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- esp/src/src-react/components/Menu.tsx
+++ esp/src/src-react/components/Menu.tsx
@@ -35,6 +35,7 @@ const navLinkGroups: INavLinkGroup[] = [
         name: "Published Queries",
         links: [
             { url: "#/queries", name: nlsHPCC.Queries },
+            { url: "#/queries/legacy", name: `${nlsHPCC.Queries} (L)` },
             { url: "#/packagemaps", name: nlsHPCC.PackageMaps },
         ]
     },
