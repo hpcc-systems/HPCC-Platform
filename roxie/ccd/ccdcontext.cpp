c2ed3d63d38890d07f1a92b5ba4391cb3be6efd6
Richard Chapman <rchapman@hpccsystems.com>
2021-03-11 10:16:52 +0000

    Extra tracing, but still can't work out what is going on
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- roxie/ccd/ccdcontext.cpp
+++ roxie/ccd/ccdcontext.cpp
@@ -3479,6 +3479,9 @@ public:
                 fileCache.setValue(expandedName, add);
             }
         }
+        else if (traceLevel > 5)
+            DBGLOG("Found entry for %s in filecache", fileName);
+
         return ret.getClear();
     }
 
@@ -3489,6 +3492,8 @@ public:
 
     virtual void endGraph(unsigned __int64 startTimeStamp, cycle_t startCycles, bool aborting) override
     {
+        if (traceLevel > 5)
+            DBGLOG("Killing filecache");
         fileCache.kill();
         CRoxieContextBase::endGraph(startTimeStamp, startCycles, aborting);
     }
