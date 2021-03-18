9ab47ea367f726f870717289a3bbb7bff6d67dde
Richard Chapman <rchapman@hpccsystems.com>
2021-03-18 11:31:27 +0000

    Community Edition 7.12.36-1 Gold
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -6,6 +6,6 @@ set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 7 )
 set ( HPCC_MINOR 12 )
 set ( HPCC_POINT 36 )
-set ( HPCC_MATURITY "rc" )
+set ( HPCC_MATURITY "release" )
 set ( HPCC_SEQUENCE 1 )
 ###
