06b33d0f7ce89ede1ba8194a367c7110c53e9f04
Jake Smith <jake.smith@lexisnexisrisk.com>
2021-03-22 11:35:28 +0000

    HPCC-25577 Capture extra info. when regression query times out
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

---
+++ .github/workflows/smoketest-preabort.sh
@@ -0,0 +1,18 @@
+echo 'List of processes:'
+ps aux 
+echo 'esp stacks'
+echo '=========='
+gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" esp `pidof esp`
+echo 'daserver stacks'
+echo '==============='
+gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" daserver `pidof daserver`
+for p in $(pidof eclcc); do
+ echo 'eclcc[${p}] stacks'
+ echo '==============='
+ gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" eclcc ${p}
+done
+echo 'job queues'
+echo '==============='
+daliadmin . export /JobQueues jq.xml
+cat jq.xml
+echo '***************'
