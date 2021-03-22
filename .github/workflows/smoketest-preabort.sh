ca60df51677997714ffbc48601c93e95afdb0a16
Jake Smith <jake.smith@lexisnexisrisk.com>
2021-03-22 16:48:17 +0000

    need sudo

--- .github/workflows/smoketest-preabort.sh
+++ .github/workflows/smoketest-preabort.sh
@@ -2,14 +2,14 @@ echo 'List of processes:'
 ps aux 
 echo 'esp stacks'
 echo '=========='
-gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" esp `pidof esp`
+sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" esp `pidof esp`
 echo 'daserver stacks'
 echo '==============='
-gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" daserver `pidof daserver`
+sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" daserver `pidof daserver`
 for p in $(pidof eclcc); do
  echo 'eclcc[${p}] stacks'
  echo '==============='
- gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" eclcc ${p}
+ sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" eclcc ${p}
 done
 echo 'job queues'
 echo '==============='
