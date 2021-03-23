6c1db5ef750871a33f630b453b0356c1c25ebdd6
Jake Smith <jake.smith@lexisnexisrisk.com>
2021-03-23 12:14:17 +0000

    wipg

--- .github/workflows/smoketest-preabort.sh
+++ .github/workflows/smoketest-preabort.sh
@@ -1,15 +1,20 @@
 echo 'List of processes:'
 ps aux 
 echo 'esp stacks'
+echo esp path=$(which esp)
+echo eclcc path=$(which eclcc)
 echo '=========='
-sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" esp `pidof esp`
+echo sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" $(which esp) $(pidof esp)
+sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" $(which esp) $(pidof esp)
 echo 'daserver stacks'
 echo '==============='
-sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" daserver `pidof daserver`
+echo sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" $(which daserver) $(pidof daserver)
+sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" $(which daserver) $(pidof daserver)
 for p in $(pidof eclcc); do
- echo 'eclcc[${p}] stacks'
+ echo "eclcc[${p}] stacks"
  echo '==============='
- sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" eclcc ${p}
+ echo sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" $(which eclcc) ${p}
+ sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" $(which eclcc) ${p}
 done
 echo 'job queues'
 echo '==============='
