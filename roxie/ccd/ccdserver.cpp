a666e852c9b6ccbd9692ede766cf8b9e6ba1bbeb
Mark Kelly <mark.kelly@lexisnexisrisk.com>
2021-04-29 11:40:39 -0400

    HPCC-25803 HashDedup can crash in dtor if upstream activity fails early
    Signed-off-by: Mark Kelly <mark.kelly@lexisnexisrisk.com>

--- roxie/ccd/ccdserver.cpp
+++ roxie/ccd/ccdserver.cpp
@@ -7751,7 +7751,8 @@ class CRoxieServerHashDedupActivity : public CRoxieServerActivity
         virtual ~HashDedupTable()
         {
             //elementRowAllocator is a unique allocator, so all rows can be freed in a single call
-            elementRowAllocator->releaseAllRows();
+            if (elementRowAllocator)
+                elementRowAllocator->releaseAllRows();
             tablecount = 0;
         }
 
