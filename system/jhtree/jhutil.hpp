d7b6ff724d5da56ed292e67d5e1e1595f5d1707b
Gavin Halliday <gavin.halliday@lexisnexis.com>
2021-08-18 12:27:33 +0100

    HPCC-26402 Protect node cache from exceptions before node fully loaded
    Also add various helper functions to make it easier to track down problems
    
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- system/jhtree/jhutil.hpp
+++ system/jhtree/jhutil.hpp
@@ -117,6 +117,22 @@ public:
         remove(element);
     }
     inline unsigned ordinality() const { return numEntries; }
+    //Check that the linked list is self-consistent.  For debugging potential issues.
+    void validate() const
+    {
+        ELEMENT * prev = nullptr;
+        ELEMENT * cur = pHead;
+        unsigned count = 0;
+        while (cur)
+        {
+            assertex(cur->prev == prev);
+            prev = cur;
+            cur = cur->next;
+            count++;
+        }
+        assertex(prev == pTail);
+        assertex(count == numEntries);
+    }
 };
 
 
