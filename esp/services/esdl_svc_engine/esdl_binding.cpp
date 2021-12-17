c50b649c4d11be2298400f4c1d18e8a24ed24210
Marcos Ribeiro <marcos.ribeiro@lexisnexisrisk.com>
2021-12-17 16:03:49 -0500

    HPCC-27177 Enable logging feature for DESDL scripted service
    Signed-off-by: Marcos Ribeiro <marcos.ribeiro@lexisnexisrisk.com>

--- esp/services/esdl_svc_engine/esdl_binding.cpp
+++ esp/services/esdl_svc_engine/esdl_binding.cpp
@@ -1053,7 +1053,7 @@ void EsdlServiceImpl::handleServiceRequest(IEspContext &context,
             context.addTraceSummaryTimeStamp(LogNormal, "end-reqproc", TXSUMMARY_GRP_CORE|TXSUMMARY_GRP_ENTERPRISE);
             context.addTraceSummaryValue(LogNormal, "custom_fields.recCount", recordCount, TXSUMMARY_GRP_ENTERPRISE);
 
-            if(isPublishedQuery(implType))
+            if(isPublishedQuery(implType) || implType==EsdlMethodImplScript)
                 tgtctx.setown(createTargetContext(context, tgtcfg.get(), srvdef, mthdef, req));
 
             reqcontent.set(reqWriter->str());
@@ -1080,6 +1080,7 @@ void EsdlServiceImpl::handleServiceRequest(IEspContext &context,
             {
                 m_pEsdlTransformer->process(context, EsdlResponseMode, srvdef.queryName(), mthdef.queryName(), out, origResp.str(), txResultFlags, ns, schema_location);
                 runPostEsdlScript(context, scriptContext, srvdef, mthdef, out, txResultFlags, ns, schema_location);
+                logdata.set("<LogDatasets/>");
             }
         }
     }
