0f10de0d7f7259f8fd1c1fc440ba8fce41fc8591
Gordon Smith <GordonJSmith@gmail.com>
2022-03-04 06:30:10 +0000

    HPCC-27293 VCPKG Plugin Support
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- cmake_modules/vcpkgSetup.cmake
+++ cmake_modules/vcpkgSetup.cmake
@@ -2,7 +2,6 @@ set (USE_OPENLDAP OFF)
 
 set (USE_CBLAS OFF)
 
-
 set (USE_AZURE OFF)
 set (USE_AWS OFF)
 set (WSSQL_SERVICE OFF)
@@ -10,6 +9,21 @@ set (WSSQL_SERVICE OFF)
 set (USE_CASSANDRA OFF)
 set (USE_NATIVE_LIBRARIES ON)
 set (CMAKE_EXPORT_COMPILE_COMMANDS TRUE)
+
+# Plugins (enable one at a time)
+set (INCLUDE_PLUGINS OFF)
+set (INCLUDE_REMBED OFF)
+set (SUPRESS_V8EMBED ON)
+set (INCLUDE_MEMCACHED OFF)
+set (INCLUDE_REDIS OFF) # hard coded libhiredis.so
+set (INCLUDE_SQS ON)
+set (INCLUDE_MYSQLEMBED ON)
+set (INCLUDE_JAVAEMBED ON)
+set (INCLUDE_SQLITE3EMBED ON)
+set (INCLUDE_KAFKA ON)
+set (INCLUDE_COUCHBASEEMBED OFF) #make[2]: *** No rule to make target `plugins/couchbase/libcouchbase-build/lib/libcouchbase.so', needed by `Debug/libs/libcouchbaseembed.dylib'.  Stop.
+set (INCLUDE_SPARK ON)
+set (INCLUDE_EXAMPLEPLUGIN ON)
 
 # Additional
 option(BUILD_TESTS "Enable libgit2 tests (override libgit2 option)" ON)
