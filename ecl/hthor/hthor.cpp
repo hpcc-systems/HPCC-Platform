087df79d3a2c416168467d492be8ebf151c5c733
Jake Smith <jake.smith@lexisnexisrisk.com>
2022-03-24 19:48:44 +0000

    HPCC-27207 hthor corrupting output file if remote copied.
    Regression introduced by:
    https://github.com/hpcc-systems/HPCC-Platform/pull/15503
    
    The IFileIO was kept open (and unflushed) until/during
    the remote copy code (used when OUTPUT,CLUSTER(target) is
    used).
    
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

--- ecl/hthor/hthor.cpp
+++ ecl/hthor/hthor.cpp
@@ -445,11 +445,10 @@ void CHThorDiskWriteActivity::stop()
     outSeq->flush(NULL);
     if(blockcompressed)
         uncompressedBytesWritten = outSeq->getPosition();
-    updateWorkUnitResult(numRecords);
     close();
+    updateWorkUnitResult(numRecords);
     if((helper.getFlags() & (TDXtemporary | TDXjobtemp) ) == 0 && !agent.queryResolveFilesLocally())
         publish();
-    io.clear();
     incomplete = false;
     if(clusterHandler)
         clusterHandler->finish(file);
@@ -645,6 +644,12 @@ void CHThorDiskWriteActivity::close()
 {
     diskout.clear();
     outSeq.clear();
+    if (io)
+    {
+        io->flush();
+        numDiskWrites = io->getStatistic(StNumDiskWrites);
+        io.clear();
+    }
     if(clusterHandler)
         clusterHandler->copyPhysical(file, agent.queryWorkUnit()->getDebugValueBool("__output_cluster_no_copy_physical", false));
 }
@@ -743,11 +748,7 @@ void CHThorDiskWriteActivity::publish()
     if (helper.getFlags() & TDWrestricted)
         properties.setPropBool("restricted", true);
 
-    if (io)
-    {
-        numDiskWrites = io->getStatistic(StNumDiskWrites);
-        properties.setPropInt64("@numDiskWrites", numDiskWrites);
-    }
+    properties.setPropInt64("@numDiskWrites", numDiskWrites);
     StringBuffer lfn;
     expandLogicalFilename(lfn, mangledHelperFileName.str(), agent.queryWorkUnit(), agent.queryResolveFilesLocally(), false);
     CDfsLogicalFileName logicalName;
