b1c0d008c623d8aecdf5513b710105f95a292a9e
Jake Smith <jake.smith@lexisnexisrisk.com>
2022-05-18 12:29:30 +0100

    wip3

---
+++ dali/ft/ftslavelib.cmake
@@ -0,0 +1,55 @@
+################################################################################
+#    HPCC SYSTEMS software Copyright (C) 2022 HPCC Systems®.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+################################################################################
+
+
+# Component: ftslavelib
+#####################################################
+# Description:
+# ------------
+#    Cmake Input File for ftslavelib
+#####################################################
+
+project( ftslavelib ) 
+
+set (    SRCS 
+         ftslavelib.cpp 
+    )
+
+include_directories ( 
+         ${HPCC_SOURCE_DIR}/dali/base 
+         ${HPCC_SOURCE_DIR}/include 
+         ${HPCC_SOURCE_DIR}/jlib
+         ${HPCC_SOURCE_DIR}/security/shared
+         ${HPCC_SOURCE_DIR}/system/mp 
+    )
+
+ADD_DEFINITIONS( -D_USRDLL -DFTSLAVELIB_EXPORTS )
+
+HPCC_ADD_LIBRARY ( ftslavelib SHARED ${SRCS} )
+set_target_properties (ftslavelib PROPERTIES COMPILE_FLAGS -D_CONSOLE)
+install ( TARGETS ftslavelib RUNTIME DESTINATION ${EXEC_DIR} LIBRARY DESTINATION ${LIB_DIR} )
+target_link_libraries ( ftslavelib
+         dalibase 
+         dalift 
+         jlib
+         mp 
+         hrpc 
+         remote 
+    )
+
+if (NOT CONTAINERIZED)
+    target_link_libraries ( ftslavelib environment )
+endif()
