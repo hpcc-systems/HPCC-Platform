e36e2ebaa3fbe58ab1988f21c619414f22989f9c
Michael Gardner <michael.gardner@lexisnexisrisk.com>
2022-06-16 14:17:49 -0400

    HPCC-23392 Add dependencies for baremetal CA changes
    Signed-off-by: Michael Gardner <michael.gardner@lexisnexisrisk.com>

--- cmake_modules/dependencies/bionic.cmake
+++ cmake_modules/dependencies/bionic.cmake
@@ -1,5 +1,5 @@
 # Ubuntu 18.04
-SET_DEPENDENCIES ( CPACK_DEBIAN_PACKAGE_DEPENDS g++ openssh-client openssh-server expect rsync libapr1 python psmisc curl )
+SET_DEPENDENCIES ( CPACK_DEBIAN_PACKAGE_DEPENDS g++ openssh-client openssh-server expect rsync libapr1 python psmisc curl jq )
 
 if(SPARK)
     SET_DEPENDENCIES ( CPACK_DEBIAN_PACKAGE_DEPENDS "openjdk-8-jdk" )
