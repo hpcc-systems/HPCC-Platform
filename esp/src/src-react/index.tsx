42354d4806a56118d94421c9176f3914759dffa7
Jeremy Clements <Jeremy.Clements@lexisnexisrisk.com>
2022-09-19 13:32:22 -0400

    HPCC-28226 Ecl Watch error on password change
    Signed-off-by: Jeremy Clements <Jeremy.Clements@lexisnexisrisk.com>

--- esp/src/src-react/index.tsx
+++ esp/src/src-react/index.tsx
@@ -39,7 +39,7 @@ store.get("ModernMode", false).then(async modernMode => {
         const authType = await authTypeResp?.text() ?? "None";
         const userStore = cookieKeyValStore();
         const userSession = await userStore.getAll();
-        if (authType.indexOf("None") < 0 && (!userSession["ECLWatchUser"] || !userSession["Status"] || userSession["Status"] === "Locked")) {
+        if (authType.indexOf("None") < 0 && (!userSession["ESPAuthenticated"] && (!userSession["ECLWatchUser"] || !userSession["Status"] || userSession["Status"] === "Locked"))) {
             window.location.replace("#/login");
             import("./components/forms/Login").then(_ => {
                 try {
