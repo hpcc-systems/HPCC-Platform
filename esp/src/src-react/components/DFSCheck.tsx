2bb8cdf6e1e3cd32de510b336ab9c67560d14cf8
Kunal Aswani <Kunal.Aswani@lexisnexisrisk.com>
2022-10-28 11:28:47 -0400

    HPCC-28072 Add "DFS" option to Dali Admin
    DaliAdmin implementation of DFSCheck, DFSExists, DFSLS
    
    Signed-off-by: Kunal Aswani <Kunal.Aswani@lexisnexisrisk.com>

---
+++ esp/src/src-react/components/DFSCheck.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { DefaultButton, DetailsList, DetailsListLayoutMode, IColumn } from "@fluentui/react";
+import { SizeMe } from "react-sizeme";
+import { csvParse } from "d3-dsv";
+import { DaliService } from "@hpcc-js/comms";
+import { scopedLogger } from "@hpcc-js/util";
+import { TableGroup } from "./forms/Groups";
+import nlsHPCC from "src/nlsHPCC";
+import { HolyGrail } from "../layouts/HolyGrail";
+
+
+const logger = scopedLogger("src-react/components/DFSCheck.tsx");
+
+const myDaliService = new DaliService({ baseUrl: "" });
+
+interface DFSCheckProps {
+}
+
+export const DFSCheck: React.FunctionComponent<DFSCheckProps> = ({
+
+}) => {
+
+    const [columns, setColumns] = React.useState<IColumn[]>([]);
+    const [items, setItems] = React.useState<any[]>([]);
+    const [dfsCheck, setDFSCheck]  = React.useState(true);
+
+    const onSubmit = React.useCallback(() => {
+        myDaliService.DFSCheck({ DFSCheck: dfsCheck }).then(response => {
+            const data = csvParse(response.Result);
+            setColumns(data.columns.map((col, idx) => {
+                return {
+                    key: col,
+                    name: col,
+                    fieldName: col,
+                    minWidth: 100
+                };
+            }));
+            setItems(data);
+        }).catch(err => logger.error(err));
+    }, [dfsCheck]);
+
+    return <HolyGrail
+        header={<span><TableGroup fields={{
+            "DFSCheck": { label: nlsHPCC.DFSCheck, type: "checkbox", value: true ? true : false },
+        }} onChange={(id, value) => {
+            setDFSCheck(value);
+        }} /><DefaultButton onClick={onSubmit} text={nlsHPCC.Submit} /></span>}
+        main={<SizeMe monitorHeight>{({ size }) => {
+            const height = `${size.height}px`;
+            return <div style={{ position: "relative", width: "100%", height: "100%" }}>
+                <div style={{ position: "absolute", width: "100%", height: `${size.height}px` }}>
+                    <DetailsList compact={true}
+                        items={items}
+                        columns={columns}
+                        setKey="key"
+                        layoutMode={DetailsListLayoutMode.justified}
+                        selectionPreservedOnEmptyClick={true}
+                        styles={{ root: { height, minHeight: height, maxHeight: height } }}
+                    />
+                </div>
+            </div>;
+        }}</SizeMe>}
+    />;
+}; 
---
+++ esp/src/src-react/components/DFSExists.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { DefaultButton, DetailsList, DetailsListLayoutMode, IColumn } from "@fluentui/react";
+import { SizeMe } from "react-sizeme";
+import { csvParse } from "d3-dsv";
+import { DaliService, Result } from "@hpcc-js/comms";
+import { scopedLogger } from "@hpcc-js/util";
+import { TableGroup } from "./forms/Groups";
+import nlsHPCC from "src/nlsHPCC";
+import { HolyGrail } from "../layouts/HolyGrail";
+
+const logger = scopedLogger("src-react/components/DFSExist.tsx");
+
+const myDaliService = new DaliService({ baseUrl: "" });
+
+interface DFSExistProps {
+}
+
+export const DFSExists: React.FunctionComponent<DFSExistProps> = ({
+
+}) => {
+
+    const [columns, setColumns] = React.useState<IColumn[]>([]);
+    const [items, setItems] = React.useState<any[]>([]);
+    const [fileName, setFileName] = React.useState<string>("");
+
+    const onSubmit = React.useCallback(() => {
+        myDaliService.DFSExists({ FileName: fileName }).then(response => {
+            const data = csvParse(Result);
+            setColumns(data.columns.map((col, idx) => {
+                return {
+                    key: col,
+                    name: col,
+                    fileName: col,
+                    minWidth: 100
+                };
+            }));
+            setItems(data);
+        }).catch(err => logger.error(err));
+    }, [fileName]);
+
+    return <HolyGrail
+        header={<span><TableGroup fields={{
+            "FileName": { label: nlsHPCC.FileName, type: "string", value: fileName },
+        }} onChange={(id, value) => {
+            setFileName(value);
+        }} /><DefaultButton onClick={onSubmit} text={nlsHPCC.Submit} /></span>}
+        main={<SizeMe monitorHeight>{({ size }) => {
+            const height = `${size.height}px`;
+            return <div style={{ position: "relative", width: "100%", height: "100%" }}>
+                <div style={{ position: "absolute", width: "100%", height: `${size.height}px` }}>
+                    <DetailsList compact={true}
+                        items={items}
+                        columns={columns}
+                        setKey="key"
+                        layoutMode={DetailsListLayoutMode.justified}
+                        selectionPreservedOnEmptyClick={true}
+                        styles={{ root: { height, minHeight: height, maxHeight: height } }}
+                    />
+                </div>
+            </div>;
+        }}</SizeMe>}
+    />;
+}; 
