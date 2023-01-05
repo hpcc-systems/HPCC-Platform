08c4fd11c1b61bc7786c41dd3e909c7640f40823
Gavin Halliday <gavin.halliday@lexisnexis.com>
2023-01-05 10:39:44 +0000

    HPCC-28753 Release git alias and tagging scripts
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

---
+++ tools/tagging/gogold.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# This script is used to tag a set of repos as gold.
+#
+# <path>/gogold.sh <version>
+
+if [[ -z $1 ]] ; then
+   echo Expected a version number
+   exit 2
+fi
+
+if [[ -z $all ]]; then
+   echo "List of repos not configured (environment variable 'all')"
+   exit 2
+fi
+
+if [[ -z $gitroot ]]; then
+   echo "Root git directory not specified (environment variable 'gitroot')"
+   exit 2
+fi
+
+hpccdir=$(dirname -- "$( readlink -f -- '$0'; )")/../..
+gitroot="${gitroot/#\~/$HOME}"
+
+echo Go gold with candidate-$1
+
+for f in $all ; do cd "${gitroot}/$f" ; git fetch origin ; git checkout candidate-$1 ; git su --force ; done
+echo Press any key to go gold
+read -n 1 -s
+
+for f in $all ; do cd $gitroot/$f ; $hpccdir/cmake_modules/go_gold.sh ; done
---
+++ tools/tagging/upmerge.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+if [[ -z $1 ]] ; then
+   echo Expected a source version number
+   exit 2
+fi
+
+if [[ -z $2 ]] ; then
+   echo Expected a target version number
+   exit 2
+fi
+
+if [[ -z $all ]]; then
+   echo "List of repos not configured (environment variable 'all')"
+   exit 2
+fi
+
+if [[ -z $gitroot ]]; then
+   echo "Root git directory not specified (environment variable 'gitroot')"
+   exit 2
+fi
+
+scriptdir=$(dirname -- "$( readlink -f -- ""$0""; )")
+hpccdir=$scriptdir/../..
+
+echo Upmerge changes from candidate-$1 to $2
+
+for f in $all ; do
+   cd $gitroot/$f
+   git fetch origin
+   git log -1 --oneline origin/candidate-$1 > /dev/null 2>&1
+   if [ $? -ne 0 ]; then
+      echo "$f: Source branch origin/candidate-$1 does not exist"
+      exit 1
+   fi
+
+   git log -1 --oneline origin/$2 > /dev/null 2>&1
+   if [ $? -ne 0 ]; then
+      echo "$f: Target branch origin/$2 does not exist"
+      exit 1
+   fi
+
+   git checkout $2
+   if [ $? -ne 0 ]; then
+      echo "$f: Target branch $2 failed to check out"
+      exit 1
+   fi
+   git merge origin/$2 --ff-only
+   if [ $? -ne 0 ]; then
+      echo "$f: Target branch $2 is inconsistent with origin/$2"
+      exit 1
+   fi
+
+   git submodule update --recursive --init --force
+done
+
+echo "----------- upmerging ---------"
+
+for f in $all ; do
+   echo ------- $f --------
+
+   cd $gitroot/$f
+   git merge origin/candidate-$1 --no-commit;
+   $scriptdir/git-unupmerge
+
+   if [[ -f "version.cmake" ]]; then
+       git co HEAD -- version.cmake
+   fi
+   if [[ -f "pom.xml" ]]; then
+       git co HEAD -- pom.xml
+   fi
+   if [[ -f "commons-hpcc/pom.xml" ]]; then
+       git co HEAD -- commons-hpcc/pom.xml
+       git co HEAD -- dfsclient/pom.xml
+       git co HEAD -- wsclient/pom.xml
+   fi
+
+   CONFLICTS=$(git ls-files -u | wc -l)
+   if [ "$CONFLICTS" -eq 0 ] ; then
+       git commit -s --no-edit
+       git push origin $2
+   else
+       echo ************ $f is incomplete **************
+   fi
+done
