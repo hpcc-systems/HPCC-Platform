edfe1765a3f33220ee95e0ecf64acff041afc35f
Gavin Halliday <gavin.halliday@lexisnexis.com>
2023-03-09 16:06:00 +0000

    Community Edition 8.8.44-rc2 Release Candidate 2
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -3,9 +3,9 @@
 ###
 set ( HPCC_NAME "Community Edition" )
 set ( HPCC_PROJECT "community" )
-set ( HPCC_MAJOR 8 )
+set ( HPCC_MINOR 8 )
 set ( HPCC_MINOR 8 )
 set ( HPCC_POINT 44 )
 set ( HPCC_MATURITY "rc" )
-set ( HPCC_SEQUENCE 1 )
+set ( HPCC_SEQUENCE 2 )
 ###
