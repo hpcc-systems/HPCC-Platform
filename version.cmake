169a1ea5ebf141204d9b279ffcd6bb2d0fbd36ad
Gavin Halliday <gavin.halliday@lexisnexis.com>
2023-04-03 17:23:55 +0100

    Community Edition 9.0.0-1 Gold
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -6,6 +6,6 @@ set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 9 )
 set ( HPCC_MINOR 0 )
 set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "rc" )
-set ( HPCC_SEQUENCE 4 )
+set ( HPCC_MATURITY "release" )
+set ( HPCC_SEQUENCE 1 )
 ###
