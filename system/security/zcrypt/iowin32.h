29d1f4adc8399879b633c6181eb1568def6289d9
Russ Whitehead <russwhitehead@yahoo.com>
2023-04-05 15:17:20 -0400

    HPCC-29294 Use latest version of ZLIB helper files
    The ZLib package offering includes several source files that wrap complex
    ZLib functions into a more manageable interface. These files have been copied
    into the HPCC-Platform repo and used for many years, and have become outdated
    from the latest available. This PR replaces those files with the latest,
    more current ones from ZLib version 1.2.13.  The source file headers include
    a reference to the location of these files, for future updates.
    
    Signed-off-by: Russ Whitehead <russwhitehead@yahoo.com>

--- system/security/zcrypt/iowin32.h
+++ system/security/zcrypt/iowin32.h
@@ -1,10 +1,36 @@
+/*##############################################################################
+
+    HPCC SYSTEMS software Copyright (C) 2023 HPCC Systems®.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+############################################################################## */
+//Following wrapper/helper file is part of the ZLib offering
+//Located at https://zlib.net/ and/or https://github.com/madler/zlib
+//version 1.2.13
+//Location zlib-1.2.13/contrib/minizip/
+//#################################################################
+
 /* iowin32.h -- IO base function header for compress/uncompress .zip
-   files using zlib + zip or unzip API
-   This IO API version uses the Win32 API (for Microsoft Windows)
+     Version 1.1, February 14h, 2010
+     part of the MiniZip project - ( http://www.winimage.com/zLibDll/minizip.html )
 
-   Version 1.01e, February 12th, 2005
+         Copyright (C) 1998-2010 Gilles Vollant (minizip) ( http://www.winimage.com/zLibDll/minizip.html )
 
-   Copyright (C) 1998-2005 Gilles Vollant
+         Modifications for Zip64 support
+         Copyright (C) 2009-2010 Mathias Svensson ( http://result42.com )
+
+         For more info read MiniZip_info.txt
+
 */
 
 #include <windows.h>
@@ -15,6 +41,9 @@ extern "C" {
 #endif
 
 void fill_win32_filefunc OF((zlib_filefunc_def* pzlib_filefunc_def));
+void fill_win32_filefunc64 OF((zlib_filefunc64_def* pzlib_filefunc_def));
+void fill_win32_filefunc64A OF((zlib_filefunc64_def* pzlib_filefunc_def));
+void fill_win32_filefunc64W OF((zlib_filefunc64_def* pzlib_filefunc_def));
 
 #ifdef __cplusplus
 }
