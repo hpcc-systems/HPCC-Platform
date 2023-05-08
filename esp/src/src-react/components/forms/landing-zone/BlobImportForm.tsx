fb0a63bd258879a43fe0c85e9d075a3de6927bc6
Jeremy Clements <Jeremy.Clements@lexisnexisrisk.com>
2023-05-08 17:11:49 -0400

    HPCC-29487 ECL Watch fix Blob import source path validation
    Signed-off-by: Jeremy Clements <Jeremy.Clements@lexisnexisrisk.com>

--- esp/src/src-react/components/forms/landing-zone/BlobImportForm.tsx
+++ esp/src/src-react/components/forms/landing-zone/BlobImportForm.tsx
@@ -213,7 +213,7 @@ export const BlobImportForm: React.FunctionComponent<BlobImportFormProps> = ({
                                     rules={{
                                         required: nlsHPCC.ValidationErrorTargetNameRequired,
                                         pattern: {
-                                            value: /^(?:[\/\\]?[-a-z0-9_]+[\/\\])+([-a-z0-9 _\.]+)$/i,
+                                            value: /^(?:[\/\\]?[-a-z0-9\*_]+[\/\\])+([-a-z0-9 \*_\.]+)$/i,
                                             message: nlsHPCC.ValidationErrorTargetNameInvalid
                                         }
                                     }}
