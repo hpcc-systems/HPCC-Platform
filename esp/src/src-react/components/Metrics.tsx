97560c76d41dc7867c62f575c31f6c53ee7adcbf
Gordon Smith <GordonJSmith@gmail.com>
2023-06-19 17:17:20 +0100

    HPCC-29540 Format metric properties
    Gordon Smith <GordonJSmith@gmail.com>

--- esp/src/src-react/components/Metrics.tsx
+++ esp/src/src-react/components/Metrics.tsx
@@ -28,6 +28,82 @@ const defaultUIState = {
     hasSelection: false
 };
 
+interface PropertyValue {
+    Key: string;
+    Value: string;
+    Avg: string;
+    Min: string;
+    Max: string;
+    Delta: string;
+    StdDev: string;
+    SkewMin: string;
+    SkewMax: string;
+    NodeMin: string;
+    NodeMax: string;
+}
+
+const regex = /[A-Z][a-z]*/g;
+function splitKey(key: string): [string, string, string] | null {
+
+    // Related properties  ---
+    const relatedProps = ["SkewMin", "SkewMax", "NodeMin", "NodeMax"];
+    for (const prop of relatedProps) {
+        const index = key.indexOf(prop);
+        if (index === 0) {
+            const before = "";
+            const after = key.slice(index + prop.length);
+            return [before, prop, after];
+        }
+    }
+
+    // Primary properties  ---
+    const keyParts = key.match(regex);
+    if (keyParts?.length) {
+        const before = keyParts.shift();
+        const newKey = keyParts.join("");
+        const props = ["Avg", "Min", "Max", "Delta", "StdDev"];
+        for (const keyword of props) {
+            const index = newKey.indexOf(keyword);
+            if (index === 0) {
+                const after = newKey.slice(index + keyword.length);
+                return [before, keyword, after];
+            }
+        }
+        // Not an aggregate property  ---
+        return [before, "", newKey];
+    }
+
+    // No match found  ---
+    return ["", "", key];
+}
+
+function formatValue(item: IScope, key: string): string {
+    return item.__formattedProps?.[key] ?? item[key] ?? "";
+}
+
+type DedupProperties = { [key: string]: boolean };
+
+function formatValues(item: IScope, key: string, dedup: DedupProperties): PropertyValue | null {
+    const keyParts = splitKey(key);
+    if (!dedup[keyParts[2]]) {
+        dedup[keyParts[2]] = true;
+        return {
+            Key: `${keyParts[0]}${keyParts[2]}`,
+            Value: formatValue(item, `${keyParts[0]}${keyParts[2]}`),
+            Avg: formatValue(item, `${keyParts[0]}Avg${keyParts[2]}`),
+            Min: formatValue(item, `${keyParts[0]}Min${keyParts[2]}`),
+            Max: formatValue(item, `${keyParts[0]}Max${keyParts[2]}`),
+            Delta: formatValue(item, `${keyParts[0]}Delta${keyParts[2]}`),
+            StdDev: formatValue(item, `${keyParts[0]}StdDev${keyParts[2]}`),
+            SkewMin: formatValue(item, `SkewMin${keyParts[2]}`),
+            SkewMax: formatValue(item, `SkewMax${keyParts[2]}`),
+            NodeMin: formatValue(item, `NodeMin${keyParts[2]}`),
+            NodeMax: formatValue(item, `NodeMax${keyParts[2]}`)
+        };
+    }
+    return null;
+}
+
 interface MetricsProps {
     wuid: string;
     parentUrl?: string;
@@ -331,16 +407,20 @@ export const Metrics: React.FunctionComponent<MetricsProps> = ({
     //  Props Table  ---
     const propsTable = useConst(() => new Table()
         .id("propsTable")
-        .columns([nlsHPCC.Property, nlsHPCC.Value])
-        .columnWidth("none")
+        .columns([nlsHPCC.Property, nlsHPCC.Value, "Avg", "Min", "Max", "Delta", "StdDev", "SkewMin", "SkewMax", "NodeMin", "NodeMax"])
+        .columnWidth("auto")
     );
 
     const updatePropsTable = React.useCallback((selection: IScope[]) => {
         const props = [];
         selection.forEach((item, idx) => {
+            const dedup: DedupProperties = {};
             for (const key in item) {
                 if (key.indexOf("__") !== 0) {
-                    props.push([key, item[key]]);
+                    const row = formatValues(item, key, dedup);
+                    if (row) {
+                        props.push([row.Key, row.Value, row.Avg, row.Min, row.Max, row.Delta, row.StdDev, row.SkewMin, row.SkewMax, row.NodeMin, row.NodeMax]);
+                    }
                 }
             }
             if (idx < selection.length - 1) {
