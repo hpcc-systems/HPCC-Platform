4a100ff7a767b95e8aceb3193673f5113bbfc4bb
Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexisrisk.com>
2023-07-10 09:14:03 -0400

    HPCC-29332 Add opentelemetry-cpp to vcpkg list
    - WIP
    
    Signed-off-by: Rodrigo Pastrana <rodrigo.pastrana@lexisnexisrisk.com>

---
+++ system/tracing/tracer.cpp
@@ -0,0 +1,112 @@
+/*##############################################################################
+
+    HPCC SYSTEMS software Copyright (C) 2023 HPCC Systems®.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+############################################################################## */
+
+#include "opentelemetry/ext/http/client/http_client_factory.h"
+#include "opentelemetry/ext/http/common/url_parser.h"
+#include "opentelemetry/trace/semantic_conventions.h"
+
+#include "tracer.hpp"
+
+namespace
+{
+
+using namespace opentelemetry::trace;
+namespace http_client = opentelemetry::ext::http::client;
+namespace context     = opentelemetry::context;
+namespace nostd       = opentelemetry::nostd;
+
+void sendRequest(const std::string &url)
+{
+  //auto http_client = http_client::HttpClientFactory::CreateSync();
+
+  // start active span
+  StartSpanOptions options;
+  options.kind = SpanKind::kClient;  // client
+  opentelemetry::ext::http::common::UrlParser url_parser(url);
+
+  std::string span_name = url_parser.path_;
+  auto span             = get_tracer("http-client")
+                  ->StartSpan(span_name,
+                              {{SemanticConventions::kHttpUrl, url_parser.url_},
+                               {SemanticConventions::kHttpScheme, url_parser.scheme_},
+                               {SemanticConventions::kHttpMethod, "GET"}},
+                              options);
+  auto scope = get_tracer("http-client")->WithActiveSpan(span);
+
+  // inject current context into http header
+  auto current_ctx = context::RuntimeContext::GetCurrent();
+  HttpTextMapCarrier<http_client::Headers> carrier;
+  auto prop = context::propagation::GlobalTextMapPropagator::GetGlobalPropagator();
+  prop->Inject(carrier, current_ctx);
+
+  // send http request
+  //http_client::Result result = http_client->GetNoSsl(url, carrier.headers_);
+  //if (result)
+  {
+    // set span attributes
+    //auto status_code = result.GetResponse().GetStatusCode();
+    auto status_code = 200;
+    span->SetAttribute(SemanticConventions::kHttpStatusCode, status_code);
+    //result.GetResponse().ForEachHeader(
+    //    [&span](nostd::string_view header_name, nostd::string_view header_value) {
+    //      span->SetAttribute("http.header." + std::string(header_name.data()), header_value);
+    //      return true;
+    //    });
+
+    if (status_code >= 400)
+    {
+      span->SetStatus(StatusCode::kError);
+    }
+  }
+  //else
+  //{
+  //  span->SetStatus(
+  //      StatusCode::kError,
+  //      "Response Status :" +
+  //          std::to_string(
+  //              static_cast<typename std::underlying_type<http_client::SessionState>::type>(
+  //                  result.GetSessionState())));
+  //}
+  // end span and export data
+  span->End();
+}
+
+}  // namespace
+
+int main(int argc, char *argv[])
+{
+  InitTracer();
+  constexpr char default_host[]   = "localhost";
+  constexpr char default_path[]   = "/helloworld";
+  constexpr uint16_t default_port = 8800;
+  uint16_t port;
+
+  // The port the validation service listens to can be specified via the command line.
+  if (argc > 1)
+  {
+    port = (uint16_t)(atoi(argv[1]));
+  }
+  else
+  {
+    port = default_port;
+  }
+
+  std::string url = "http://" + std::string(default_host) + ":" + std::to_string(port) +
+                    std::string(default_path);
+  sendRequest(url);
+  CleanupTracer();
+}
