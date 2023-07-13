ffd9db71d565f453e4a107af390b75eaba778d11
Jake Smith <jake.smith@lexisnexisrisk.com>
2023-07-13 19:23:23 +0100

    HPCC-28924 Avoid spurious workunit wait state
    Thor uses as a transitional state between itself and the agent.
    However, if the state changed outside of Thor, e.g. due to
    failure, Thor would spuriously have set the state to wait.
    Ensure it is only set if it is in an expected state, otherwise
    set to fail.
    
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

--- thorlcr/master/thgraphmanager.cpp
+++ thorlcr/master/thgraphmanager.cpp
@@ -860,8 +860,8 @@ void CJobManager::reply(IConstWorkUnit *workunit, const char *wuid, IException *
             we->setExceptionMessage(errStr);
             we->setExceptionSource("thormasterexception");
             we->setExceptionCode(e->errorCode());
-
-            w->setState(WUStateWait);
+            WUState newState = (WUStateRunning == w->getState()) ? WUStateWait : WUStateFailed;
+            w->setState(newState);
         }
         return;
     }
@@ -1342,9 +1342,10 @@ void thorMain(ILogMsgHandler *logHandler, const char *wuid, const char *graphNam
                         jobManager->execute(workunit, currentWuid, currentGraphName, dummyAgentEp);
                         IException *e = jobManager->queryExitException();
                         Owned<IWorkUnit> w = &workunit->lock();
+                        WUState newState = (WUStateRunning == w->getState()) ? WUStateWait : WUStateFailed;
                         if (e)
                         {
-                            if (WUStateWait != w->getState()) // if set already, can only mean exception has been set already (see CJobManager::reply)
+                            if (WUStateWait != w->getState()) // if set already, CJobManager::reply may have already set to WUStateWait
                             {
                                 Owned<IWUException> we = w->createException();
                                 we->setSeverity(SeverityInformation);
@@ -1354,7 +1355,7 @@ void thorMain(ILogMsgHandler *logHandler, const char *wuid, const char *graphNam
                                 we->setExceptionSource("thormasterexception");
                                 we->setExceptionCode(e->errorCode());
 
-                                w->setState(WUStateWait);
+                                w->setState(newState);
                             }
                             break;
                         }
@@ -1362,7 +1363,7 @@ void thorMain(ILogMsgHandler *logHandler, const char *wuid, const char *graphNam
                         if (!multiJobLinger && lingerPeriod)
                             w->setDebugValue(instance, "1", true);
 
-                        w->setState(WUStateWait);
+                        w->setState(newState);
                     }
                     currentGraphName.clear();
 
