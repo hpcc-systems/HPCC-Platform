d492adb18b538b8a77ecaed0a220411b02151ab9
Gordon Smith <GordonJSmith@gmail.com>
2023-11-03 14:48:38 +0000

    Revert "WIP"
    This reverts commit a64b53624405b9c9bf0e505a4182bf0eb37b36d1.

--- esp/bindings/http/platform/httpprot.hpp
+++ esp/bindings/http/platform/httpprot.hpp
@@ -47,7 +47,7 @@ private:
     bool m_is_ssl;
     ISecureSocketContext* m_ssctx;
     IPersistentHandler* m_persistentHandler = nullptr;
-    bool m_shouldClose = true;
+    bool m_shouldClose = false;
     IHttpServerService* m_httpserver = nullptr;
     bool m_socketReturned = false;
     bool m_processAborted = false;
