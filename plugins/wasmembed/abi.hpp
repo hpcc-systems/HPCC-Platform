5c15a1a99fc5302ae5170896b8cf0ffddc6f9e70
Gordon Smith <GordonJSmith@gmail.com>
2023-11-22 15:45:10 +0000

    HPCC-30909 WASMEMBED build failing on OSX + Ubuntu-23.10
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- plugins/wasmembed/abi.hpp
+++ plugins/wasmembed/abi.hpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <sstream>
+
 #include <wasmtime.hh>
 
 std::tuple<uint32_t /*ptr*/, std::string /*encoding*/, uint32_t /*byte length*/> load_string(const wasmtime::Span<uint8_t> &data, uint32_t ptr);
