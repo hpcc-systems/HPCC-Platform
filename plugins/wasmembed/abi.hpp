9b3db2dc36deca8fb8135d8d860f2a461e464fed
Gordon Smith <GordonJSmith@gmail.com>
2023-12-19 14:11:53 +0000

    HPCC-31034 wasmembed has problematic includes
    Causes an issue with Ubuntu 23.10 + upstream #include <chrono>
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- plugins/wasmembed/abi.hpp
+++ plugins/wasmembed/abi.hpp
@@ -1,6 +1,4 @@
-#include <string>
-#include <sstream>
-
+#include <span>
 #include <wasmtime.hh>
 
 std::tuple<uint32_t /*ptr*/, std::string /*encoding*/, uint32_t /*byte length*/> load_string(const wasmtime::Span<uint8_t> &data, uint32_t ptr);
