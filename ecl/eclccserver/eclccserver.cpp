ee292315a6d3e578dec408f110425def408f5d9f
Jake Smith <jake.smith@lexisnexisrisk.com>
2024-01-26 15:44:47 +0000

    HPCC-31157 eclccserver option quoting fix
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

--- ecl/eclccserver/eclccserver.cpp
+++ ecl/eclccserver/eclccserver.cpp
@@ -317,27 +317,36 @@ class EclccCompileThread : implements IPooledThread, implements IErrorReporter, 
                     throw MakeStringException(0, "eclcc-hook option can not be set per-workunit");  // for security reasons
                 eclccProgName.set(value);
             }
-            else if (stricmp(optName, "compileOption") == 0)
-                eclccCmd.appendf(" -Wc,%s", value);
-            else if (stricmp(optName, "linkOption") == 0)
-                eclccCmd.appendf(" -Wl,%s", value);
-            else if (stricmp(optName, "includeLibraryPath") == 0)
-                eclccCmd.appendf(" -I%s", value);
-            else if (stricmp(optName, "libraryPath") == 0)
-                eclccCmd.appendf(" -L%s", value);
-            else if (strnicmp(optName, "-allow", 6)==0)
-            {
-                if (isLocal)
-                    throw MakeStringException(0, "eclcc-allow option can not be set per-workunit");  // for security reasons
-                eclccCmd.appendf(" -%s=%s", optName.get(), value);
-            }
-            else if (*optName == 'd')
-            {
-                //Short term work around for the problem that all debug names get lower-cased
-                eclccCmd.appendf(" -D%s=%s", optName.get()+1, value);
-            }
             else
-                eclccCmd.appendf(" -%s=%s", optName.get(), value);
+            {
+                StringBuffer quotedValue;
+                if (value && (*value != '\'' && *value != '"'))
+                {
+                    quotedValue.append('\'').append(value).append('\'');
+                    value = quotedValue.str();
+                }
+                if (stricmp(optName, "compileOption") == 0)
+                    eclccCmd.appendf(" -Wc,%s", value);
+                else if (stricmp(optName, "linkOption") == 0)
+                    eclccCmd.appendf(" -Wl,%s", value);
+                else if (stricmp(optName, "includeLibraryPath") == 0)
+                    eclccCmd.appendf(" -I%s", value);
+                else if (stricmp(optName, "libraryPath") == 0)
+                    eclccCmd.appendf(" -L%s", value);
+                else if (strnicmp(optName, "-allow", 6)==0)
+                {
+                    if (isLocal)
+                        throw MakeStringException(0, "eclcc-allow option can not be set per-workunit");  // for security reasons
+                    eclccCmd.appendf(" -%s=%s", optName.get(), value);
+                }
+                else if (*optName == 'd')
+                {
+                    //Short term work around for the problem that all debug names get lower-cased
+                    eclccCmd.appendf(" -D%s=%s", optName.get()+1, value);
+                }
+                else
+                    eclccCmd.appendf(" -%s=%s", optName.get(), value);
+            }
         }
         else if (strchr(option, '-'))
         {
