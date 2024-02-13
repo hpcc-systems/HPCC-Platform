14ecf3f454ec8b180b998755b9c3afe7404e4715
Gordon Smith <GordonJSmith@gmail.com>
2024-02-13 14:06:12 +0000

    HPCC-31235 Merge "old" roxie stats into "new" query metrics
    Refactor Query Graph code into @hpcc-js/comms
    
    Gordon Smith <GordonJSmith@gmail.com>

--- esp/src/src-react/hooks/metrics.ts
+++ esp/src/src-react/hooks/metrics.ts
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { useConst, useForceUpdate } from "@fluentui/react-hooks";
 import { WUDetails, WUDetailsMeta, WorkunitsService, IScope } from "@hpcc-js/comms";
+import { scopedLogger } from "@hpcc-js/util";
 import { userKeyValStore } from "src/KeyValStore";
 import { useWorkunit } from "./workunit";
+import { useQuery } from "./query";
 import { useCounter } from "./util";
-import { scopedLogger } from "@hpcc-js/util";
 
 const logger = scopedLogger("src-react\hooks\metrics.ts");
 
@@ -169,4 +170,77 @@ export function useWorkunitMetrics(
     }, [workunit, state, count, scopeFilter, nestedFilter]);
 
     return [data, columns, activities, properties, measures, scopeTypes, status, increment];
+}
+
+export function useQueryMetrics(
+    querySet: string,
+    queryId: string,
+    scopeFilter: WUDetails.RequestNS.ScopeFilter = scopeFilterDefault,
+    nestedFilter: WUDetails.RequestNS.NestedFilter = nestedFilterDefault
+): [IScope[], { [id: string]: any }, WUDetailsMeta.Activity[], WUDetailsMeta.Property[], string[], string[], FetchStatus, () => void] {
+
+    const [query, state, _refresh] = useQuery(querySet, queryId);
+    const [data, setData] = React.useState<IScope[]>([]);
+    const [columns, setColumns] = React.useState<{ [id: string]: any }>([]);
+    const [activities, setActivities] = React.useState<WUDetailsMeta.Activity[]>([]);
+    const [properties, setProperties] = React.useState<WUDetailsMeta.Property[]>([]);
+    const [measures, setMeasures] = React.useState<string[]>([]);
+    const [scopeTypes, setScopeTypes] = React.useState<string[]>([]);
+    const [status, setStatus] = React.useState<FetchStatus>(FetchStatus.COMPLETE);
+    const [count, increment] = useCounter();
+
+    React.useEffect(() => {
+        setStatus(FetchStatus.STARTED);
+        query?.fetchDetailsNormalized({
+            ScopeFilter: scopeFilter,
+            NestedFilter: nestedFilter,
+            PropertiesToReturn: {
+                AllScopes: true,
+                AllAttributes: true,
+                AllProperties: true,
+                AllNotes: true,
+                AllStatistics: true,
+                AllHints: true
+            },
+            ScopeOptions: {
+                IncludeId: true,
+                IncludeScope: true,
+                IncludeScopeType: true,
+                IncludeMatchedScopesInResults: true
+            },
+            PropertyOptions: {
+                IncludeName: true,
+                IncludeRawValue: true,
+                IncludeFormatted: true,
+                IncludeMeasure: true,
+                IncludeCreator: false,
+                IncludeCreatorType: false
+            }
+        }).then(response => {
+            setData(response?.data);
+            setColumns(response?.columns);
+            setActivities(response?.meta?.Activities?.Activity || []);
+            setProperties(response?.meta?.Properties?.Property || []);
+            setMeasures(response?.meta?.Measures?.Measure || []);
+            setScopeTypes(response?.meta?.ScopeTypes?.ScopeType || []);
+        }).catch(e => {
+            logger.error(e);
+        }).finally(() => {
+            setStatus(FetchStatus.COMPLETE);
+        });
+    }, [query, state, count, scopeFilter, nestedFilter]);
+
+    return [data, columns, activities, properties, measures, scopeTypes, status, increment];
+}
+
+export function useWUQueryMetrics(
+    wuid: string,
+    querySet: string,
+    queryId: string,
+    scopeFilter: WUDetails.RequestNS.ScopeFilter = scopeFilterDefault,
+    nestedFilter: WUDetails.RequestNS.NestedFilter = nestedFilterDefault
+): [IScope[], { [id: string]: any }, WUDetailsMeta.Activity[], WUDetailsMeta.Property[], string[], string[], FetchStatus, () => void] {
+    const wuMetrics = useWorkunitMetrics(wuid, scopeFilter, nestedFilter);
+    const queryMetrics = useQueryMetrics(querySet, queryId, scopeFilter, nestedFilter);
+    return querySet && queryId ? [...queryMetrics] : [...wuMetrics];
 }
