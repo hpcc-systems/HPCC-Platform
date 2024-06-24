aad4f52027a6e0cc7eb209343294b9aed5f6f4c0
Gordon Smith <GordonJSmith@gmail.com>
2024-06-24 15:15:43 +0100

    Split off 9.8.0
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- version.cmake
+++ version.cmake
@@ -4,9 +4,9 @@
 set ( HPCC_NAME "Community Edition" )
 set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 9 )
-set ( HPCC_MINOR 7 )
-set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "trunk" )
+set ( HPCC_MINOR 8 )
+set ( HPCC_POINT 1 )
+set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 0 )
-set ( HPCC_TAG_TIMESTAMP "2024-03-08T17:36:05Z" )
+set ( HPCC_TAG_TIMESTAMP "2024-06-24T14:15:43Z" )
 ###
