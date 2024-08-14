ebbdde4033b90c8cdd9ed51190b227b8eb504d94
Richard Chapman <rchapman@hpccsystems.com>
2024-08-14 14:10:05 +0100

    HPCC-32435 Fix variable not used errors
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- esp/test/httptest/httptest.cpp
+++ esp/test/httptest/httptest.cpp
@@ -952,7 +952,6 @@ void CHttpProxyThread::start()
 int CHttpProxyThread::run()
 {
     Thread::Link();
-
     int ret = 0;
     try
     {
@@ -1102,7 +1101,7 @@ int CHttpProxyThread::run()
 
     Thread::Release();
 
-    return 0;
+    return ret;
 }
 
 int CHttpProxyThread::readline(ISocket* socket, char* buf, int bufsize, bool& socketclosed)
