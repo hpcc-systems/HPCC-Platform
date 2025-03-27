7d0501e876abe6af368b5596d1b2ef688b510345
Tim Klemm <Tim.Klemm@lexisnexisrisk.com>
2025-03-27 07:40:17 -0400

    HPCC-33666: Define a command line tool to manipulate binare event data files
    `evtool dump <filename>` will open and parse a binary file and write the
    contents to standard out in an unstructured text format.
    
    Signed-off-by: Tim Klemm <Tim.Klemm@lexisnexisrisk.com>

---
+++ tools/evtool/evtool.hpp
@@ -0,0 +1,39 @@
+/*##############################################################################
+
+    Copyright (C) 2024 HPCC Systems®.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+############################################################################## */
+
+#pragma once
+
+#include "evtool.h"
+
+// Abstract implementation of the command interface. `dispatch` is implemented with the help of
+// several virtual and abstract methods. `usage` remains for subclasses to implement.
+class CEvToolCommand : public CInterfaceOf<IEvToolCommand>
+{
+public:
+    virtual bool isGoodRequest() = 0;
+    virtual int  doRequest() = 0;
+public:
+    virtual int dispatch(int argc, const char* argv[], int pos) override;
+protected:
+    bool isHelp = false;
+public:
+    virtual bool accept(const char* arg);
+    virtual bool acceptTerseOption(char opt);
+    virtual bool acceptVerboseOption(const char* opt);
+    virtual bool acceptParameter(const char* arg);
+    void usagePrefix(int argc, const char* argv[], int pos, std::ostream& out);
+};
