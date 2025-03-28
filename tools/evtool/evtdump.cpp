f067e536161c762258769ac2c163c9c8c5fccba1
Tim Klemm <Tim.Klemm@lexisnexisrisk.com>
2025-03-28 07:56:28 -0400

    HPCC-33667: Support XML-formatted event data dumps
    `evtool dump -x <filename>` will assemble and serialize a property tree.
    
    `evtool dump -X <filename>` will generate XML directly from visited data.
    
    Signed-off-by: Tim Klemm <Tim.Klemm@lexisnexisrisk.com>

--- tools/evtool/evtdump.cpp
+++ tools/evtool/evtdump.cpp
@@ -1,6 +1,6 @@
 /*##############################################################################
 
-    Copyright (C) 2024 HPCC Systems®.
+    Copyright (C) 2025 HPCC Systems®.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -16,16 +16,158 @@
 ############################################################################## */
 
 #include "evtool.hpp"
+#include "evtptree.hpp"
 #include "jevent.hpp"
 #include "jfile.hpp"
+#include "jptree.hpp"
 #include "jstring.hpp"
 #include <iostream>
 
-// Initial version supports output as a sequence of visits. Future enhancements
-// may include:
-// - an output format selector to choose between XML, jSON, YAML, CSV, et al
+// Future enhancements may include:
+// - an output format selector to choose between jSON, YAML, CSV, et al
 // - support for multiple input files
 // - support for event filtering
+
+// Supported dump output formats.
+enum class OutputFormat : byte
+{
+    text,
+    xml_p, // XML from property tree
+    xml_d, // XML directly from visited content
+};
+
+// Extension of CPTreeEventVisitor in which the destructor outputs the complete property tree in a
+// requested structured format.
+class DumpPTreeEventVisitor : public CPTreeEventVisitor
+{
+protected:
+    OutputFormat format;
+public:
+    DumpPTreeEventVisitor(std::ostream& out, OutputFormat _format)
+        : CPTreeEventVisitor(out)
+        , format(_format)
+    {
+    }
+    ~DumpPTreeEventVisitor()
+    {
+        if (tree)
+        {
+            StringBuffer markup;
+            switch (format)
+            {
+            case OutputFormat::xml_p:
+                toXML(tree, markup);
+                break;
+            default:
+                std::cerr << "unsupported output format " << byte(format) << std::endl;
+                break;
+            }
+            out << markup.str() << std::endl;
+        }
+    }
+};
+
+// Implementation of IEventVisitor constructs and outputs one XML element at a time. The formatted
+// output resembles that produced by DumpPTreeEventVisitor, with the exception that element and
+// attribute ordering may differ.
+class DumpXMLEventVisitor : public CInterfaceOf<IEventVisitor>
+{
+public:
+    virtual bool visitFile(const char* filename, uint32_t version) override
+    {
+        appendXMLOpenTag(markup, EVT_PTREE_ROOT, nullptr, false);
+        attribute(EVT_PTREE_FILE_NAME, filename);
+        markup.append('>');
+        appendXMLOpenTag(markup, EVT_PTREE_HEADER, nullptr, false);
+        attribute(EVT_PTREE_FILE_VERSION, version);
+        return true;
+    }
+    virtual Continuation visitEvent(EventType id) override
+    {
+        endElement();
+        appendXMLOpenTag(markup, EVT_PTREE_EVENT, nullptr, false);
+        appendXMLAttr(markup, EVT_PTREE_EVENT_NAME, queryEventName(id));
+        attribute(EVT_PTREE_EVENT_ID, uint64_t(id));
+        return visitContinue;
+    }
+    virtual Continuation visitAttribute(EventAttr id) override
+    {
+        return visitContinue;
+    }
+    virtual Continuation visitAttribute(EventAttr id, const char* value) override
+    {
+        if (EvAttrSysOption == id)
+            attribute(value, true);
+        else
+            attribute(queryEventAttributeName(id), value);
+        return visitContinue;
+    }
+    virtual Continuation visitAttribute(EventAttr id, bool value) override
+    {
+        attribute(queryEventAttributeName(id), value);
+        return visitContinue;
+    }
+    virtual Continuation visitAttribute(EventAttr id, uint8_t value) override
+    {
+        attribute(queryEventAttributeName(id), value);
+        return visitContinue;
+    }
+    virtual Continuation visitAttribute(EventAttr id, uint16_t value) override
+    {
+        attribute(queryEventAttributeName(id), value);
+        return visitContinue;
+    }
+    virtual Continuation visitAttribute(EventAttr id, uint32_t value) override
+    {
+        attribute(queryEventAttributeName(id), value);
+        return visitContinue;
+    }
+    virtual Continuation visitAttribute(EventAttr id, uint64_t value) override
+    {
+        attribute(queryEventAttributeName(id), value);
+        return visitContinue;
+    }
+    virtual void leaveFile(uint32_t bytesRead) override
+    {
+        endElement();
+        appendXMLOpenTag(markup, EVT_PTREE_FOOTER, nullptr, false);
+        attribute(EVT_PTREE_FILE_BYTES_READ, bytesRead);
+        endElement();
+        appendXMLCloseTag(markup, EVT_PTREE_ROOT);
+        out << markup.str() << std::endl;
+    }
+protected:
+    std::ostream& out;
+    StringBuffer markup;
+public:
+    DumpXMLEventVisitor(std::ostream& _out) : out(_out)
+    {
+    }
+    void attribute(const char* name, uint64_t value)
+    {
+        char buf[21];
+        sprintf(buf, "%lu", value);
+        appendXMLAttr(markup, name, buf);
+    }
+    void attribute(const char* name, const char* value)
+    {
+        appendXMLAttr(markup, name, value);
+    }
+    template <typename T>
+    void attribute(const char* name, T value)
+    {
+        markup.append(' ').append(name).append("=\"").append(value).append('"');
+    }
+    void endElement()
+    {
+        if (!markup.isEmpty())
+        {
+            markup.append("/>");
+            out << markup.str();
+            markup.clear();
+        }
+    }
+};
 
 // Open and parse a binary event file.
 class EventFileDump
@@ -35,6 +177,12 @@ public:
     void setFile(const char* filename)
     {
         file.set(filename);
+    }
+
+    // Cache the requested output format.
+    void setFormat(OutputFormat format)
+    {
+        this->format = format;
     }
 
     // Cache the output stream to receive the parsed data.
@@ -55,11 +203,28 @@ public:
     // Perform the requested action.
     bool dump()
     {
-        Owned<IEventVisitor> visitor = createVisitTrackingEventVisitor(*out);
+        Owned<IEventVisitor> visitor;
+        switch (format)
+        {
+        case OutputFormat::text:
+            visitor.setown(createVisitTrackingEventVisitor(*out));
+            break;
+        case OutputFormat::json_p:
+        case OutputFormat::xml_p:
+        case OutputFormat::yaml_p:
+            visitor.setown(new DumpPTreeEventVisitor(*out, format));
+            break;
+        case OutputFormat::xml_d:
+            visitor.setown(new DumpXMLEventVisitor(*out));
+            break;
+        default:
+            throw makeStringExceptionV(-1, "unsupported output format: %d", (int)format);
+        }
         return readEvents(file.str(), *visitor);
     }
 protected:
     StringAttr file;
+    OutputFormat format = OutputFormat::text;
     std::ostream* out = nullptr;
 };
 
@@ -67,6 +232,27 @@ protected:
 class CEvtDumpCommand : public CEvToolCommand
 {
 public:
+    virtual bool acceptTerseOption(char opt) override
+    {
+        bool accepted = CEvToolCommand::acceptTerseOption(opt);
+        if (!accepted)
+        {
+            switch (opt)
+            {
+            case 'x':
+                efd.setFormat(OutputFormat::xml_p);
+                accepted = true;
+                break;
+            case 'X':
+                efd.setFormat(OutputFormat::xml_d);
+                accepted = true;
+                break;
+            default:
+                break;
+            }
+        }
+        return accepted;
+    }
     virtual bool acceptParameter(const char* arg) override
     {
         efd.setFile(arg);
@@ -74,13 +260,13 @@ public:
     }
     virtual bool isGoodRequest() override
     {
+        efd.setOutput(std::cout);
         return efd.ready();
     }
     virtual int doRequest() override
     {
         try
         {
-            efd.setOutput(std::cout);
             return efd.dump() ? 0 : 1;
         }
         catch (IException* e)
@@ -98,6 +284,8 @@ public:
         out << "[options] <filename>" << std::endl << std::endl;
         out << "Parse a binary event file and write its contents to standard output." << std::endl << std::endl;
         out << "  -?, -h, --help  show this help message and exit" << std::endl;
+        out << "  -x              output a property tree as XML" << std::endl;
+        out << "  -X              output directly as XML" << std::endl;
         out << "  <filename>      full path to a binary event data file" << std::endl;
         out << std::endl;
     }
