bdd071672ea952f04bcbdd55926ab4c34044a545
Jake Smith <jake.smith@lexisnexisrisk.com>
2025-04-10 13:48:08 +0100

    HPCC-31835 Thor client queue priority
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

--- ecl/agentexec/agentexec.cpp
+++ ecl/agentexec/agentexec.cpp
@@ -51,6 +51,7 @@ private:
     Owned<IThreadPool> pool; // for containerized only
     std::atomic<bool> running = { false };
     bool isThorAgent = false;
+    bool disableQueuePriority = false; // temporary JIC, while new client priority queuing beds in.
 
 friend class WaitThread;
 };
@@ -80,6 +81,7 @@ CEclAgentExecutionServer::CEclAgentExecutionServer(IPropertyTree *_config) : con
         isThorAgent = streq("thor", apptype);
     else
         apptype = "hthor";
+    disableQueuePriority = getComponentConfigSP()->getPropBool("expert/@disableQueuePriority");
 
     StatisticCreatorType ctype = SCThthor;
     if (strieq(apptype, "roxie"))
@@ -122,14 +124,20 @@ int CEclAgentExecutionServer::run()
         Owned<IGroup> serverGroup = createIGroupRetry(daliServers, DALI_SERVER_PORT);
         initClientProcess(serverGroup, DCR_AgentExec);
 #ifdef _CONTAINERIZED
-        if (streq("thor", apptype))
+        if (isThorAgent)
         {
             getClusterThorQueueName(queueNames, agentName);
-            if (config->getPropBool("@multiJobLinger", defaultThorMultiJobLinger))
+            if (disableQueuePriority)
             {
-                StringBuffer lingerQueueName;
-                getClusterLingerThorQueueName(lingerQueueName, agentName);
-                lingerQueue.setown(createJobQueue(lingerQueueName));
+                // old mechanism used to test a separate queue that only Thor instances listened to (<name>.lingerthor)
+                // by queuing to it for a short period, and if not pulled off, launching a new instance. (see queueJobIfQueueWaiting)
+                // In new client priority implementation, the agent will not pull it off the queue in the 1st place.
+                if (config->getPropBool("@multiJobLinger", defaultThorMultiJobLinger))
+                {
+                    StringBuffer lingerQueueName;
+                    getClusterLingerThorQueueName(lingerQueueName, agentName);
+                    lingerQueue.setown(createJobQueue(lingerQueueName));
+                }
             }
         }
         else
@@ -170,6 +178,7 @@ int CEclAgentExecutionServer::run()
     {
         running = true;
         LocalIAbortHandler abortHandler(*this);
+        unsigned __int64 priority = 0;
         while (running)
         {
             if (pool)
@@ -183,7 +192,7 @@ int CEclAgentExecutionServer::run()
             }
 
             PROGLOG("AgentExec: Waiting on queue(s) '%s'", queueNames.str());
-            Owned<IJobQueueItem> item = queue->dequeue();
+            Owned<IJobQueueItem> item = queue->dequeuePriority(priority);
             if (item.get())
             {
                 PROGLOG("AgentExec: Dequeued workunit request '%s'", item->queryWUID());
@@ -206,6 +215,16 @@ int CEclAgentExecutionServer::run()
                 removeSentinelFile(sentinelFile); // no reason to restart
                 break;
             }
+
+            // At the moment, we only prioritize if a thoragent, because it is the only component that fires instances that then also listen to the same queue
+            // That will change, when eclccserver and hthor use the same model.
+            //
+            // In all other cases, there may be multiple agents (via replicas) listening to the same queue, but there's no particular advantage to prioritizing
+            // one over the other, and theoretically could introduce some unexpected behaviour, like a skew of local disk usage.
+            // That will requirement will also change, if we have automatic horizontal scaling in place. Then we would want to prioritize the agents that have
+            // recently been in use.
+            if (!disableQueuePriority && isThorAgent)
+                priority = getTimeStampNowValue();
         }
         DBGLOG("Closing down");
     }
@@ -279,6 +298,7 @@ public:
             {
                 sharedK8sJob = true;
                 constexpr unsigned queueWaitingCheckPeriodMs = 1000;
+                // NB: queueJobIfQueueWaiting is a legacy mechanism (should be deleted), for now only used if disableQueuePriority=true
                 if (!owner.lingerQueue || !queueJobIfQueueWaiting(owner.lingerQueue, item, queueWaitingCheckPeriodMs, queueWaitingCheckPeriodMs))
                 {
                     std::list<std::pair<std::string, std::string>> params = { };
