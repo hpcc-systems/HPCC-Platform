a597d8e49d22240c4dc7695130f76053a6891f74
Jake Smith <jake.smith@lexisnexisrisk.com>
2025-05-23 12:29:31 +0100

    HPCC-34196 Fix single part striped regression
    Regression introduced by HPCC-33928 fixes, causing single part
    (hthor) striped logical files to be unreadable by Thor.
    
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

--- dali/base/dafdesc.hpp
+++ dali/base/dafdesc.hpp
@@ -193,8 +193,9 @@ typedef IIteratorOf<IPartDescriptor> IPartDescriptorIterator;
 enum class FileDescriptorFlags
 {
     none          = 0x00,
-    dirperpart    = 0x01,
-    foreign       = 0x02
+    dirperpart    = 0x01, // each part is in a separate directory (named by part number)
+    foreign       = 0x02, // the file descriptor is for a foreign file
+    absoluteparts = 0x04  // the file descriptor has been constructed with parts with absolute filenames (see doSetPart)
 };
 BITMASK_ENUM(FileDescriptorFlags);
 
