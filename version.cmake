a3f3d6929df8d8209d3b957264eded5b9c99a5e9
Gordon Smith <GordonJSmith@gmail.com>
2025-06-24 16:39:00 +0100

    Community Edition 9.14.0-rc1 Release Candidate 1
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- version.cmake
+++ version.cmake
@@ -4,9 +4,9 @@
 set ( HPCC_NAME "Community Edition" )
 set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 9 )
-set ( HPCC_MINOR 13 )
+set ( HPCC_MINOR 14 )
 set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "trunk" )
-set ( HPCC_SEQUENCE 0 )
-set ( HPCC_TAG_TIMESTAMP "2025-04-14T14:22:42Z" )
+set ( HPCC_MATURITY "rc" )
+set ( HPCC_SEQUENCE 1 )
+set ( HPCC_TAG_TIMESTAMP "2025-06-24T15:38:59Z" )
 ###
