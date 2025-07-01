8b3fab0e3a48fb1366c880bbb077cb1a64d47455
Shamser Ahmed <shamser.ahmed@lexisnexisrisk.com>
2025-07-01 14:16:58 +0100

    HPCC-34493 Report CostExecute and CostExecute aggregates while the graph is active
    Signed-off-by: Shamser Ahmed <shamser.ahmed@lexisnexisrisk.com>

--- thorlcr/graph/thgraphmaster.cpp
+++ thorlcr/graph/thgraphmaster.cpp
@@ -3193,9 +3193,11 @@ void CMasterGraph::getStats(IStatisticGatherer &stats)
     cost_type costDiskAccess = getDiskAccessCost();
     if (costDiskAccess)
         stats.addStatistic(StCostFileAccess, costDiskAccess);
+    stats.addStatistic(StCostExecute, getExecuteCost());
+    stats.addStatistic(StTimeElapsed, cycle_to_nanosec(getElapsedCycles()));
 }
 
-cost_type CMasterGraph::getDiskAccessCost()
+cost_type CMasterGraph::getDiskAccessCost() const
 {
     cost_type totalDiskAccessCost = 0;
     Owned<IThorGraphIterator> iterChildGraph = getChildGraphIterator();
