cda7e9ca6df343719592f2c304d5085eb0fe74d8
Dave Streeter <dave.streeter@lexisnexisrisk.com>
2025-08-07 12:15:53 +0100

    HPCC-34477 Load and Save timing tests.
    Signed-off-by: Dave Streeter <dave.streeter@lexisnexisrisk.com>

--- dali/base/dasds.cpp
+++ dali/base/dasds.cpp
@@ -5778,6 +5778,12 @@ public:
             OwnedIFile iBinaryFileTmpStore;
             unsigned binaryCrc{0};
             unsigned *binaryCrcPtr{nullptr};
+
+#define DAVE
+#ifdef DAVE
+            saveBinary = true;
+#endif
+
             if (saveBinary)
             {
                 StringBuffer tmpStoreName;
@@ -6421,6 +6427,36 @@ void CCovenSDSManager::loadStore(const bool *abort)
             LOG(MCdebugInfo, "Store %d does not exist, creating new store", iStoreHelper->queryCurrentEdition());
             root = new CServerRemoteTree("SDS");
         }
+
+#define DAVE
+#ifdef DAVE
+        int i{0};
+        for(i = 0; i < 5; i++)
+        {
+            PROGLOG("Save test %d", i);
+            iStoreHelper->saveStore(root, nullptr);
+
+        }
+        PROGLOG("Save tests complete");
+        for(i = 0; i < 5; i++)
+        {
+            PROGLOG("Load test %d", i);
+            // Try loading binary store first if it exists
+            try
+            {
+                root = loadStoreType(StoreFormat::BINARY, bufferSize, abort);
+            }
+            catch (IException *e)
+            {
+                // Log the exception but do not re-throw or exit to allow fall back to loading the XML store
+                EXCLOG(e);
+                e->Release();
+            }
+            root = loadStoreType(StoreFormat::XML, bufferSize, abort);
+        }
+        PROGLOG("Load tests complete");
+#endif
+
         bool errors;
         Owned<IException> deltaE;
         try { iStoreHelper->loadDeltas(root, &errors); }
