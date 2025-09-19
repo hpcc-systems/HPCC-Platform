0713d6d99d3dedf372fc833d6278ca71a2c03efa
Jeremy Clements <79224539+jeclrsg@users.noreply.github.com>
2025-09-19 14:38:52 -0400

    HPCC-34981 ECL Watch v9 MessageBox and forms/Groups to Fluent v9
    replace any FluentUI v8 components with v9 in our MessageBox component
    and forms/Groups helper components
    
    Signed-off-by: Jeremy Clements <79224539+jeclrsg@users.noreply.github.com>

--- esp/src/src-react/components/forms/Groups.tsx
+++ esp/src/src-react/components/forms/Groups.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
-import { Label, mergeStyleSets } from "@fluentui/react";
+import { Label, makeStyles } from "@fluentui/react-components";
 import { createInputs, Fields } from "./Fields";
-import { useUserTheme } from "../../hooks/theme";
 
 interface FieldsTableProps {
     fields: Fields;
@@ -9,22 +8,35 @@ interface FieldsTableProps {
     onChange?: (id: string, newValue: any) => void;
 }
 
-const tableGroupStyles = mergeStyleSets({
+const useTableGroupStyles = makeStyles({
     root: {
-        padding: 4,
+        padding: "4px",
         minWidth: "66%",
-        selectors: {
-            ".ms-Textfield": {
-                width: "80%"
-            },
-            ".ms-TextField-field[readonly]": {
-                padding: 0
-            },
-            ".ms-Link": {
-                paddingLeft: "0 !important"
-            }
+        "& .ms-TextField-field[readonly]": { padding: 0 },
+        "& .ms-Link": { paddingLeft: "0 !important" }
+    },
+    labelCell: { padding: "5px 0", whiteSpace: "nowrap", fontWeight: 600 },
+    fieldCell: { width: "80%", paddingLeft: "8px" }
+});
+
+const useMultiColumnStyles = makeStyles({
+    root: {
+        borderCollapse: "collapse",
+        minWidth: "80%",
+        "& tbody tr:nth-child(odd)": { backgroundColor: "var(--colorNeutralBackground3)" },
+        "& th": {
+            padding: "0 1em 1em 1em",
+            fontSize: "1em",
+            fontWeight: 600
+        },
+        "& td": {
+            padding: "0.85em 1em",
+            fontSize: "1em",
+            lineHeight: "1em"
         }
-    }
+    },
+    wrapper: { padding: "0 0 1em 0.3em" },
+    label: { marginBottom: 0 }
 });
 
 export const TableGroup: React.FunctionComponent<FieldsTableProps> = ({
@@ -35,12 +47,14 @@ export const TableGroup: React.FunctionComponent<FieldsTableProps> = ({
 
     const formFields: { id: string, label: string, field: any }[] = createInputs(fields, onChange);
 
-    return <table className={tableGroupStyles.root} style={{ width }}>
+    const tableGroupClasses = useTableGroupStyles();
+
+    return <table className={tableGroupClasses.root} style={{ width }}>
         <tbody>
             {formFields.map((ff) => {
                 return <tr key={ff.id}>
-                    <td style={{ whiteSpace: "nowrap" }}><Label htmlFor={ff.id}>{ff.label}</Label></td>
-                    <td style={{ width: "80%", paddingLeft: 8 }}>{ff.field}</td>
+                    <td className={tableGroupClasses.labelCell}><Label htmlFor={ff.id}>{ff.label}</Label></td>
+                    <td className={tableGroupClasses.fieldCell}>{ff.field}</td>
                 </tr>;
             })}
         </tbody>
@@ -58,33 +72,11 @@ export const MultiColumnTableGroup: React.FunctionComponent<MultiColumnTableGrou
     columns,
     rows
 }) => {
-    const { theme } = useUserTheme();
+    const multiColumnClasses = useMultiColumnStyles();
 
-    const tableClasses = React.useMemo(() => mergeStyleSets({
-        root: {
-            borderCollapse: "collapse",
-            minWidth: "80%",
-            selectors: {
-                "tbody tr:nth-child(odd)": {
-                    backgroundColor: theme.palette.neutralLighter
-                },
-                "th": {
-                    padding: label ? "0 1em 1em 1em" : "1em",
-                    fontSize: "1em",
-                    fontWeight: 600
-                },
-                "td": {
-                    padding: "0.85em 1em",
-                    fontSize: "1em",
-                    lineHeight: "1em"
-                }
-            }
-        }
-    }), [label, theme]);
-
-    return <div style={{ padding: "0 0 1em 0.3em" }}>
-        {label && <h3 style={{ marginBottom: 0 }}>{label}</h3>}
-        <table className={tableClasses.root}>
+    return <div className={multiColumnClasses.wrapper}>
+        {label && <h3 className={multiColumnClasses.label}>{label}</h3>}
+        <table className={multiColumnClasses.root}>
             <thead>
                 <tr>
                     {columns.length < Object.keys(rows[0]).length && <th></th>}
