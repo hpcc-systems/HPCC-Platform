hpcc-systems/HPCC-Platform

commit a685b609fcc1591948ba866780ceb29e7252b415

HPCC-34937 Bump About to Fluent UI v9
Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

esp/src/eclwatch/WUDetailsWidget.js
@@ -367,7 +367,7 @@ define([
             this.checkIfClustersAllowed();
             this.checkThorLogStatus();
             this.statusNode = dom.byId(this.id + "WUStatus");
-            srcReact.render(srcReact.WUStatus, { wuid: params.Wuid }, this.statusNode);
+            srcReact.themedRender(srcReact.WUStatus, { wuid: params.Wuid }, this.statusNode);
 
             this.protected.on("click", function (evt) {
                 context._onSave();

esp/src/eclwatch/HPCCPlatformWidget.js
@@ -556,7 +556,7 @@ define([
         _onAboutLoaded: false,
         _onAbout: function (evt) {
             var placeholderNode = dom.byId(this.id + "DialogPlaceholder");
-            srcReact.render(AboutModule.About, {
+            srcReact.lightThemedRender(AboutModule.About, {
                 eclwatchVersion: "5",
                 show: true,
                 onClose: function () {

esp/src/src-react/components/About.tsx
@@ -1,14 +1,32 @@
 import * as React from "react";
-import { DefaultButton, Dialog, DialogFooter, DialogType, Pivot, PivotItem, Spinner } from "@fluentui/react";
+import { makeStyles, Dialog, Button, SelectTabData, SelectTabEvent, Spinner, Tab, TabList, TabValue, DialogSurface, DialogBody, DialogTitle, DialogContent, DialogActions, DialogOpenChangeEvent, DialogOpenChangeData } from "@fluentui/react-components";
 import nlsHPCC from "src/nlsHPCC";
 import { useCheckFeatures, fetchLatestReleases } from "../hooks/platform";
 import { TableGroup } from "./forms/Groups";
 import { Fields } from "./forms/Fields";
 
-const dialogContentProps = {
-    type: DialogType.largeHeader,
-    title: nlsHPCC.AboutHPCCSystems
-};
+const dateOptions: Intl.DateTimeFormatOptions = { year: "numeric", month: "long", day: "numeric" };
+
+const useStyles = makeStyles({
+    dialogSurface: {
+        minWidth: "640px",
+    },
+    tabContent: {
+        minHeight: "208px",
+        paddingTop: "32px",
+    },
+    tabContentLatest: {
+        minHeight: "208px",
+        overflow: "hidden",
+        paddingTop: "32px",
+    },
+});
+
+enum LOAD_RELEASES {
+    NOT_LOADED = 0,
+    LOADING = 1,
+    LOADED = 2
+}
 
 interface AboutProps {
     eclwatchVersion: string;
@@ -16,67 +34,86 @@ interface AboutProps {
     onClose?: () => void;
 }
 
-const dateOptions: Intl.DateTimeFormatOptions = { year: "numeric", month: "long", day: "numeric" };
-
 export const About: React.FunctionComponent<AboutProps> = ({
     eclwatchVersion = "",
     show = false,
     onClose = () => { }
 }) => {
 
-    const [loaded, setLoaded] = React.useState(0);
+    const [activeTab, setActiveTab] = React.useState<TabValue>("about");
+    const [loaded, setLoaded] = React.useState<LOAD_RELEASES>(LOAD_RELEASES.NOT_LOADED);
     const [latestReleases, setLatestReleases] = React.useState<Fields>({});
 
     const features = useCheckFeatures();
+    const styles = useStyles();
 
     React.useEffect(() => {
-        if (show && loaded === 0) {
-            setLoaded(1);
-            fetchLatestReleases().then(releases => {
-                const fields: Fields = {};
-                releases.forEach(release => {
-                    fields[release.tag_name] = {
-                        label: release.tag_name,
-                        type: "link",
-                        value: release.html_url.replace("github.com/hpcc-systems/HPCC-Platform", "..."),
-                        href: release.html_url,
-                        newTab: true,
-                        readonly: true
-                    };
+        if (!show) return;
+        if (loaded === LOAD_RELEASES.NOT_LOADED) {
+            setLoaded(LOAD_RELEASES.LOADING);
+            fetchLatestReleases()
+                .then(releases => {
+                    const fields: Fields = {};
+                    releases.forEach(release => {
+                        fields[release.tag_name] = {
+                            label: release.tag_name,
+                            type: "link",
+                            value: release.html_url.replace("github.com/hpcc-systems/HPCC-Platform", "..."),
+                            href: release.html_url,
+                            newTab: true,
+                            readonly: true
+                        };
+                    });
+                    setLoaded(LOAD_RELEASES.LOADED);
+                    setLatestReleases(fields);
                 });
-                setLoaded(2);
-                setLatestReleases(fields);
-            });
         }
     }, [loaded, show]);
 
-    return <Dialog hidden={!show} onDismiss={onClose} dialogContentProps={dialogContentProps} minWidth="640px">
-        <Pivot>
-            <PivotItem itemKey="about" headerText={nlsHPCC.About}>
-                <div style={{ minHeight: "208px", paddingTop: "32px" }}>
-                    <TableGroup width="100%" fields={{
-                        platformVersion: { label: `${nlsHPCC.Platform}:`, type: "string", value: features?.version || "???", readonly: true },
-                        platformDate: { label: `${nlsHPCC.BuildDate}:`, type: "string", value: features?.timestamp?.toLocaleDateString(undefined, dateOptions) || "???", readonly: true },
-                        eclwatchVersion: { label: "ECL Watch:", type: "string", value: eclwatchVersion, readonly: true },
-                    }}>
-                    </TableGroup>
-                </div>
-            </PivotItem>
-            <PivotItem itemKey="latest" headerText={nlsHPCC.LatestReleases}>
-                {
-                    loaded >= 2 &&
-                    <div style={{ minHeight: "208px", overflow: "hidden", paddingTop: "32px" }}>
-                        <TableGroup width="100%" fields={latestReleases}>
-                        </TableGroup>
-                    </div> ||
-                    <div style={{ minHeight: "208px", paddingTop: "32px" }}>
-                        <Spinner labelPosition="bottom" label={nlsHPCC.Loading} />
+    const onTabSelect = React.useCallback((event: SelectTabEvent, data: SelectTabData) => {
+        setActiveTab(data.value);
+    }, []);
+
+    const onDialogOpenChange = React.useCallback((_: DialogOpenChangeEvent, data: DialogOpenChangeData) => {
+        if (!data.open) {
+            onClose();
+        }
+    }, [onClose]);
+
+    return <Dialog open={show} modalType="modal" onOpenChange={onDialogOpenChange}>
+        <DialogSurface className={styles.dialogSurface}>
+            <DialogBody>
+                <DialogTitle>{nlsHPCC.AboutHPCCSystems}</DialogTitle>
+                <DialogContent>
+                    <TabList selectedValue={activeTab} onTabSelect={onTabSelect}>
+                        <Tab value="about">{nlsHPCC.About}</Tab>
+                        <Tab value="latest">{nlsHPCC.LatestReleases}</Tab>
+                    </TabList>
+                    <div>
+                        {activeTab === "about" &&
+                            <div className={styles.tabContent}>
+                                <TableGroup width="100%" fields={{
+                                    platformVersion: { label: `${nlsHPCC.Platform}:`, type: "string", value: features?.version || "???", readonly: true },
+                                    platformDate: { label: `${nlsHPCC.BuildDate}:`, type: "string", value: features?.timestamp?.toLocaleDateString(undefined, dateOptions) || "???", readonly: true },
+                                    eclwatchVersion: { label: "ECL Watch:", type: "string", value: eclwatchVersion, readonly: true },
+                                }} />
+                            </div>
+                        }
+                        {activeTab === "latest" && (loaded >= LOAD_RELEASES.LOADED ?
+                            <div className={styles.tabContentLatest}>
+                                <TableGroup width="100%" fields={latestReleases}>
+                                </TableGroup>
+                            </div> :
+                            <div className={styles.tabContent}>
+                                <Spinner labelPosition="below" label={nlsHPCC.Loading} />
+                            </div>)
+                        }
                     </div>
-                }
-            </PivotItem>
-        </Pivot>
-        <DialogFooter>
-            <DefaultButton onClick={onClose} text={nlsHPCC.OK} />
-        </DialogFooter>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={onClose} appearance="primary" >{nlsHPCC.OK}</Button>
+                </DialogActions>
+            </DialogBody>
+        </DialogSurface>
     </Dialog>;
 };

esp/src/src-react/hooks/theme.ts
@@ -24,6 +24,14 @@ export function useNavWide(): { navWide: boolean, setNavWide: (value: boolean) =
     return { navWide, setNavWide };
 }
 
+export function useLightTheme(): { theme: Theme, themeV9: ThemeV9 } {
+
+    return {
+        theme: lightTheme,
+        themeV9: lightThemeV9,
+    };
+}
+
 export function useUserTheme(): { theme: Theme, themeV9: ThemeV9, setTheme: (value: "light" | "dark") => void, isDark: boolean } {
 
     const [theme, setTheme] = useUserStore(THEME, "light", true);

esp/src/src/react/Themed.tsx
@@ -1,0 +1,29 @@
+
+import * as React from "react";
+import { ReactNode } from "react";
+import { ThemeProvider } from "@fluentui/react";
+import { FluentProvider, makeStyles } from "@fluentui/react-components";
+import { useLightTheme } from "../../src-react/hooks/theme";
+
+const useStyles = makeStyles({
+    root: {
+        height: "100%"
+    }
+});
+
+export interface LightThemedProps {
+    children?: ReactNode;
+}
+
+export const LightThemed = ({ children }: LightThemedProps) => {
+
+    const { theme, themeV9 } = useLightTheme();
+    const styles = useStyles();
+
+    return <FluentProvider theme={themeV9} className={styles.root}>
+        <ThemeProvider theme={theme} className={styles.root}>
+            {children}
+        </ThemeProvider>
+    </FluentProvider>;
+};
+

esp/src/src/react/render.ts
@@ -1,9 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { LightThemed } from "./Themed";
 
 export function render<P>(C: React.FunctionComponent<P>, props: Readonly<P>, _parent: Element | Document | ShadowRoot | DocumentFragment | string) {
     const parent = typeof _parent === "string" ? document.getElementById(_parent) : _parent;
     ReactDOM.render(React.createElement(C, props), parent);
+}
+
+export function lightThemedRender<P>(C: React.FunctionComponent<P>, props: Readonly<P>, _parent: Element | Document | ShadowRoot | DocumentFragment | string) {
+    const parent = typeof _parent === "string" ? document.getElementById(_parent) : _parent;
+    ReactDOM.render(
+        React.createElement(LightThemed, null, React.createElement(C, props)),
+        parent
+    );
 }
 
 export function svgRender<P>(C: React.FunctionComponent<P>, props: Readonly<P>, parent: Element | Document | ShadowRoot | DocumentFragment) {

esp/src/src/react/wuStatus.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
-import { ThemeProvider } from "@fluentui/react";
-import { FluentProvider } from "@fluentui/react-components";
 import { Workunit, WUStateID } from "@hpcc-js/comms";
-import { useUserTheme } from "../../src-react/hooks/theme";
 import { StepProps, Stepper } from "../../src-react/components/controls/Stepper";
 import nlsHPCC from "../nlsHPCC";
 
@@ -70,7 +67,6 @@ export const WUStatus: React.FunctionComponent<WUStatus> = ({
     wuid
 }) => {
     const [activeStep, setActiveStep] = React.useState(-1);
-    const { theme, themeV9 } = useUserTheme();
     const [failed, setFailed] = React.useState(false);
     const [stepProps, setStepProps] = React.useState<StepProps[]>();
     const [steps, setSteps] = React.useState([]);
@@ -102,9 +98,5 @@ export const WUStatus: React.FunctionComponent<WUStatus> = ({
         }));
     }, [activeStep, failed, steps]);
 
-    return <FluentProvider theme={themeV9} >
-        <ThemeProvider theme={theme}>
-            <Stepper activeStep={activeStep} steps={stepProps}></Stepper>
-        </ThemeProvider>
-    </FluentProvider>;
+    return <Stepper activeStep={activeStep} steps={stepProps}></Stepper>;
 };

commit 87e39ac6e060f5cc5eb26d5bfa01e44edc3d0434

HPCC-35017 ECL Watch Invalid function name
Caused by https://github.com/hpcc-systems/HPCC-Platform/commit/a685b609fcc1591948ba866780ceb29e7252b415

Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

esp/src/eclwatch/WUDetailsWidget.js
@@ -367,7 +367,7 @@ define([
             this.checkIfClustersAllowed();
             this.checkThorLogStatus();
             this.statusNode = dom.byId(this.id + "WUStatus");
-            srcReact.themedRender(srcReact.WUStatus, { wuid: params.Wuid }, this.statusNode);
+            srcReact.lightThemedRender(srcReact.WUStatus, { wuid: params.Wuid }, this.statusNode);
 
             this.protected.on("click", function (evt) {
                 context._onSave();