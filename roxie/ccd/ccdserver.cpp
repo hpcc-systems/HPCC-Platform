5b8dd351096d656801c78d6d2cb89456498a66cf
Gavin Halliday <gavin.halliday@lexisnexis.com>
2025-10-31 17:13:28 +0000

    HPCC-35278 Stop deriving CJoinGroup from CInterface
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- roxie/ccd/ccdserver.cpp
+++ roxie/ccd/ccdserver.cpp
@@ -25920,7 +25920,7 @@ interface IJoinProcessor
 //   
 //------------------------------------------------------------------------------------------------------
 
-class CJoinGroup : public CInterface
+class CJoinGroup
 {
 protected:
     const void *left;                   // LHS row
@@ -25975,6 +25975,12 @@ public:
             roxiemem::ReleaseRoxieRowArray(rows.ordinality(), (const void * *)rows.getArray());
             rows.kill();
         }
+    }
+
+    inline bool Release(void) const
+    {
+        delete this;
+        return true;
     }
 
     inline bool isHeadRecord() const
