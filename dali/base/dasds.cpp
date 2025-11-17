aaebc7dc3a39519b495f75b1193fadc2c6d6dbcf
Jake Smith <jake.smith@lexisnexisrisk.com>
2025-11-17 16:12:28 +0000

    HPCC-35372 Fix transaction slowdown regression
    The fix for HPCC-34374 inadvertently introduce a significant
    slowdown. Changes in the CDeltaWriter thread should have released
    the pendingCrit CS before saving, but held it, that meant all
    other transactons queuing transactions were held up whenever the
    CDeltaWriter was writing.
    
    This was not that noticable in most environments (BM), but in
    Azure where the Dali storage is backed by remote storage, it caused
    a significant slowdown.
    
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

--- dali/base/dasds.cpp
+++ dali/base/dasds.cpp
@@ -1447,6 +1447,7 @@ public:
                         todo.push(std::move(pending.front()));
                         pending.pop();
                     }
+                    b.leave();
                     // Because blockedSaveCrit is held, it will also block 'synchronous save' (see addToQueue)
                     // i.e. if stuck here, the transactions will start building up, and trigger a 'Forced synchronous save',
                     // which will in turn block. This must complete!
