638aa78e41147426809ec70d9c627dcf32fcc539
Gavin Halliday <gavin.halliday@lexisnexis.com>
2025-11-28 14:32:54 +0000

    HPCC-35430 Increase the max compression ratio to 50x
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- system/jhtree/jhinplace.hpp
+++ system/jhtree/jhinplace.hpp
@@ -175,7 +175,7 @@ public:
     offset_t leafMemorySize = 0;
     struct {
         double minCompressionThreshold = 0.95; // use uncompressed if compressed is > 95% uncompressed
-        unsigned maxCompressionFactor = 25;    // Don't compress payload to less than 4% of the original by default (beause when it is read it will use lots of memory)
+        unsigned maxCompressionFactor = 50;    // Don't compress payload to less than 2% of the original by default (because when it is read it will use lots of memory)
         bool recompress = false;
         bool reuseCompressor = true;
     } options;
