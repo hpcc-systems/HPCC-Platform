cccfdea2d31f02622de15e3158c7e1da95a5a6b1
Gavin Halliday <gavin.halliday@lexisnexis.com>
2026-01-13 09:37:08 +0000

    Community Edition 10.2.0-rc1 Release Candidate 1
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -4,9 +4,9 @@
 set ( HPCC_NAME "Community Edition" )
 set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 10 )
-set ( HPCC_MINOR 1 )
+set ( HPCC_MINOR 2 )
 set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "trunk" )
-set ( HPCC_SEQUENCE 0 )
-set ( HPCC_TAG_TIMESTAMP "2025-09-22T16:06:12Z" )
+set ( HPCC_MATURITY "rc" )
+set ( HPCC_SEQUENCE 1 )
+set ( HPCC_TAG_TIMESTAMP "2026-01-13T09:37:08Z" )
 ###
