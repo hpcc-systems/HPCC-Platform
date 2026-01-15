abc4918c482ec4e8fbbffdf1cc954bf75e7a0492
Gordon Smith <GordonJSmith@gmail.com>
2026-01-15 16:01:12 +0000

    HPCC-35599 Bump wasm dependency versions
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- esp/src/src-react/components/MetricsSQL.tsx
+++ esp/src/src-react/components/MetricsSQL.tsx
@@ -5,34 +5,64 @@ import { useConst } from "@fluentui/react-hooks";
 import { IScope } from "@hpcc-js/comms";
 import { ICompletion } from "@hpcc-js/codemirror";
 import { Table } from "@hpcc-js/dgrid";
+import { scopedLogger } from "@hpcc-js/util";
 import * as Utility from "src/Utility";
+import nlsHPCC from "src/nlsHPCC";
 import { useDuckDBConnection } from "../hooks/duckdb";
 import { HolyGrail } from "../layouts/HolyGrail";
 import { AutosizeHpccJSComponent } from "../layouts/HpccJSAdapter";
 import { debounce } from "../util/throttle";
 import { SQLSourceEditor } from "./SourceEditor";
-import nlsHPCC from "src/nlsHPCC";
+
+const logger = scopedLogger("src-react/hooks/MetricsSQL.tsx");
 
 const spaceRegex = new RegExp("\\s", "g");
+function parseSQLErrorMessage(message?: string | Array<string>): string[][] {
+    if (Array.isArray(message)) {
+        message = message.join("\n");
+    }
+    if (typeof message === "string") {
+        return message.toString().split("\n").map(line => {
+            if (line.indexOf("LINE") === 0) {
+            } else if (line.includes("^")) {
+                line = line.replace(spaceRegex, "&nbsp;");
+            }
+            return [line];
+        });
+    }
+    return [];
+}
+
+
+interface Schema {
+    column_name: string;
+    column_type: string;
+    default: unknown;
+    extra: unknown;
+    key: unknown;
+    null: string;
+}
 
 interface MetricsDataProps {
+    wuid: string;
     defaultSql: string;
     scopes: IScope[];
     onSelectionChanged: (selection: IScope[]) => void;
 }
 
 export const MetricsSQL: React.FunctionComponent<MetricsDataProps> = ({
+    wuid,
     defaultSql,
     scopes,
     onSelectionChanged
 }) => {
 
-    const connection = useDuckDBConnection(scopes, "metrics");
-    const [schema, setSchema] = React.useState<any[]>([]);
+    const { connection } = useDuckDBConnection(scopes, `${wuid}-metrics`);
+    const [schema, setSchema] = React.useState<Schema[]>([]);
+    const [result, setResult] = React.useState<object[]>([]);
     const [sql, setSql] = React.useState<string>(defaultSql);
     const [sqlError, setSqlError] = React.useState<Error | undefined>();
     const [dirtySql, setDirtySql] = React.useState<string>(sql);
-    const [data, setData] = React.useState<any[]>([]);
 
     //  Grid ---
     const columns = React.useMemo((): string[] => {
@@ -60,55 +90,51 @@ export const MetricsSQL: React.FunctionComponent<MetricsDataProps> = ({
     }, [onSelectionChanged, scopesTable]);
 
     React.useEffect(() => {
-        if (columns.length === 0 && data.length === 0 && sqlError) {
+        if (columns.length === 0 && result.length === 0 && sqlError) {
             scopesTable
                 .columns(["Error"])
-                .data(sqlError.message.split("\n").map(line => {
-                    if (line.indexOf("LINE") === 0) {
-                    } else if (line.includes("^")) {
-                        line = line.replace(spaceRegex, "&nbsp;");
-                    }
-                    return [line];
-                }))
+                .data(parseSQLErrorMessage(sqlError.message))
                 .lazyRender()
                 ;
         } else {
+            const data = result.map((row, idx) => {
+                const newRow = [idx + 1];
+                for (const col of columns) {
+                    newRow.push(row[col]);
+                }
+                return newRow;
+            });
             scopesTable
                 .columns(["##"])    //  Reset hash to force recalculation of default widths
                 .columns(["##", ...columns])
-                .data(data.map((row, idx) => [idx + 1, ...row]))
+                .data(data)
                 .lazyRender()
                 ;
         }
-    }, [columns, data, sqlError, scopesTable]);
-
+    }, [columns, result, sqlError, scopesTable]);
     //  Query  ---
     React.useEffect(() => {
         if (scopes.length === 0) {
             setSchema([]);
-            setData([]);
+            setResult([]);
         } else if (connection) {
-            connection.query(`DESCRIBE ${sql}`).then(result => {
-                if (connection) {
-                    setSchema(result.toArray().map((row) => row.toJSON()));
-                }
-            }).catch(e => {
+            try {
+                const result = JSON.parse(connection.queryToJSON(`DESCRIBE ${sql}`));
+                setSchema(result);
+            } catch (e) {
                 setSchema([]);
-            });
+            }
 
-            setSqlError(undefined);
-            connection.query(sql).then(result => {
-                if (connection) {
-                    setData(result.toArray().map((row) => {
-                        return row.toArray();
-                    }));
-                }
-            }).catch(e => {
+            try {
+                setSqlError(undefined);
+                const result = JSON.parse(connection.queryToJSON(sql));
+                setResult(result);
+            } catch (e) {
                 setSqlError(e);
-                setData([]);
-            }).finally(() => {
+                setResult([]);
+            } finally {
                 scopesTable.noDataMessage(nlsHPCC.noDataMessage);
-            });
+            }
         }
     }, [connection, scopes.length, scopesTable, sql]);
 
@@ -118,28 +144,29 @@ export const MetricsSQL: React.FunctionComponent<MetricsDataProps> = ({
     }, []);
 
     const onFetchHints = React.useCallback((cm, option): Promise<ICompletion | null> => {
-        const cursor = cm.getCursor();
-        const lineStr = cm.getLine(cursor.line);
-        let lineEnd = cursor.ch;
-        let end = cm.indexFromPos({ line: cursor.line, ch: lineEnd });
-        if (connection) {
-            return connection.query(`SELECT * FROM sql_auto_complete("${dirtySql.substring(0, end)}")`).then(result => {
-                if (connection) {
-                    const hints = result.toArray().map((row) => row.toJSON());
+        return new Promise<ICompletion | null>(resolve => {
+            const cursor = cm.getCursor();
+            const lineStr = cm.getLine(cursor.line);
+            let lineEnd = cursor.ch;
+            let end = cm.indexFromPos({ line: cursor.line, ch: lineEnd });
+            if (connection) {
+                try {
+                    const hints = JSON.parse(connection.queryToJSON(`SELECT * FROM sql_auto_complete("${dirtySql.substring(0, end)}")`));
                     while (lineEnd < lineStr.length && /\w/.test(lineStr.charAt(lineEnd))) ++lineEnd;
                     end = cm.indexFromPos({ line: cursor.line, ch: lineEnd });
                     const suggestion_start = hints.length ? hints[0].suggestion_start : end;
-                    return {
+                    resolve({
                         list: hints.map(row => row.suggestion),
                         from: cm.posFromIndex(suggestion_start),
                         to: cm.posFromIndex(end)
-                    };
+                    });
+                } catch (e) {
+                    logger.debug(e);
+                    return resolve(null);
                 }
-            }).catch(e => {
-                return Promise.resolve(null);
-            });
-        }
-        return Promise.resolve(null);
+            }
+            return resolve(null);
+        });
     }, [connection, dirtySql]);
 
     const onSubmit = React.useCallback(() => {
@@ -163,8 +190,8 @@ export const MetricsSQL: React.FunctionComponent<MetricsDataProps> = ({
                     <SQLSourceEditor sql={sql} toolbar={false} onSqlChange={onChange} onFetchHints={onFetchHints} onSubmit={onSubmit} ></SQLSourceEditor>
                 </div>
                 <CommandBarButton iconProps={{ iconName: "Play" }} onClick={() => setSql(dirtySql)} />
-                <CommandBarButton disabled={data.length === 0} iconProps={{ iconName: "Copy" }} onClick={onCopy} />
-                <CommandBarButton disabled={data.length === 0} iconProps={{ iconName: "Download" }} onClick={onDownload} />
+                <CommandBarButton disabled={result.length === 0} iconProps={{ iconName: "Copy" }} onClick={onCopy} />
+                <CommandBarButton disabled={result.length === 0} iconProps={{ iconName: "Download" }} onClick={onDownload} />
             </StackShim>
         }
         main={<AutosizeHpccJSComponent widget={scopesTable} ></AutosizeHpccJSComponent>}
