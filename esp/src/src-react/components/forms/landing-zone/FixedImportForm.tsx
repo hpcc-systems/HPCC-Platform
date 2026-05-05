2c9db3c6ef40c16be63f75f1693bb749c2d4eefb
Gordon Smith <GordonJSmith@gmail.com>
2026-05-05 10:54:35 +0100

    fix(eclwatch): improve regex performance (ReDOS)
    Fixes #36095
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- esp/src/src-react/components/forms/landing-zone/FixedImportForm.tsx
+++ esp/src/src-react/components/forms/landing-zone/FixedImportForm.tsx
@@ -225,7 +225,7 @@ export const FixedImportForm: React.FunctionComponent<FixedImportFormProps> = ({
                     />}
                 rules={{
                     pattern: {
-                        value: /^([_a-z0-9]+(::)?)+$/i,
+                        value: /^[_a-z0-9]+(::[_a-z0-9]+)*(?:::)?$/i,
                         message: nlsHPCC.ValidationErrorNamePrefix
                     }
                 }}
